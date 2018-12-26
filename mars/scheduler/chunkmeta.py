854c8539ef60b14581cba9f77036d969b74fb472
wenjun.swj <wenjun.swj@alibaba-inc.com>
2018-12-26 10:12:17 +0800

    add worker-> chunks mapping and delete worker support

--- mars/scheduler/chunkmeta.py
+++ mars/scheduler/chunkmeta.py
@@ -14,16 +14,149 @@
 
 import logging
 import os
-from collections import namedtuple, defaultdict
+from collections import defaultdict
 
 from .kvstore import KVStoreActor
 from .utils import SchedulerActor
-from ..compat import OrderedDict3
+from ..compat import PY27, OrderedDict3
 
 logger = logging.getLogger(__name__)
 
-WorkerMeta = namedtuple('WorkerMeta', 'chunk_size workers')
 _META_CACHE_SIZE = 1000
+
+
+class WorkerMeta(object):
+    __slots__ = 'chunk_size', 'workers'
+
+    def __init__(self, chunk_size=0, workers=None):
+        self.chunk_size = chunk_size
+        self.workers = workers or ()
+
+    def __eq__(self, other):
+        return all(getattr(self, attr) == getattr(other, attr)
+                   for attr in self.__slots__)
+
+    def __iter__(self):
+        for slot in self.__slots__:
+            yield getattr(self, slot)
+
+    def __repr__(self):
+        return 'WorkerMeta(%r, %r)' % (self.chunk_size, self.workers)
+
+    if PY27:
+        def __getstate__(self):
+            return tuple(getattr(self, s) for s in self.__slots__)
+
+        def __setstate__(self, state):
+            self.__init__(**dict(zip(self.__slots__, state)))
+
+
+class ChunkMetaStore(object):
+    """
+    Storage of chunk meta, holding worker -> chunk relation as well
+    """
+    def __init__(self):
+        self._store = dict()
+        self._worker_chunks = defaultdict(set)
+
+    def __contains__(self, item):
+        return item in self._store
+
+    def __getitem__(self, item):
+        return self._store[item]
+
+    def __setitem__(self, key, value):
+        if key in self._store:
+            self._del_worker_in_metas(key, self._store[key].workers)
+
+        self._store[key] = value
+
+        worker_chunks = self._worker_chunks
+        for w in value.workers:
+            worker_chunks[w].add(key)
+
+    def __delitem__(self, key):
+        self._del_worker_in_metas(key, self._store[key].workers)
+        del self._store[key]
+
+    def _del_worker_in_metas(self, key, workers):
+        """
+        Delete worker from meta
+        """
+        worker_chunks = self._worker_chunks
+        for w in workers:
+            worker_chunks[w].remove(key)
+
+    def get(self, key, default=None):
+        return self._store.get(key, default)
+
+    def get_worker_chunks(self, worker, default=None):
+        """
+        Get chunks held in a worker
+        :param worker: worker endpoint
+        :param default: default value
+        """
+        return self._worker_chunks.get(worker, default)
+
+    def remove_worker_keys(self, worker, filter_fun=None):
+        """
+        Remove a worker from storage and return keys of lost chunks
+        :param worker: worker endpoint
+        :param filter_fun: key filter
+        :return: keys of lost chunks
+        """
+        if worker not in self._worker_chunks:
+            return []
+
+        filter_fun = filter_fun or (lambda k: True)
+        store = self._store
+        affected = []
+        for ckey in tuple(self._worker_chunks[worker]):
+            if not filter_fun(ckey):
+                continue
+            self._del_worker_in_metas(ckey, (worker,))
+            meta = store[ckey]
+            new_workers = meta.workers = tuple(w for w in meta.workers if w != worker)
+            if not new_workers:
+                affected.append(ckey)
+        for ckey in affected:
+            del self[ckey]
+        if not self._worker_chunks[worker]:
+            del self._worker_chunks[worker]
+        return affected
+
+
+class ChunkMetaCache(ChunkMetaStore):
+    """
+    Cache of chunk meta with an LRU
+    """
+    def __init__(self, limit=_META_CACHE_SIZE):
+        super(ChunkMetaCache, self).__init__()
+        self._store = OrderedDict3()
+        self._limit = limit
+
+    def __getitem__(self, item):
+        self._store.move_to_end(item)
+        return super(ChunkMetaCache, self).__getitem__(item)
+
+    def get(self, key, default=None):
+        try:
+            self._store.move_to_end(key)
+        except KeyError:
+            pass
+        return super(ChunkMetaCache, self).get(key, default)
+
+    def __setitem__(self, key, value):
+        limit = self._limit
+        store = self._store
+        if key in store:
+            super(ChunkMetaCache, self).__setitem__(key, value)
+            store.move_to_end(key)
+        else:
+            super(ChunkMetaCache, self).__setitem__(key, value)
+            while len(store) > limit:
+                dkey, ditem = store.popitem(False)
+                self._del_worker_in_metas(dkey, ditem.workers)
 
 
 class LocalChunkMetaActor(SchedulerActor):
@@ -36,9 +169,9 @@ class LocalChunkMetaActor(SchedulerActor):
 
     def __init__(self, chunk_info_uid=None):
         super(LocalChunkMetaActor, self).__init__()
-        self._meta_store = dict()
+        self._meta_store = ChunkMetaStore()
         self._meta_broadcasts = dict()
-        self._meta_cache = OrderedDict3()
+        self._meta_cache = ChunkMetaCache()
         self._chunk_info_uid = chunk_info_uid
 
         self._kv_store_ref = None
@@ -75,7 +208,6 @@ class LocalChunkMetaActor(SchedulerActor):
             size = size if size is not None else stored_size
             if workers is not None:
                 workers = set(tuple(workers) + stored_workers)
-        meta = self._meta_store[query_key] = WorkerMeta(size, tuple(workers or ()))
 
         # sync to external kv store
         if self._kv_store_ref is not None:
@@ -85,6 +217,8 @@ class LocalChunkMetaActor(SchedulerActor):
             if workers is not None:
                 for w in workers:
                     self._kv_store_ref.write(path + '/workers/%s' % w, '', _tell=True, _wait=False)
+
+        meta = self._meta_store[query_key] = WorkerMeta(size, tuple(workers or ()))
 
         # broadcast into pre-determined destinations
         futures = []
@@ -105,13 +239,7 @@ class LocalChunkMetaActor(SchedulerActor):
         :param meta: meta data
         """
         query_key = (session_id, chunk_key)
-        if query_key in self._meta_cache:
-            self._meta_cache[query_key] = meta
-            self._meta_cache.move_to_end(query_key)
-        else:
-            self._meta_cache[query_key] = meta
-            while len(self._meta_cache) > _META_CACHE_SIZE:
-                self._meta_cache.popitem(False)
+        self._meta_cache[query_key] = meta
 
     def get_chunk_meta(self, session_id, chunk_key):
         """
@@ -122,26 +250,6 @@ class LocalChunkMetaActor(SchedulerActor):
             return self._meta_store[query_key]
         except KeyError:
             return self._meta_cache.get(query_key)
-
-    def get_workers(self, session_id, chunk_key):
-        """
-        Obtain workers. If not exists, None will be returned.
-        """
-        meta = self.get_chunk_meta(session_id, chunk_key)
-        return meta.workers if meta is not None else None
-
-    def get_chunk_size(self, session_id, chunk_key):
-        """
-        Obtain chunk size. If not exists, None will be returned.
-        """
-        meta = self.get_chunk_meta(session_id, chunk_key)
-        return meta.chunk_size if meta is not None else None
-
-    def add_worker(self, session_id, chunk_key, worker_addr):
-        """
-        Add a worker into metadata
-        """
-        self.set_chunk_meta(session_id, chunk_key, workers=(worker_addr,))
 
     def batch_get_chunk_meta(self, session_id, chunk_keys):
         """
@@ -172,6 +280,20 @@ class LocalChunkMetaActor(SchedulerActor):
         """
         for chunk_key in chunk_keys:
             self.delete_meta(session_id, chunk_key)
+
+    def remove_workers_in_session(self, session_id, workers):
+        """
+        Remove workers from storage given session id and return keys of lost chunks
+        :param session_id: session id
+        :param workers: worker endpoints
+        :return: keys of lost chunks
+        """
+        logger.debug('Removing workers %r from store', workers)
+        chunks = set()
+        for w in workers:
+            self._meta_cache.remove_worker_keys(w, lambda k: k[0] == session_id)
+            chunks.update(self._meta_store.remove_worker_keys(w, lambda k: k[0] == session_id))
+        return [k[1] for k in chunks]
 
 
 class ChunkMetaActor(SchedulerActor):
