548e85e19cca3b8548df33a6c80e3389dbe12815
继盛 <xuye.qin@alibaba-inc.com>
2019-01-12 11:29:46 +0800

    refine import and comment

--- mars/dataframe/core.py
+++ mars/dataframe/core.py
@@ -14,8 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from ..serialize import SerializableWithKey
-from ..core import Entity
+from ..core import Entity, SerializableWithKey
 
 
 class IndexData(SerializableWithKey):
