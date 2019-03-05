66a73af0280a6389019d4ebc3549ba3b4195c2de
hekaisheng <kaisheng.hks@alibaba-inc.com>
2019-03-05 11:38:57 +0800

    use csc matrix in sparse lu execution to make it more efficient

--- mars/lib/sparse/matrix.py
+++ mars/lib/sparse/matrix.py
@@ -87,10 +87,11 @@ def where(cond, x, y):
 
 def lu_sparse_matrix(a):
     a = naked(a)
+    a = a.tocsc()
     super_lu = splinalg.splu(a, permc_spec="NATURAL", diag_pivot_thresh=0, options={"SymmetricMode": True})
     l = super_lu.L
     u = super_lu.U
-    p = sps.csc_matrix(a.shape)
+    p = sps.lil_matrix(a.shape)
     p[super_lu.perm_r, np.arange(a.shape[1])] = 1
     return SparseMatrix(p), SparseMatrix(l), SparseMatrix(u),
 
