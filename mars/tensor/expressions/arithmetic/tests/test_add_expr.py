044778fa3812a99455d29952acb16dfa94d93fd9
继盛 <xuye.qin@alibaba-inc.com>
2019-03-11 16:21:31 +0800

    add more tests

--- mars/tensor/expressions/arithmetic/tests/test_add_expr.py
+++ mars/tensor/expressions/arithmetic/tests/test_add_expr.py
@@ -17,7 +17,7 @@ import numpy as np
 
 from mars.tests.core import TestBase, sps
 from mars.tensor import tensor
-from mars.tensor.expressions.arithmetic import TensorAdd, TensorAddConstant
+from mars.tensor.expressions.arithmetic import add, TensorAdd, TensorAddConstant
 
 
 class Test(TestBase):
@@ -67,6 +67,33 @@ class Test(TestBase):
         self.assertIsInstance(t2.op, TensorAddConstant)
         self.assertEqual(t.issparse(), t2.issparse())
 
+        # test add with out
+        t = tensor(self.raw_data1, chunk_size=(3, 4))
+        t_op_type = type(t.op)
+        t = add(t, 1, out=t)
+
+        serials = self._pb_serial(t)
+        t2 = self._pb_deserial(serials)[t.data]
+
+        self.assertIsInstance(t2.op, TensorAdd)  # not constant
+        self.assertIsInstance(t2.op.out.op, t_op_type)
+        self.assertEqual(t.issparse(), t2.issparse())
+
+        # test add with out and where
+        t = tensor(self.raw_data1, chunk_size=(3, 4))
+        w = tensor(self.raw_data1 < 0.5, chunk_size=(3, 4))
+        t_op_type = type(t.op)
+        w_op_type = type(w.op)
+        t = add(t, 1, out=t, where=w)
+
+        serials = self._pb_serial(t)
+        t2 = self._pb_deserial(serials)[t.data]
+
+        self.assertIsInstance(t2.op, TensorAdd)  # not constant
+        self.assertIsInstance(t2.op.out.op, t_op_type)
+        self.assertIsInstance(t2.op.where.op, w_op_type)
+        self.assertEqual(t.issparse(), t2.issparse())
+
     def testChunkSerialize(self):
         # test add two dense tensors
         t = tensor(self.raw_data1, chunk_size=(3, 4)) + \
@@ -111,6 +138,37 @@ class Test(TestBase):
         c2 = self._pb_deserial(serials)[c.data]
 
         self.assertIsInstance(c2.op, TensorAddConstant)
+        self.assertEqual(c.issparse(), c2.issparse())
+
+        # test add with out
+        t = tensor(self.raw_data1, chunk_size=(3, 4))
+        t_op_type = type(t.op)
+        t = add(t, 1, out=t)
+        t.tiles()
+        c = t.chunks[0]
+
+        serials = self._pb_serial(c)
+        c2 = self._pb_deserial(serials)[c.data]
+
+        self.assertIsInstance(c2.op, TensorAdd)  # not constant
+        self.assertIsInstance(c2.op.out.op, t_op_type)
+        self.assertEqual(c.issparse(), c2.issparse())
+
+        # test add with out and where
+        t = tensor(self.raw_data1, chunk_size=(3, 4))
+        w = tensor(self.raw_data1 < 0.5, chunk_size=(3, 4))
+        t_op_type = type(t.op)
+        w_op_type = type(w.op)
+        t = add(t, 1, out=t, where=w)
+        t.tiles()
+        c = t.chunks[0]
+
+        serials = self._pb_serial(c)
+        c2 = self._pb_deserial(serials)[c.data]
+
+        self.assertIsInstance(c2.op, TensorAdd)  # not constant
+        self.assertIsInstance(c2.op.out.op, t_op_type)
+        self.assertIsInstance(c2.op.where.op, w_op_type)
         self.assertEqual(c.issparse(), c2.issparse())
 
     def testDenseExpr(self):
@@ -194,21 +252,91 @@ class Test(TestBase):
             # test chunk's sparse
             self.assertFalse(chunk.issparse())
 
-    def testSparseExpr(self):
-        # test add two sparse tensor
-        t1 = tensor(self.raw_sparse_data1, chunk_size=(3, 4))
-        t2 = tensor(self.raw_sparse_data2, chunk_size=4)
-        t = t1 + t2
+        # test add with out
+        t1 = tensor(self.raw_data1, chunk_size=(3, 4))
+        t1_data = t1.data
+        t = add(t1, 10, out=t1)
 
         self.assertEqual(t.shape, self.raw_data1.shape)
         self.assertEqual(t.dtype, self.raw_data1.dtype)
-        self.assertTrue(t.issparse())
+        self.assertFalse(t.issparse())
+        self.assertIs(t.op.out, t1_data)
 
         t.tiles()
 
         self.assertEqual(t.shape, self.raw_data1.shape)
         self.assertEqual(t.chunk_shape, (3, 2))
         self.assertEqual(t.dtype, self.raw_data1.dtype)
+        self.assertFalse(t.issparse())
+
+        for i, chunk in enumerate(t.chunks):
+            # test chunk's inputs
+            self.assertEqual(len(chunk.inputs), 3)
+            self.assertIs(chunk.inputs[0], t1_data.chunks[i].data)
+            self.assertEqual(chunk.inputs[1].op.data, 10)
+            self.assertIs(chunk.inputs[0], chunk.inputs[2])  # out is input
+            # test chunk's index
+            self.assertEqual(chunk.index, t1.chunks[i].index)
+            # test chunk's dtype
+            self.assertEqual(chunk.dtype, t.dtype)
+            # test chunk's sparse
+            self.assertFalse(chunk.issparse())
+            # test chunk's out
+            self.assertIs(chunk.op.out, t1_data.chunks[i].data)
+
+        # test add with out and where
+        t1 = tensor(self.raw_data1, chunk_size=(3, 4))
+        t1_data = t1.data
+        w1 = tensor(self.raw_data1 < 0.5, chunk_size=(3, 4))
+        w1_data = w1.data
+        t = add(t1, 10, out=t1, where=w1)
+
+        self.assertEqual(t.shape, self.raw_data1.shape)
+        self.assertEqual(t.dtype, self.raw_data1.dtype)
+        self.assertFalse(t.issparse())
+        self.assertIs(t.op.out, t1_data)
+        self.assertIs(t.op.where, w1_data)
+
+        t.tiles()
+
+        self.assertEqual(t.shape, self.raw_data1.shape)
+        self.assertEqual(t.chunk_shape, (3, 2))
+        self.assertEqual(t.dtype, self.raw_data1.dtype)
+        self.assertFalse(t.issparse())
+
+        for i, chunk in enumerate(t.chunks):
+            # test chunk's inputs
+            self.assertEqual(len(chunk.inputs), 4)
+            self.assertIs(chunk.inputs[0], t1_data.chunks[i].data)
+            self.assertEqual(chunk.inputs[1].op.data, 10)
+            self.assertIs(chunk.inputs[0], chunk.inputs[2])  # out is input
+            self.assertIs(chunk.inputs[3], chunk.op.where)
+            # test chunk's index
+            self.assertEqual(chunk.index, t1.chunks[i].index)
+            # test chunk's dtype
+            self.assertEqual(chunk.dtype, t.dtype)
+            # test chunk's sparse
+            self.assertFalse(chunk.issparse())
+            # test chunk's out
+            self.assertIs(chunk.op.out, t1_data.chunks[i].data)
+            # test chunk's where
+            self.assertIs(chunk.op.where, w1_data.chunks[i].data)
+
+    def testSparseExpr(self):
+        # test add two sparse tensor
+        t1 = tensor(self.raw_sparse_data1, chunk_size=(3, 4))
+        t2 = tensor(self.raw_sparse_data2, chunk_size=4)
+        t = t1 + t2
+
+        self.assertEqual(t.shape, self.raw_sparse_data1.shape)
+        self.assertEqual(t.dtype, self.raw_sparse_data1.dtype)
+        self.assertTrue(t.issparse())
+
+        t.tiles()
+
+        self.assertEqual(t.shape, self.raw_sparse_data1.shape)
+        self.assertEqual(t.chunk_shape, (3, 2))
+        self.assertEqual(t.dtype, self.raw_sparse_data1.dtype)
         self.assertTrue(t.issparse())
 
         for i, chunk in enumerate(t.chunks):
@@ -228,15 +356,15 @@ class Test(TestBase):
         t2 = tensor(self.raw_data2, chunk_size=4)
         t = t1 + t2
 
-        self.assertEqual(t.shape, self.raw_data1.shape)
-        self.assertEqual(t.dtype, self.raw_data1.dtype)
+        self.assertEqual(t.shape, self.raw_sparse_data1.shape)
+        self.assertEqual(t.dtype, self.raw_sparse_data1.dtype)
         self.assertFalse(t.issparse())
 
         t.tiles()
 
-        self.assertEqual(t.shape, self.raw_data1.shape)
+        self.assertEqual(t.shape, self.raw_sparse_data1.shape)
         self.assertEqual(t.chunk_shape, (3, 2))
-        self.assertEqual(t.dtype, self.raw_data1.dtype)
+        self.assertEqual(t.dtype, self.raw_sparse_data1.dtype)
         self.assertFalse(t.issparse())
 
         for i, chunk in enumerate(t.chunks):
@@ -255,15 +383,15 @@ class Test(TestBase):
         t1 = tensor(self.raw_sparse_data1, chunk_size=(3, 4))
         t = t1 + 10
 
-        self.assertEqual(t.shape, self.raw_data1.shape)
-        self.assertEqual(t.dtype, self.raw_data1.dtype)
+        self.assertEqual(t.shape, self.raw_sparse_data1.shape)
+        self.assertEqual(t.dtype, self.raw_sparse_data1.dtype)
         self.assertFalse(t.issparse())
 
         t.tiles()
 
-        self.assertEqual(t.shape, self.raw_data1.shape)
+        self.assertEqual(t.shape, self.raw_sparse_data1.shape)
         self.assertEqual(t.chunk_shape, (3, 2))
-        self.assertEqual(t.dtype, self.raw_data1.dtype)
+        self.assertEqual(t.dtype, self.raw_sparse_data1.dtype)
         self.assertFalse(t.issparse())
 
         for i, chunk in enumerate(t.chunks):
@@ -281,15 +409,15 @@ class Test(TestBase):
         t1 = tensor(self.raw_sparse_data1, chunk_size=(3, 4))
         t = t1 + 0
 
-        self.assertEqual(t.shape, self.raw_data1.shape)
-        self.assertEqual(t.dtype, self.raw_data1.dtype)
+        self.assertEqual(t.shape, self.raw_sparse_data1.shape)
+        self.assertEqual(t.dtype, self.raw_sparse_data1.dtype)
         self.assertTrue(t.issparse())
 
         t.tiles()
 
-        self.assertEqual(t.shape, self.raw_data1.shape)
+        self.assertEqual(t.shape, self.raw_sparse_data1.shape)
         self.assertEqual(t.chunk_shape, (3, 2))
-        self.assertEqual(t.dtype, self.raw_data1.dtype)
+        self.assertEqual(t.dtype, self.raw_sparse_data1.dtype)
         self.assertTrue(t.issparse())
 
         for i, chunk in enumerate(t.chunks):
@@ -302,3 +430,73 @@ class Test(TestBase):
             self.assertEqual(chunk.dtype, t.dtype)
             # test chunk's sparse
             self.assertTrue(chunk.issparse())
+
+        # test add with out
+        t1 = tensor(self.raw_sparse_data1, chunk_size=(3, 4))
+        t1_data = t1.data
+        t = add(t1, 10, out=t1)
+
+        self.assertEqual(t.shape, self.raw_sparse_data1.shape)
+        self.assertEqual(t.dtype, self.raw_sparse_data1.dtype)
+        self.assertFalse(t.issparse())
+        self.assertIs(t.op.out, t1_data)
+
+        t.tiles()
+
+        self.assertEqual(t.shape, self.raw_sparse_data1.shape)
+        self.assertEqual(t.chunk_shape, (3, 2))
+        self.assertEqual(t.dtype, self.raw_sparse_data1.dtype)
+        self.assertFalse(t.issparse())
+
+        for i, chunk in enumerate(t.chunks):
+            # test chunk's inputs
+            self.assertEqual(len(chunk.inputs), 3)
+            self.assertIs(chunk.inputs[0], t1_data.chunks[i].data)
+            self.assertEqual(chunk.inputs[1].op.data, 10)
+            self.assertIs(chunk.inputs[0], chunk.inputs[2])  # out is input
+            # test chunk's index
+            self.assertEqual(chunk.index, t1.chunks[i].index)
+            # test chunk's dtype
+            self.assertEqual(chunk.dtype, t.dtype)
+            # test chunk's sparse
+            self.assertFalse(chunk.issparse())
+            # test chunk's out
+            self.assertIs(chunk.op.out, t1_data.chunks[i].data)
+
+        # test add with out and where
+        t1 = tensor(self.raw_sparse_data1, chunk_size=(3, 4))
+        t1_data = t1.data
+        w1 = tensor(self.raw_sparse_data1 < 0.5, chunk_size=(3, 4))
+        w1_data = w1.data
+        t = add(t1, 10, out=t1, where=w1)
+
+        self.assertEqual(t.shape, self.raw_sparse_data1.shape)
+        self.assertEqual(t.dtype, self.raw_sparse_data1.dtype)
+        self.assertFalse(t.issparse())
+        self.assertIs(t.op.out, t1_data)
+        self.assertIs(t.op.where, w1_data)
+
+        t.tiles()
+
+        self.assertEqual(t.shape, self.raw_sparse_data1.shape)
+        self.assertEqual(t.chunk_shape, (3, 2))
+        self.assertEqual(t.dtype, self.raw_sparse_data1.dtype)
+        self.assertFalse(t.issparse())
+
+        for i, chunk in enumerate(t.chunks):
+            # test chunk's inputs
+            self.assertEqual(len(chunk.inputs), 4)
+            self.assertIs(chunk.inputs[0], t1_data.chunks[i].data)
+            self.assertEqual(chunk.inputs[1].op.data, 10)
+            self.assertIs(chunk.inputs[0], chunk.inputs[2])  # out is input
+            self.assertIs(chunk.inputs[3], chunk.op.where)
+            # test chunk's index
+            self.assertEqual(chunk.index, t1.chunks[i].index)
+            # test chunk's dtype
+            self.assertEqual(chunk.dtype, t.dtype)
+            # test chunk's sparse
+            self.assertFalse(chunk.issparse())
+            # test chunk's out
+            self.assertIs(chunk.op.out, t1_data.chunks[i].data)
+            # test chunk's where
+            self.assertIs(chunk.op.where, w1_data.chunks[i].data)
