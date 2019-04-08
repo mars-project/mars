0660de252b406619aa23a661a1170b2846f57a77
Qin Xuye <chinekingseu@gmail.com>
2019-04-08 14:18:35 +0800

    Implement __array_function__ protocol (#332)

--- mars/tensor/core.py
+++ mars/tensor/core.py
@@ -248,6 +248,22 @@ class Tensor(Entity):
         else:
             return np.asarray(self.execute(), dtype=dtype)
 
+    def __array_function__(self, func, types, args, kwargs):
+        from .. import tensor as module
+
+        for submodule in func.__module__.split('.')[1:]:
+            try:
+                module = getattr(module, submodule)
+            except AttributeError:
+                return NotImplemented
+        if not hasattr(module, func.__name__):
+            return NotImplemented
+        mars_func = getattr(module, func.__name__)
+        if mars_func is func:
+            # avoid Numpy func
+            return NotImplemented
+        return mars_func(*args, **kwargs)
+
     def transpose(self, *axes):
         """
         Returns a view of the tensor with axes transposed.
