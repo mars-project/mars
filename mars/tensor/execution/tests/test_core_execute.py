54cf9626ec8a2546774706c3edd9c8b7edf5f2ad
继盛 <xuye.qin@alibaba-inc.com>
2019-04-08 12:02:41 +0800

    implement __array_function__ protocol

--- mars/tensor/execution/tests/test_core_execute.py
+++ mars/tensor/execution/tests/test_core_execute.py
@@ -16,6 +16,8 @@
 
 import unittest
 
+import numpy as np
+
 from mars.executor import Executor
 from mars.tensor import ones
 from mars.session import LocalSession, Session
@@ -40,3 +42,13 @@ class Test(unittest.TestCase):
         del b
         # decref called
         self.assertEqual(len(self.executor.chunk_result), 0)
+
+    def testArrayFunction(self):
+        a = ones((10, 20), chunk_size=5)
+
+        # test sum
+        self.assertEqual(np.sum(a).execute(), 200)
+
+        # test qr
+        q, r = np.linalg.qr(a)
+        self.assertTrue(np.allclose(np.dot(q, r), a).execute())
