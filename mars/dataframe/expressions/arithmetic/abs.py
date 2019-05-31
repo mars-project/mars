82be8a59d144631b0400eadfd5d7bafe72357676
Tao He <linzhu.ht@alibaba-inc.com>
2019-05-31 20:00:15 +0800

    Remve unused imports.
    Signed-off-by: Tao He <linzhu.ht@alibaba-inc.com>

--- mars/dataframe/expressions/arithmetic/abs.py
+++ mars/dataframe/expressions/arithmetic/abs.py
@@ -15,7 +15,6 @@
 import operator
 
 from .... import opcodes as OperandDef
-from ....serialize import AnyField, Float64Field
 from ....utils import classproperty
 from ..core import DataFrameOperand
 from .core import DataFrameUnaryOpMixin
