dd58811f5f3aa132e33b2d6c31804a67026ff5de
wenjun.swj <wenjun.swj@alibaba-inc.com>
2019-06-04 14:02:47 +0800

    Account for procedural memory in fuses

--- mars/tests/test_executor.py
+++ mars/tests/test_executor.py
@@ -82,6 +82,43 @@ class Test(unittest.TestCase):
             self.assertIsNone(actor.send(1))
 
     def testMockExecuteSize(self):
+        import mars.tensor as mt
+        from mars.graph import DAG
+        from mars.tensor.expressions.fetch import TensorFetch
+        from mars.tensor.expressions.arithmetic import TensorTreeAdd
+
+        graph_add = DAG()
+        input_chunks = []
+        for _ in range(2):
+            fetch_op = TensorFetch(dtype=np.dtype('int64'))
+            inp_chunk = fetch_op.new_chunk(None, shape=(100, 100)).data
+            input_chunks.append(inp_chunk)
+
+        add_op = TensorTreeAdd(dtype=np.dtype('int64'))
+        add_chunk = add_op.new_chunk(input_chunks, shape=(100, 100), dtype=np.dtype('int64')).data
+        graph_add.add_node(add_chunk)
+        for inp_chunk in input_chunks:
+            graph_add.add_node(inp_chunk)
+            graph_add.add_edge(inp_chunk, add_chunk)
+
+        executor = Executor()
+        res = executor.execute_graph(graph_add, [add_chunk.key], compose=False, mock=True)[0]
+        self.assertEqual(res, (80000, 80000))
+        self.assertEqual(executor.mock_max_memory, 80000)
+
+        for _ in range(3):
+            new_add_op = TensorTreeAdd(dtype=np.dtype('int64'))
+            new_add_chunk = new_add_op.new_chunk([add_chunk], shape=(100, 100), dtype=np.dtype('int64')).data
+            graph_add.add_node(new_add_chunk)
+            graph_add.add_edge(add_chunk, new_add_chunk)
+
+            add_chunk = new_add_chunk
+
+        executor = Executor()
+        res = executor.execute_graph(graph_add, [add_chunk.key], compose=False, mock=True)[0]
+        self.assertEqual(res, (80000, 80000))
+        self.assertEqual(executor.mock_max_memory, 160000)
+
         a = mt.random.rand(10, 10, chunk_size=10)
         b = a[:, mt.newaxis, :] - a
         r = mt.triu(mt.sqrt(b ** 2).sum(axis=2))
@@ -90,4 +127,4 @@ class Test(unittest.TestCase):
         res = executor.execute_tensor(r, concat=False, mock=True)
         # larger than maximal memory size in calc procedure
         self.assertGreaterEqual(res[0][0], 800)
-        self.assertGreaterEqual(res[0][1], 8000)
+        self.assertGreaterEqual(executor.mock_max_memory, 8000)
