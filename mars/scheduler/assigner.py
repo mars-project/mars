mars-project/mars

commit ba17e81ad2af965a87b8b5363174f0bb8148b864

Try applying placement order (#498)

mars/worker/execution.py
@@ -560,7 +560,6 @@ class ExecutionActor(WorkerActor):
 
         @log_unhandled
         def _handle_success(*_):
-            self._notify_successors(session_id, graph_key)
             self._invoke_finish_callbacks(session_id, graph_key)
 
         @log_unhandled
@@ -811,7 +810,8 @@ class ExecutionActor(WorkerActor):
         if self._daemon_ref is not None and not self._daemon_ref.is_actor_process_alive(raw_inproc_ref):
             raise WorkerProcessStopped
 
-        def _cache_result(*_):
+        def _cache_result(result_sizes):
+            save_sizes.update(result_sizes)
             self._result_cache[(session_id, graph_key)] = GraphResultRecord(save_sizes)
 
         if not send_addresses:
@@ -819,7 +819,8 @@ class ExecutionActor(WorkerActor):
             logger.debug('Worker graph %s(%s) finished execution. Dumping results into plasma...',
                          graph_key, graph_record.op_string)
             return inproc_ref.dump_cache(session_id, calc_keys, _promise=True) \
-                .then(_cache_result)
+                .then(_cache_result) \
+                .then(lambda *_: self._notify_successors(session_id, graph_key))
         else:
             # dump keys into shared memory and send
             all_addresses = [{v} if isinstance(v, six.string_types) else set(v)
@@ -833,10 +834,10 @@ class ExecutionActor(WorkerActor):
                                      if k in save_sizes)
 
             return inproc_ref.dump_cache(session_id, calc_keys, _promise=True) \
-                .then(save_sizes.update) \
+                .then(_cache_result) \
+                .then(lambda *_: self._notify_successors(session_id, graph_key)) \
                 .then(lambda *_: functools.partial(self._do_active_transfer,
-                                                   session_id, graph_key, data_to_addresses)) \
-                .then(_cache_result)
+                                                   session_id, graph_key, data_to_addresses))
 
     def _cleanup_graph(self, session_id, graph_key):
         """

mars/api.py
@@ -15,6 +15,7 @@
 import logging
 
 from .actors import new_client
+from .errors import GraphNotExists
 from .scheduler import SessionActor, GraphActor, GraphMetaActor, ResourceActor, \
     SessionManagerActor, ChunkMetaClient
 from .scheduler.graph import ResultReceiverActor
@@ -63,10 +64,12 @@ class MarsAPI(object):
     def delete_session(self, session_id):
         self.session_manager.delete_session(session_id)
 
-    def submit_graph(self, session_id, serialized_graph, graph_key, target, compose=True):
+    def submit_graph(self, session_id, serialized_graph, graph_key, target,
+                     compose=True, wait=True):
         session_uid = SessionActor.gen_uid(session_id)
         session_ref = self.get_actor_ref(session_uid)
-        session_ref.submit_tileable_graph(serialized_graph, graph_key, target, compose=compose, _tell=True)
+        session_ref.submit_tileable_graph(
+            serialized_graph, graph_key, target, compose=compose, _tell=not wait)
 
     def delete_graph(self, session_id, graph_key):
         graph_uid = GraphActor.gen_uid(session_id, graph_key)
@@ -90,7 +93,7 @@ class MarsAPI(object):
             state_obj = graph_meta_ref.get_state()
             state = state_obj.value if state_obj else 'preparing'
         else:
-            state = 'preparing'
+            raise GraphNotExists
         state = GraphState(state.lower())
         return state
 

mars/scheduler/analyzer.py
@@ -278,7 +278,7 @@ class GraphAnalyzer(object):
         """
         graph = self._graph
         op_states = self._op_states
-        cur_assigns = self._fixed_assigns.copy()
+        cur_assigns = OrderedDict(self._fixed_assigns)
 
         key_to_chunks = defaultdict(list)
         for n in graph:
@@ -359,7 +359,8 @@ class GraphAnalyzer(object):
             self._assign_by_bfs(cur, worker, worker_quotas, spread_ranges, op_keys,
                                 cur_assigns, graph=graph)
 
-        return dict((n.op.key, cur_assigns[n.op.key]) for n in chunks_to_assign)
+        keys_to_assign = set(n.op.key for n in chunks_to_assign)
+        return OrderedDict((k, v) for k, v in cur_assigns.items() if k in keys_to_assign)
 
     def analyze_state_changes(self):
         """

mars/scheduler/graph.py
@@ -564,7 +564,8 @@ class GraphActor(SchedulerActor):
         if analyzer is None:
             analyzer = GraphAnalyzer(chunk_graph, self._get_worker_slots())
         assignments = analyzer.calc_operand_assignments(op_keys, input_chunk_metas=input_chunk_metas)
-        for k, v in assignments.items():
+        for idx, (k, v) in enumerate(assignments.items()):
+            operand_infos[k]['optimize']['placement_order'] = idx
             operand_infos[k]['target_worker'] = v
         return assignments
 
@@ -1029,7 +1030,11 @@ class GraphActor(SchedulerActor):
 
         graph = self.get_chunk_graph()
         new_states = dict()
-        analyzer = GraphAnalyzer(graph, worker_slots, fixed_assigns, graph_states, lost_chunks)
+        ordered_states = OrderedDict(
+            sorted(((k, v) for k, v in graph_states.items()),
+                   key=lambda d: operand_infos[d[0]]['optimize'].get('placement_order', 0))
+        )
+        analyzer = GraphAnalyzer(graph, worker_slots, fixed_assigns, ordered_states, lost_chunks)
         if removes or lost_chunks:
             new_states = analyzer.analyze_state_changes()
             logger.debug('%d chunks lost. %d operands changed state.', len(lost_chunks),

mars/web/api.py
@@ -23,6 +23,7 @@ from tornado import gen, concurrent, web, ioloop
 
 from ..actors import new_client
 from ..compat import six, futures
+from ..errors import GraphNotExists
 from ..lib.tblib import pickling_support
 from ..serialize.dataserializer import CompressType
 from ..utils import to_str
@@ -111,7 +112,11 @@ class GraphApiHandler(ApiRequestHandler):
     def get(self, session_id, graph_key):
         from ..scheduler.utils import GraphState
 
-        state = self.web_api.get_graph_state(session_id, graph_key)
+        try:
+            state = self.web_api.get_graph_state(session_id, graph_key)
+        except GraphNotExists:
+            raise web.HTTPError(404, 'Graph not exists')
+
         if state == GraphState.RUNNING:
             self.write(json.dumps(dict(state='running')))
         elif state == GraphState.SUCCEEDED:

mars/web/server.py
@@ -19,6 +19,7 @@ import threading
 import os
 from collections import defaultdict
 
+import numpy as np
 from bokeh.application import Application
 from bokeh.application.handlers import FunctionHandler
 from bokeh.server.server import Server
@@ -44,7 +45,7 @@ def get_jinja_env():
     )
 
     def format_ts(value):
-        if value is None:
+        if value is None or np.isnan(value):
             return None
         return datetime.fromtimestamp(value).strftime('%Y-%m-%d %H:%M:%S')
 

commit 8e82b8a81168c5bbc2f697891943447575245a31

Fix errors caused by #498

mars/scheduler/assigner.py
@@ -120,7 +120,10 @@ class AssignerActor(SchedulerActor):
         :param op_key: operand key
         :param priority_data: new priority data
         """
-        self._req_heap.update_priority((session_id, op_key), self._extract_op_priority(priority_data))
+        try:
+            self._req_heap.update_priority((session_id, op_key), self._extract_op_priority(priority_data))
+        except KeyError:
+            pass
 
     def update_target_workers(self, session_id, keys_to_workers):
         for op_key, target_worker in keys_to_workers.items():

mars/worker/execution.py
@@ -714,8 +714,7 @@ class ExecutionActor(WorkerActor):
             logger.debug('Worker graph %s(%s) finished execution. Dumping results into plasma...',
                          graph_key, graph_record.op_string)
             return inproc_ref.dump_cache(session_id, calc_keys, _promise=True) \
-                .then(_cache_result) \
-                .then(lambda *_: self._notify_successors(session_id, graph_key))
+                .then(_cache_result)
         else:
             # dump keys into shared memory and send
             all_addresses = [{v} if isinstance(v, six.string_types) else set(v)
@@ -730,7 +729,6 @@ class ExecutionActor(WorkerActor):
 
             return inproc_ref.dump_cache(session_id, calc_keys, _promise=True) \
                 .then(_cache_result) \
-                .then(lambda *_: self._notify_successors(session_id, graph_key)) \
                 .then(lambda *_: functools.partial(self._do_active_transfer,
                                                    session_id, graph_key, data_to_addresses))
 