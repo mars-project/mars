8d1d5e8da8ca13ff55a74eb53299c437cf7158d1
继盛 <xuye.qin@alibaba-inc.com>
2019-07-17 15:27:44 +0800

    fix codacy

--- mars/tensor/rechunk/__init__.py
+++ mars/tensor/rechunk/__init__.py
@@ -11,3 +11,16 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from .rechunk import rechunk
+
+
+def _install():
+    from ..core import Tensor, TensorData
+
+    setattr(Tensor, 'rechunk', rechunk)
+    setattr(TensorData, 'rechunk', rechunk)
+
+
+_install()
+del _install
