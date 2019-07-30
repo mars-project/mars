f8568a653f18abf236eb3e3d6aaed5e9006d11b3
Tao He <sighingnow@gmail.com>
2019-07-30 12:16:47 +0800

    Backport patch from pyarrow. (#589)

--- mars/serialize/dataserializer.py
+++ mars/serialize/dataserializer.py
@@ -315,9 +315,11 @@ _serialize_context = None
 def mars_serialize_context():
     global _serialize_context
     if _serialize_context is None:
+        from ..compat import apply_pyarrow_serialization_patch
         ctx = pyarrow.default_serialization_context()
         ctx.register_type(SparseNDArray, 'mars.SparseNDArray',
                           custom_serializer=_serialize_sparse_csr_list,
                           custom_deserializer=_deserialize_sparse_csr_list)
+        apply_pyarrow_serialization_patch(ctx)
         _serialize_context = ctx
     return _serialize_context
