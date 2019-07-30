9344927894952c9174552c5f0c5208fef3112f85
Xuye (Chris) Qin <xuye.qin@alibaba-inc.com>
2019-07-30 16:13:59 +0800

    Fix build issue under cp27-cp27m-manylinux1 (#588) (#590)

--- bin/travis-build-wheels.sh
+++ bin/travis-build-wheels.sh
@@ -4,10 +4,19 @@ set -e -x
 # Install a system package required by our library
 yum install -y atlas-devel
 
-# Compile wheels
+# Install requirements
 PYBIN=/opt/python/${PYVER}/bin
-"${PYBIN}/pip" install -r /io/requirements-wheel.txt
+REQ_FILE=/io/requirements-wheel.txt
 
+# install packages without pandas first
+# todo remove this section when dropping support for Python 2.7
+REQ_FILE_NO_PD=/tmp/requirements-no-pd.txt
+grep -v "pandas" $REQ_FILE > $REQ_FILE_NO_PD
+"${PYBIN}/pip" install -r $REQ_FILE_NO_PD
+
+"${PYBIN}/pip" install -r $REQ_FILE
+
+# Compile wheels
 cd /io
 "${PYBIN}/python" setup.py bdist_wheel
 
