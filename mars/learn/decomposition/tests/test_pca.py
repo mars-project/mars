13a3f70c282394a8079b0b32b030b7c5a91ac88f
继盛 <xuye.qin@alibaba-inc.com>
2019-08-15 22:43:41 +0800

    add more ut

--- mars/learn/decomposition/tests/test_pca.py
+++ mars/learn/decomposition/tests/test_pca.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 import unittest
+from itertools import product
 
 import numpy as np
 
@@ -22,7 +23,7 @@ from mars.session import new_session
 try:
     import sklearn
     from sklearn import datasets
-    from sklearn.utils.testing import assert_array_almost_equal
+    from sklearn.utils.testing import assert_array_almost_equal, assert_almost_equal
 
     from ..pca import PCA
 except ImportError:
@@ -33,6 +34,8 @@ except ImportError:
 class Test(unittest.TestCase):
     def setUp(self):
         self.iris = mt.tensor(datasets.load_iris().data)
+        # solver_list not includes arpack
+        self.solver_list = ['full', 'randomized', 'auto']
         self.session = new_session().as_default()
 
     def testPCA(self):
@@ -61,3 +64,88 @@ class Test(unittest.TestCase):
         pca = PCA(svd_solver='full')
         pca.fit(X)
         np.testing.assert_allclose(pca.explained_variance_ratio_.sum(), 1.0, 3)
+
+    def testPCARandomizedSolver(self):
+        # PCA on dense arrays
+        X = self.iris
+
+        # Loop excluding the 0, invalid for randomized
+        for n_comp in np.arange(1, X.shape[1]):
+            pca = PCA(n_components=n_comp, svd_solver='randomized', random_state=0)
+
+            X_r = pca.fit(X).transform(X)
+            np.testing.assert_equal(X_r.shape[1], n_comp)
+
+            X_r2 = pca.fit_transform(X)
+            assert_array_almost_equal(X_r.fetch(), X_r2.fetch())
+
+            X_r = pca.transform(X)
+            assert_array_almost_equal(X_r.fetch(), X_r2.fetch())
+
+            # Test get_covariance and get_precision
+            cov = pca.get_covariance()
+            precision = pca.get_precision()
+            assert_array_almost_equal(mt.dot(cov, precision).execute(),
+                                      mt.eye(X.shape[1]).execute(), 12)
+
+        pca = PCA(n_components=0, svd_solver='randomized', random_state=0)
+        with self.assertRaises(ValueError):
+            pca.fit(X)
+
+        pca = PCA(n_components=0, svd_solver='randomized', random_state=0)
+        with self.assertRaises(ValueError):
+            pca.fit(X)
+        # Check internal state
+        self.assertEqual(pca.n_components,
+                         PCA(n_components=0,
+                             svd_solver='randomized', random_state=0).n_components)
+        self.assertEqual(pca.svd_solver,
+                         PCA(n_components=0,
+                             svd_solver='randomized', random_state=0).svd_solver)
+
+    def testWhitening(self):
+        # Check that PCA output has unit-variance
+        rng = np.random.RandomState(0)
+        n_samples = 100
+        n_features = 80
+        n_components = 30
+        rank = 50
+
+        # some low rank data with correlated features
+        X = mt.dot(rng.randn(n_samples, rank),
+                   mt.dot(mt.diag(mt.linspace(10.0, 1.0, rank)),
+                          rng.randn(rank, n_features)))
+        # the component-wise variance of the first 50 features is 3 times the
+        # mean component-wise variance of the remaining 30 features
+        X[:, :50] *= 3
+
+        self.assertEqual(X.shape, (n_samples, n_features))
+
+        # the component-wise variance is thus highly varying:
+        self.assertGreater(X.std(axis=0).std().execute(), 43.8)
+
+        for solver, copy in product(self.solver_list, (True, False)):
+            # whiten the data while projecting to the lower dim subspace
+            X_ = X.copy()  # make sure we keep an original across iterations.
+            pca = PCA(n_components=n_components, whiten=True, copy=copy,
+                      svd_solver=solver, random_state=0, iterated_power=7)
+            # test fit_transform
+            X_whitened = pca.fit_transform(X_.copy())
+            self.assertEqual(X_whitened.shape, (n_samples, n_components))
+            X_whitened2 = pca.transform(X_)
+            assert_array_almost_equal(X_whitened.fetch(), X_whitened2.fetch())
+
+            assert_almost_equal(X_whitened.std(ddof=1, axis=0).execute(),
+                                np.ones(n_components),
+                                decimal=6)
+            assert_almost_equal(X_whitened.mean(axis=0).execute(), np.zeros(n_components))
+
+            X_ = X.copy()
+            pca = PCA(n_components=n_components, whiten=False, copy=copy,
+                      svd_solver=solver).fit(X_)
+            X_unwhitened = pca.transform(X_)
+            self.assertEqual(X_unwhitened.shape, (n_samples, n_components))
+
+            # in that case the output components still have varying variances
+            assert_almost_equal(X_unwhitened.std(axis=0).std().execute(), 74.1, 1)
+            # we always center, so no test for non-centering.
