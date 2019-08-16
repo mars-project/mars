e83aa4ad17633b99926beb21dc102552e5295c02
继盛 <xuye.qin@alibaba-inc.com>
2019-08-16 16:52:49 +0800

    remove unsed function

--- mars/learn/utils/validation.py
+++ mars/learn/utils/validation.py
@@ -60,38 +60,6 @@ def _ensure_no_complex_data(array):
                          "{}\n".format(array))
 
 
-# Use at least float64 for the accumulating functions to avoid precision issue
-# see https://github.com/numpy/numpy/issues/9393. The float64 is also retained
-# as it is in case the float overflows
-def _safe_accumulator_op(op, x, *args, **kwargs):
-    """
-    This function provides numpy accumulator functions with a float64 dtype
-    when used on a floating point input. This prevents accumulator overflow on
-    smaller floating point dtypes.
-
-    Parameters
-    ----------
-    op : function
-        A numpy accumulator function such as np.mean or np.sum
-    x : numpy array
-        A numpy array to apply the accumulator function
-    *args : positional arguments
-        Positional arguments passed to the accumulator function after the
-        input x
-    **kwargs : keyword arguments
-        Keyword arguments passed to the accumulator function
-
-    Returns
-    -------
-    result : The output of the accumulator function passed to this function
-    """
-    if np.issubdtype(x.dtype, np.floating) and x.dtype.itemsize < 8:
-        result = op(x, *args, **kwargs, dtype=np.float64)
-    else:
-        result = op(x, *args, **kwargs)
-    return result
-
-
 def _ensure_sparse_format(spmatrix, accept_sparse, dtype, copy,
                           force_all_finite, accept_large_sparse):
     """Convert a sparse matrix to a given format.
