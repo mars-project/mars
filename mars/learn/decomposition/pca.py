8c4bf21175cf52ccbd6b8b7e298d327440c283a1
继盛 <xuye.qin@alibaba-inc.com>
2019-08-21 23:28:20 +0800

    add truncated_svd support

--- mars/learn/decomposition/pca.py
+++ mars/learn/decomposition/pca.py
@@ -265,35 +265,26 @@ class PCA(_BasePCA):
 
     Examples
     --------
-    >>> import numpy as np
-    >>> from sklearn.decomposition import PCA
-    >>> X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+    >>> import mars.tensor as mt
+    >>> from mars.learn.decomposition import PCA
+    >>> X = mt.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
     >>> pca = PCA(n_components=2)
     >>> pca.fit(X)  # doctest: +NORMALIZE_WHITESPACE
     PCA(copy=True, iterated_power='auto', n_components=2, random_state=None,
       svd_solver='auto', tol=0.0, whiten=False)
-    >>> print(pca.explained_variance_ratio_)  # doctest: +ELLIPSIS
+    >>> print(pca.explained_variance_ratio_.execute())  # doctest: +ELLIPSIS
     [0.9924... 0.0075...]
-    >>> print(pca.singular_values_)  # doctest: +ELLIPSIS
+    >>> print(pca.singular_values_.execute())  # doctest: +ELLIPSIS
     [6.30061... 0.54980...]
 
     >>> pca = PCA(n_components=2, svd_solver='full')
     >>> pca.fit(X)                 # doctest: +ELLIPSIS +NORMALIZE_WHITESPACE
     PCA(copy=True, iterated_power='auto', n_components=2, random_state=None,
       svd_solver='full', tol=0.0, whiten=False)
-    >>> print(pca.explained_variance_ratio_)  # doctest: +ELLIPSIS
+    >>> print(pca.explained_variance_ratio_.execute())  # doctest: +ELLIPSIS
     [0.9924... 0.00755...]
-    >>> print(pca.singular_values_)  # doctest: +ELLIPSIS
+    >>> print(pca.singular_values_.execute())  # doctest: +ELLIPSIS
     [6.30061... 0.54980...]
-
-    >>> pca = PCA(n_components=1, svd_solver='arpack')
-    >>> pca.fit(X)  # doctest: +NORMALIZE_WHITESPACE
-    PCA(copy=True, iterated_power='auto', n_components=1, random_state=None,
-      svd_solver='arpack', tol=0.0, whiten=False)
-    >>> print(pca.explained_variance_ratio_)  # doctest: +ELLIPSIS
-    [0.99244...]
-    >>> print(pca.singular_values_)  # doctest: +ELLIPSIS
-    [6.30061...]
 
     See also
     --------
