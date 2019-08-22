3a1e9b5d5ce04474750a0cbb0208a069d98fbf7e
Xuye (Chris) Qin <xuye.qin@alibaba-inc.com>
2019-08-22 09:02:04 +0800

    Add truncated_svd support (#686)
    (cherry picked from commit 64351875492dc9a4bab087e5d20ae66ac404c7d6)

--- mars/learn/decomposition/__init__.py
+++ mars/learn/decomposition/__init__.py
@@ -11,3 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from .pca import PCA
+from .truncated_svd import TruncatedSVD
