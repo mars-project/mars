4988013906677cd84becc605596262cf218757d0
wdkwyf <441545218@qq.com>
2019-08-27 16:00:42 +0800

    fix ci error

--- mars/tensor/tests/test_core_execute.py
+++ mars/tensor/tests/test_core_execute.py
@@ -48,11 +48,11 @@ class Test(unittest.TestCase):
         a = ones((10, 20), chunk_size=5)
 
         # test sum
-        self.assertEqual(np.sum(a).execute(), 200)
+        self.assertEqual(np.sum(a).execute('numpy'), 200)
 
         # test qr
         q, r = np.linalg.qr(a)
-        self.assertTrue(np.allclose(np.dot(q, r), a).execute())
+        self.assertTrue(np.allclose(np.dot(q, r), a).execute('numpy'))
 
     def testViewDataOnSlice(self):
         data = np.random.rand(10, 20)
