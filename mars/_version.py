d39579a1ed13129714cbf533d8768fd5f38a6580
Xuye (Chris) Qin <xuye.qin@alibaba-inc.com>
2019-09-21 08:34:42 +0800

    Bump version to v0.2.1 (#761)
    * Implementation of `Dataframe.merge/join` by shuffle (#574)
    
    (cherry picked from commit 2a40684fc7782e8576519375caa9a7755ddf6de4)
    
    * regenerate operand_pb2.py
    
    * bump version

--- mars/_version.py
+++ mars/_version.py
@@ -16,7 +16,7 @@ import subprocess
 import os
 import sys
 
-version_info = (0, 2, 0)
+version_info = (0, 2, 1)
 _num_index = max(idx if isinstance(v, int) else 0
                  for idx, v in enumerate(version_info))
 __version__ = '.'.join(map(str, version_info[:_num_index + 1])) + \
