8a996764641b2e080908c9282f05f71dae349d06
He Kaisheng <heks93@163.com>
2019-10-30 20:25:58 +0800

    Support conversion between series and tensor (#791)
    (cherry picked from commit 65e65f6fce030b475f8b4bca67f6a96fa3537460)

--- mars/dataframe/__init__.py
+++ mars/dataframe/__init__.py
@@ -16,7 +16,7 @@
 
 from .initializer import DataFrame, Series
 # do imports to register operands
-from .datasource.from_tensor import from_tensor
+from .datasource.from_tensor import dataframe_from_tensor
 from .datasource.from_records import from_records
 from .utils import concat_tileable_chunks, get_fetch_op_cls, get_fuse_op_cls
 from .fetch import DataFrameFetch, DataFrameFetchShuffle
