4b984c64b2fdc8bb18eff18557f9a3e1fd3fe11c
hekaisheng <kaisheng.hks@alibaba-inc.com>
2019-11-11 12:14:04 +0800

    rename columns in sort_dataframe_result

--- mars/utils.py
+++ mars/utils.py
@@ -610,8 +610,8 @@ def sort_dataframe_result(df, result):
     if hasattr(df, 'index_value'):
         if getattr(df.index_value, 'should_be_monotonic', False):
             result.sort_index(inplace=True)
-        if hasattr(df, 'columns'):
-            if getattr(df.columns, 'should_be_monotonic', False):
+        if hasattr(df, 'columns_value'):
+            if getattr(df.columns_value, 'should_be_monotonic', False):
                 result.sort_index(axis=1, inplace=True)
     return result
 
