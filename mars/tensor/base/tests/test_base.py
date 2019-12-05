5618ca3eec619767c98f6fed3aa5af3f48f1cd30
hekaisheng <kaisheng.hks@alibaba-inc.com>
2019-12-05 12:09:34 +0800

    support einsum

--- mars/tensor/base/tests/test_base.py
+++ mars/tensor/base/tests/test_base.py
@@ -23,7 +23,7 @@ from mars.tensor.datasource import ones, tensor, arange, array, asarray, \
 from mars.tensor.base import transpose, broadcast_to, where, argwhere, array_split, \
     split, squeeze, result_type, repeat, copyto, isin, moveaxis, TensorCopyTo, \
     atleast_1d, atleast_2d, atleast_3d, ravel, searchsorted, unique, sort, \
-    partition, to_gpu, to_cpu
+    partition, to_gpu, to_cpu, einsum
 from mars.tensor.base.searchsorted import Stage
 from mars.tiles import get_tiled
 
@@ -795,3 +795,48 @@ class Test(unittest.TestCase):
 
         with self.assertRaises(ValueError):
             partition(np.random.rand(10), [-11, 2])
+
+    def testEinsum(self):
+        data1 = np.random.rand(3, 4, 5)
+        data2 = np.random.rand(4, 3, 2)
+
+        t1 = tensor(data1, chunk_size=2)
+        t2 = tensor(data2, chunk_size=3)
+        t = einsum('ijk, jil -> kl', t1, t2)
+
+        self.assertEqual(t.shape, (5, 2))
+
+        t = t.tiles()
+        self.assertEqual(len(t.chunks), 3)
+
+        # multiply(data1, data2)
+        data1 = np.random.rand(6, 6)
+        data2 = np.random.rand(6, 6)
+        t1 = tensor(data1, chunk_size=3)
+        t2 = tensor(data2, chunk_size=3)
+        t = einsum('..., ...', t1, t2)
+
+        self.assertEqual(t.shape, (6, 6))
+
+        t = t.tiles()
+        self.assertEqual(len(t.chunks), 4)
+
+        t = einsum('..., ...', t1, t2, optimize=True)
+        self.assertEqual(t.op.optimize, ['einsum_path', (0, 1)])
+
+        # test broadcast
+        data1 = np.random.rand(1, 10, 9)
+        data2 = np.random.rand(9, 6)
+        data3 = np.random.rand(10, 6)
+        data4 = np.random.rand(8,)
+
+        t1 = tensor(data1, chunk_size=(1, (5, 5), (3, 3, 3)))
+        t2 = tensor(data2, chunk_size=((3, 3, 3), (3, 3)))
+        t3 = tensor(data3, chunk_size=((6, 4), (4, 2)))
+        t4 = tensor(data4, chunk_size=3)
+        t = einsum('ajk,kl,jl,a->a', t1, t2, t3, t4, optimize=True)
+
+        self.assertEqual(t.shape, (8,))
+
+        t = t.tiles()
+        self.assertEqual(len(t.chunks), 3)
