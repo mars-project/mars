f8b08a4110199be2663f256e0187d36873810474
wenjun.swj <wenjun.swj@alibaba-inc.com>
2020-01-02 14:58:46 +0800

    Replace gevent in etcd

--- mars/cluster_info.py
+++ mars/cluster_info.py
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import asyncio
 import logging
 import os
 
@@ -48,7 +49,7 @@ class StaticSchedulerDiscoverer(object):
     def __reduce__(self):
         return type(self), (self._schedulers,)
 
-    def get(self):
+    async def get(self):
         return self._schedulers
 
     def watch(self):
@@ -71,15 +72,24 @@ class KVStoreSchedulerDiscoverer(object):
     def _resolve_schedulers(result):
         return [to_str(s.key.rsplit('/', 1)[1]) for s in result.children]
 
-    def get(self):
+    async def get(self):
         try:
             return self._resolve_schedulers(self._client.read(SCHEDULER_PATH))
         except KeyError:
-            return next(self.watch())
+            return self._resolve_schedulers(await self._client.watch(SCHEDULER_PATH))
 
     def watch(self):
-        for result in self._client.eternal_watch(SCHEDULER_PATH):
-            yield self._resolve_schedulers(result)
+        this = self
+        ait = self._client.eternal_watch(SCHEDULER_PATH).__aiter__()
+
+        class _AsyncIterator(object):
+            def __aiter__(self):
+                return self
+
+            async def __anext__(self):
+                return this._resolve_schedulers(await ait.__anext__())
+
+        return _AsyncIterator()
 
 
 class _ClusterInfoWatchActor(FunctionActor):
@@ -90,11 +100,11 @@ class _ClusterInfoWatchActor(FunctionActor):
     def post_create(self):
         self._cluster_info_ref = self.ctx.actor_ref(self._cluster_info_ref)
 
-    def get_schedulers(self):
-        return self._discoverer.get()
+    async def get_schedulers(self):
+        return await self._discoverer.get()
 
     def watch(self):
-        for schedulers in self._discoverer.watch():
+        async for schedulers in self._discoverer.watch():
             self._cluster_info_ref.set_schedulers(schedulers, _tell=True)
 
 
@@ -135,14 +145,16 @@ class ClusterInfoActor(FunctionActor):
     def get_schedulers(self):
         return self._schedulers
 
-    def set_schedulers(self, schedulers):
+    async def set_schedulers(self, schedulers):
         logger.debug('Setting schedulers %r', schedulers)
         self._schedulers = schedulers
         self._hash_ring = create_hash_ring(self._schedulers)
 
+        set_tasks = []
         for observer_ref, fun_name in self._observer_refs:
             # notify the observers to update the new scheduler list
-            getattr(observer_ref, fun_name)(schedulers, _tell=True)
+            set_tasks.append(getattr(observer_ref, fun_name)(schedulers, _tell=True))
+        await asyncio.wait(set_tasks)
 
     def get_scheduler(self, key, size=1):
         if len(self._schedulers) == 1 and size == 1:
@@ -173,13 +185,13 @@ class HasClusterInfoActor(PromiseActor):
         if len(schedulers) > 1:
             self._hash_ring = create_hash_ring(schedulers)
 
-    def set_cluster_info_ref(self, set_schedulers_fun_name=None):
+    async def set_cluster_info_ref(self, set_schedulers_fun_name=None):
         set_schedulers_fun_name = set_schedulers_fun_name or self.set_schedulers.__name__
 
         # cluster_info_actor is created when scheduler initialized
         self._cluster_info_ref = self.ctx.actor_ref(self.cluster_info_uid)
         # when some schedulers lost, notification will be received
-        self._cluster_info_ref.register_observer(
+        await self._cluster_info_ref.register_observer(
             self.ref(), set_schedulers_fun_name, _tell=True, _wait=False)
         if not self._schedulers:
             self.set_schedulers(self._cluster_info_ref.get_schedulers())
