68f32eb8ba7d905040ef82dcda44442b14fafdf6
Wenjun Si <wenjun.swj@alibaba-inc.com>
2020-01-02 16:22:24 +0800

    Skip preparing specified chunks when preparing for execution (#891)
    (cherry picked from commit 2455833bc36a4bb490204e6b73a03a40055d474b)

--- mars/operands.py
+++ mars/operands.py
@@ -75,6 +75,7 @@ class Operand(six.with_metaclass(OperandMetaclass, AttributeAsDictKey)):
     _create_view = BoolField('create_view')
 
     _inputs = ListField('inputs', ValueType.key)
+    _prepare_inputs = ListField('prepare_inputs', ValueType.bool)
     _outputs = ListField('outputs', ValueType.key, weak_ref=True)
 
     _stage = Int32Field('stage', on_serialize=lambda s: s.value if s is not None else s,
@@ -153,6 +154,13 @@ class Operand(six.with_metaclass(OperandMetaclass, AttributeAsDictKey)):
     @property
     def stage(self):
         return getattr(self, '_stage', None)
+
+    @property
+    def prepare_inputs(self):
+        val = getattr(self, '_prepare_inputs', None)
+        if not val:
+            return [True] * len(self.inputs or ())
+        return val
 
     @property
     def extra_params(self):
