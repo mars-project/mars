0a79c4c3020cd072a7f403b88b036c66fbf4147e
继盛 <xuye.qin@alibaba-inc.com>
2020-02-13 20:46:22 +0800

    add docs

--- mars/dataframe/datasource/read_sql_table.py
+++ mars/dataframe/datasource/read_sql_table.py
@@ -278,6 +278,69 @@ class DataFrameReadSQLTable(DataFrameOperand, DataFrameOperandMixin):
 def read_sql_table(table_name, con, schema=None, index_col=None, coerce_float=True,
                    parse_dates=None, columns=None, chunksize=None,
                    test_rows=5, chunk_size=None, engine_kwargs=None):
+    """
+    Read SQL database table into a DataFrame.
+
+    Given a table name and a SQLAlchemy connectable, returns a DataFrame.
+    This function does not support DBAPI connections.
+
+    Parameters
+    ----------
+    table_name : str
+        Name of SQL table in database.
+    con : SQLAlchemy connectable or str
+        A database URI could be provided as as str.
+        SQLite DBAPI connection mode not supported.
+    schema : str, default None
+        Name of SQL schema in database to query (if database flavor
+        supports this). Uses default schema if None (default).
+    index_col : str or list of str, optional, default: None
+        Column(s) to set as index(MultiIndex).
+    coerce_float : bool, default True
+        Attempts to convert values of non-string, non-numeric objects (like
+        decimal.Decimal) to floating point. Can result in loss of Precision.
+    parse_dates : list or dict, default None
+        - List of column names to parse as dates.
+        - Dict of ``{column_name: format string}`` where format string is
+          strftime compatible in case of parsing string times or is one of
+          (D, s, ns, ms, us) in case of parsing integer timestamps.
+        - Dict of ``{column_name: arg dict}``, where the arg dict corresponds
+          to the keyword arguments of :func:`pandas.to_datetime`
+          Especially useful with databases without native Datetime support,
+          such as SQLite.
+    columns : list, default None
+        List of column names to select from SQL table.
+    chunksize : int, default None
+        If specified, returns an iterator where `chunksize` is the number of
+        rows to include in each chunk.
+    test_rows: int, default 5
+        The number of rows to fetch for inferring dtypes.
+    chunk_size: : int or tuple of ints, optional
+        Specifies chunk size for each dimension.
+    engine_kwargs: dict, default None
+        Extra kwargs to pass to sqlalchemy.create_engine
+
+    Returns
+    -------
+    DataFrame
+        A SQL table is returned as two-dimensional data structure with labeled
+        axes.
+
+    See Also
+    --------
+    read_sql_query : Read SQL query into a DataFrame.
+    read_sql : Read SQL query or database table into a DataFrame.
+
+    Notes
+    -----
+    Any datetime values with time zone information will be converted to UTC.
+
+    Examples
+    --------
+    >>> import mars.dataframe as md
+    >>> md.read_sql_table('table_name', 'postgres:///db_name')  # doctest:+SKIP
+    """
+
     op = DataFrameReadSQLTable(table_name=table_name, con=con, schema=schema,
                                index_col=index_col, coerce_float=coerce_float,
                                parse_dates=parse_dates, columns=columns,
