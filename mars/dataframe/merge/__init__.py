313c823bd50e8ab8ade036c4bc459ecfffef9fd9
hekaisheng <kaisheng.hks@alibaba-inc.com>
2020-03-06 00:12:09 +0800

    support df.concat

--- mars/dataframe/merge/__init__.py
+++ mars/dataframe/merge/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .concat import DataFrameConcat
+from .concat import DataFrameConcat, concat
 from .merge import join, merge, DataFrameShuffleMerge, DataFrameMergeAlign
 from .append import DataFrameAppend, append
 
