4ac19186a97cbe826833eab332d1ce7b98e27df6
hekaisheng <kaisheng.hks@alibaba-inc.com>
2020-03-17 15:59:15 +0800

    fix in #962

--- mars/operands.py
+++ mars/operands.py
@@ -133,7 +133,8 @@ class Operand(six.with_metaclass(OperandMetaclass, AttributeAsDictKey)):
         return 1
 
     def get_dependent_data_keys(self):
-        return [dep.key for dep in self.inputs or ()]
+        return [dep.key for dep, has_dep
+                in zip(self.inputs or (), self.prepare_inputs) if has_dep]
 
     @property
     def gpu(self):
