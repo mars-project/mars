035f814acb6e7ced46b7c0faae5d12e7feb1cb5d
Xuye (Chris) Qin <xuye.qin@alibaba-inc.com>
2020-04-15 13:46:07 +0800

    Implements `mars.learn.metrics.pairwise.rbf_kernel` (#1158)

---
+++ mars/learn/metrics/pairwise/rbf_kernel.py
@@ -0,0 +1,51 @@
+# Copyright 1999-2020 Alibaba Group Holding Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from .... import tensor as mt
+from .core import PairwiseDistances
+from .euclidean import euclidean_distances
+
+
+def rbf_kernel(X, Y=None, gamma=None):
+    """
+    Compute the rbf (gaussian) kernel between X and Y::
+
+        K(x, y) = exp(-gamma ||x-y||^2)
+
+    for each pair of rows x in X and y in Y.
+
+    Read more in the :ref:`User Guide <rbf_kernel>`.
+
+    Parameters
+    ----------
+    X : tensor of shape (n_samples_X, n_features)
+
+    Y : tensor of shape (n_samples_Y, n_features)
+
+    gamma : float, default None
+        If None, defaults to 1.0 / n_features
+
+    Returns
+    -------
+    kernel_matrix : tensor of shape (n_samples_X, n_samples_Y)
+    """
+
+    X, Y = PairwiseDistances.check_pairwise_arrays(X, Y)
+    if gamma is None:
+        gamma = 1.0 / X.shape[1]
+
+    K = euclidean_distances(X, Y, squared=True)
+    K *= -gamma
+    K = mt.exp(K)
+    return K
