fb0cb3c908211eab71b8dc300b7c336c4a565f10
wjsi <swj0066@gmail.com>
2020-05-21 23:38:25 +0800

    Try fixing CI errors

--- mars/deploy/local/core.py
+++ mars/deploy/local/core.py
@@ -170,6 +170,10 @@ def gen_endpoint(address):
 
 
 def _start_cluster(endpoint, event, n_process=None, shared_memory=None, **kw):
+    modules = kw.pop('modules', None) or []
+    for m in modules:
+        __import__(m, globals(), locals(), [])
+
     cluster = LocalDistributedCluster(endpoint, n_process=n_process,
                                       shared_memory=shared_memory, **kw)
     cluster.start_service()
