3f0d19fa4102b85cfb00de10405173082354e5a3
Xuye (Chris) Qin <xuye.qin@alibaba-inc.com>
2020-05-29 10:26:41 +0800

    Support running tileables inside functions which spawned via mr.spawn (#1248)

--- mars/remote/__init__.py
+++ mars/remote/__init__.py
@@ -12,4 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# noinspection PyUnresolvedReferences
+from ..core import ExecutableTuple
 from .core import spawn
