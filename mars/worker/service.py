1926dffde6fb0b70c6389a2348e37d8655da3e95
Wenjun Si <wenjun.swj@alibaba-inc.com>
2020-06-05 21:29:04 +0800

    [BACKPORT] Fix failure for None-result spawn functions (#1276) (#1280)

--- mars/worker/service.py
+++ mars/worker/service.py
@@ -300,26 +300,28 @@ class WorkerService(object):
 
     def stop(self):
         try:
+            destroy_futures = []
             for actor in (self._cpu_calc_actors + self._sender_actors + self._inproc_holder_actors
                           + self._inproc_io_runner_actors + self._cuda_calc_actors
                           + self._cuda_holder_actors + self._receiver_actors + self._spill_actors
                           + self._process_helper_actors):
                 if actor and actor.ctx:
-                    actor.destroy(wait=False)
+                    destroy_futures.append(actor.destroy(wait=False))
 
             if self._result_sender_ref:
-                self._result_sender_ref.destroy(wait=False)
+                destroy_futures.append(self._result_sender_ref.destroy(wait=False))
             if self._status_ref:
-                self._status_ref.destroy(wait=False)
+                destroy_futures.append(self._status_ref.destroy(wait=False))
             if self._shared_holder_ref:
-                self._shared_holder_ref.destroy(wait=False)
+                destroy_futures.append(self._shared_holder_ref.destroy(wait=False))
             if self._storage_manager_ref:
-                self._storage_manager_ref.destroy(wait=False)
+                destroy_futures.append(self._storage_manager_ref.destroy(wait=False))
             if self._events_ref:
-                self._events_ref.destroy(wait=False)
+                destroy_futures.append(self._events_ref.destroy(wait=False))
             if self._dispatch_ref:
-                self._dispatch_ref.destroy(wait=False)
+                destroy_futures.append(self._dispatch_ref.destroy(wait=False))
             if self._execution_ref:
-                self._execution_ref.destroy(wait=False)
+                destroy_futures.append(self._execution_ref.destroy(wait=False))
+            [f.result(5) for f in destroy_futures]
         finally:
             self._plasma_store.__exit__(None, None, None)
