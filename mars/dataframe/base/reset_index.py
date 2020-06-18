adb4cd30f16e416cb041569a82592bfda35cf273
He Kaisheng <heks93@163.com>
2020-06-19 07:21:39 +0800

    Fix wrong columns value in `reset_index` (#1320)

--- mars/dataframe/base/reset_index.py
+++ mars/dataframe/base/reset_index.py
@@ -96,11 +96,15 @@ class DataFrameResetIndex(DataFrameOperand, DataFrameOperandMixin):
         out_df = op.outputs[0]
         added_columns_num = len(out_df.dtypes) - len(in_df.dtypes)
         out_chunks = []
-        is_range_index = out_df.index_value.has_value()
+        index_has_value = out_df.index_value.has_value()
+        chunk_has_nan = any(np.isnan(s) for s in in_df.nsplits[0])
         cum_range = np.cumsum((0, ) + in_df.nsplits[0])
         for c in in_df.chunks:
-            if is_range_index:
-                index_value = parse_index(pd.RangeIndex(cum_range[c.index[0]], cum_range[c.index[0] + 1]))
+            if index_has_value:
+                if chunk_has_nan:
+                    index_value = parse_index(pd.RangeIndex(-1))
+                else:
+                    index_value = parse_index(pd.RangeIndex(cum_range[c.index[0]], cum_range[c.index[0] + 1]))
             else:
                 index_value = out_df.index_value
             if c.index[1] == 0:
@@ -116,8 +120,9 @@ class DataFrameResetIndex(DataFrameOperand, DataFrameOperandMixin):
                 new_chunk = chunk_op.new_chunk([c], shape=c.shape, index_value=index_value,
                                                index=c.index, columns_value=c.columns_value, dtypes=c.dtypes)
             out_chunks.append(new_chunk)
-        if not is_range_index and isinstance(out_df.index_value._index_value, IndexValue.RangeIndex):
-            out_chunks = standardize_range_index(out_chunks)
+        if not index_has_value or chunk_has_nan:
+            if isinstance(out_df.index_value._index_value, IndexValue.RangeIndex):
+                out_chunks = standardize_range_index(out_chunks)
         new_op = op.copy()
         columns_splits = list(in_df.nsplits[1])
         columns_splits[0] += added_columns_num
@@ -187,7 +192,7 @@ class DataFrameResetIndex(DataFrameOperand, DataFrameOperandMixin):
             empty_df.index = a.index_value.to_pandas()[:0]
             empty_df = empty_df.reset_index(level=self.level, col_level=self.col_level, col_fill=self.col_fill)
             shape = (a.shape[0], len(empty_df.columns))
-            columns_value = parse_index(empty_df.columns)
+            columns_value = parse_index(empty_df.columns, store_data=True)
             dtypes = empty_df.dtypes
             index_value = self._get_out_index(empty_df, shape)
         return self.new_dataframe([a], shape=shape, columns_value=columns_value,
