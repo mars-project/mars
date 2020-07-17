c4d8f1f7e4f4704c455ed4258de744fc60c1e928
Wenjun Si <swj0066@gmail.com>
2020-07-17 15:03:57 +0800

    Patch bug in dependent keys

--- mars/operands.py
+++ mars/operands.py
@@ -549,7 +549,7 @@ class MapReduceOperand(Operand):
             inputs = self.inputs or ()
             deps = []
             for inp in inputs:
-                if isinstance(inp.op, ShuffleProxy):
+                if isinstance(inp.op, (ShuffleProxy, FetchShuffle)):
                     deps.extend([(chunk.key, self._shuffle_key) for chunk in inp.inputs or ()])
                 else:
                     deps.append(inp.key)
