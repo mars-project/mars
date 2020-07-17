88edc56376fcca2f5010f0ca0935582bc49603a0
hekaisheng <kaisheng.hks@alibaba-inc.com>
2020-07-17 14:32:39 +0800

    small fix for `is_object_dtype`

--- mars/utils.py
+++ mars/utils.py
@@ -1050,6 +1050,9 @@ class FixedSizeFileObject:
 
 
 def is_object_dtype(dtype):
-    return np.issubdtype(dtype, np.object_) \
-        or np.issubdtype(dtype, np.unicode_) \
-        or np.issubdtype(dtype, np.bytes_)
+    try:
+        return np.issubdtype(dtype, np.object_) \
+            or np.issubdtype(dtype, np.unicode_) \
+            or np.issubdtype(dtype, np.bytes_)
+    except TypeError:
+        return False
