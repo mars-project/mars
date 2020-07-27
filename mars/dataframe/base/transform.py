18981d2f582ae69dd965f3e18cadd27a1ffcdf9c
He Kaisheng <heks93@163.com>
2020-07-27 15:19:23 +0800

    [BACKPORT] Fix the error raised when inferring dtype in DataFrame.transform (#1424) (#1427)

--- mars/dataframe/base/transform.py
+++ mars/dataframe/base/transform.py
@@ -171,21 +171,35 @@ class TransformOperand(DataFrameOperand, DataFrameOperandMixin):
     def _infer_df_func_returns(self, in_dtypes, dtypes):
         if self.object_type == ObjectType.dataframe:
             empty_df = build_empty_df(in_dtypes, index=pd.RangeIndex(2))
-            with np.errstate(all='ignore'):
-                if self.call_agg:
-                    infer_df = empty_df.agg(self._func, axis=self._axis, *self.args, **self.kwds)
-                else:
-                    infer_df = empty_df.transform(self._func, axis=self._axis, *self.args, **self.kwds)
+            try:
+                with np.errstate(all='ignore'):
+                    if self.call_agg:
+                        infer_df = empty_df.agg(self._func, axis=self._axis, *self.args, **self.kwds)
+                    else:
+                        infer_df = empty_df.transform(self._func, axis=self._axis, *self.args, **self.kwds)
+            except:  # noqa: E722
+                infer_df = None
         else:
             empty_df = build_empty_series(in_dtypes[1], index=pd.RangeIndex(2), name=in_dtypes[0])
-            with np.errstate(all='ignore'):
-                if self.call_agg:
-                    infer_df = empty_df.agg(self._func, args=self.args, **self.kwds)
-                else:
-                    infer_df = empty_df.transform(self._func, convert_dtype=self.convert_dtype,
-                                                  args=self.args, **self.kwds)
+            try:
+                with np.errstate(all='ignore'):
+                    if self.call_agg:
+                        infer_df = empty_df.agg(self._func, args=self.args, **self.kwds)
+                    else:
+                        infer_df = empty_df.transform(self._func, convert_dtype=self.convert_dtype,
+                                                      args=self.args, **self.kwds)
+            except:  # noqa: E722
+                infer_df = None
 
-        if isinstance(infer_df, pd.DataFrame):
+        if infer_df is None and dtypes is None:
+            raise TypeError('Failed to infer dtype, please specify dtypes as arguments.')
+
+        if infer_df is None:
+            is_df = self.object_type == ObjectType.dataframe
+        else:
+            is_df = isinstance(infer_df, pd.DataFrame)
+
+        if is_df:
             new_dtypes = dtypes or infer_df.dtypes
             self._object_type = ObjectType.dataframe
         else:
