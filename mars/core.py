e61cfc3852a7da5a9ea531db8503c93832611193
继盛 <xuye.qin@alibaba-inc.com>
2020-08-04 19:33:46 +0800

    fix test_distributed_kmeans

--- mars/core.py
+++ mars/core.py
@@ -387,6 +387,8 @@ class _ExecuteAndFetchMixin:
     __slots__ = ()
 
     def _execute_and_fetch(self, session=None, **kw):
+        if session is None and len(self._executed_sessions) > 0:
+            session = self._executed_sessions[-1]
         try:
             # fetch first, to reduce the potential cost of submitting a graph
             return self.fetch(session=session)
