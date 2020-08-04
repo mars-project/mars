cf59c5d601c89cf6f9727dd5356379d0f5510667
wjsi <swj0066@gmail.com>
2020-08-04 23:22:45 +0800

    Fix coverage

--- mars/dataframe/arrays.py
+++ mars/dataframe/arrays.py
@@ -317,9 +317,9 @@ class ArrowStringArray(StringArrayBase):
 
     @classmethod
     def _concat_same_type(
-        cls, to_concat: Sequence["ArrowStringArray"]) -> "ArrowStringArray":
-        chunks = list(itertools.chain.from_iterable(x._arrow_array.chunks
-                                                    for x in to_concat))
+            cls, to_concat: Sequence["ArrowStringArray"]) -> "ArrowStringArray":
+        chunks = list(itertools.chain.from_iterable(
+            x._arrow_array.chunks for x in to_concat))
         if len(chunks) == 0:
             chunks = [pa.array([], type=pa.string())]
         return cls(pa.chunked_array(chunks))
