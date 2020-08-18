7e0511e7c8a9f39facb97488d364d350ffa8e113
Xuye (Chris) Qin <xuye.qin@alibaba-inc.com>
2020-08-18 21:47:29 +0800

    Fix `mt.isclose` when some of the arguments is scalar (#1498)
    (cherry picked from commit 9657ebd5d40195dec6f0c482451cf6469fed06e7)

--- mars/tensor/arithmetic/isclose.py
+++ mars/tensor/arithmetic/isclose.py
@@ -59,10 +59,13 @@ class TensorIsclose(TensorBinOp):
 
     @classmethod
     def execute(cls, ctx, op):
-        (a, b), device_id, xp = as_same_device(
+        inputs, device_id, xp = as_same_device(
             [ctx[c.key] for c in op.inputs], device=op.device, ret_extra=True)
 
         with device(device_id):
+            a = op.lhs if np.isscalar(op.lhs) else inputs[0]
+            b = op.rhs if np.isscalar(op.rhs) else inputs[-1]
+
             ctx[op.outputs[0].key] = xp.isclose(a, b, atol=op.atol, rtol=op.rtol,
                                                 equal_nan=op.equal_nan)
 
