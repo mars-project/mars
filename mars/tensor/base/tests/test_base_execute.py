f56c5e0e16c9456673242f7148c86733bb7139d7
yoshiera <huangjasper@126.com>
2020-08-20 16:34:37 +0800

    Update mt.split to support list and tuple: remove unused imports

--- mars/tensor/base/tests/test_base_execute.py
+++ mars/tensor/base/tests/test_base_execute.py
@@ -14,7 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from operator import concat
 import numpy as np
 import scipy.sparse as sps
 import pandas as pd
