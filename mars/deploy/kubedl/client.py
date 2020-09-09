93f0a4729f2b75bafe107c107d38c4fd954fd358
Wenjun Si <wenjun.swj@alibaba-inc.com>
2020-09-09 11:03:32 +0800

    Support running Mars under KubeDL (#1549)

---
+++ mars/deploy/kubedl/client.py
@@ -0,0 +1,234 @@
+# Copyright 1999-2020 Alibaba Group Holding Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import time
+import warnings
+
+import requests
+
+from ...session import new_session
+from .config import MarsSchedulerSpecConfig, MarsWorkerSpecConfig, MarsWebSpecConfig, \
+    MarsJobConfig
+
+try:
+    from kubernetes.client.rest import ApiException as K8SApiException
+except ImportError:  # pragma: no cover
+    K8SApiException = None
+
+KUBEDL_API_VERSION = 'kubedl.io/v1alpha1'
+KUBEDL_MARS_PLURAL = 'marsjobs'
+
+
+class KubeDLClusterClient:
+    def __init__(self, cluster):
+        self._cluster = cluster
+        self._endpoint = None
+        self._session = None
+
+    @property
+    def endpoint(self):
+        return self._endpoint
+
+    @property
+    def namespace(self):
+        return self._cluster.namespace
+
+    @property
+    def session(self):
+        return self._session
+
+    def start(self):
+        self._endpoint = self._cluster.start()
+        self._session = new_session(self._endpoint, verify_ssl=self._cluster.verify_ssl).as_default()
+
+    def stop(self, wait=False, timeout=0):
+        self._cluster.stop(wait=wait, timeout=timeout)
+
+
+class KubeDLCluster:
+    def __init__(self, kube_api_client=None, image=None, job_name=None, namespace=None,
+                 scheduler_num=1, scheduler_cpu=None, scheduler_mem=None,
+                 worker_num=1, worker_cpu=None, worker_mem=None, worker_spill_paths=None,
+                 worker_cache_mem=None, min_worker_num=None,
+                 web_num=1, web_cpu=None, web_mem=None,
+                 slb_endpoint=None, verify_ssl=True, timeout=None, **kwargs):
+        from kubernetes import client as kube_client
+        self._kube_api_client = kube_api_client
+        self._custom_api = kube_client.CustomObjectsApi(kube_api_client)
+
+        self._slb_endpoint = slb_endpoint.rstrip("/")
+        self._verify_ssl = verify_ssl
+
+        self._job_name = job_name
+        self._mars_endpoint = None
+        self._namespace = namespace or 'default'
+        self._image = image
+        self._timeout = timeout
+        self._extra_volumes = kwargs.pop('extra_volumes', ())
+        self._pre_stop_command = kwargs.pop('pre_stop_command', None)
+        self._log_when_fail = kwargs.pop('log_when_fail', False)
+        self._node_selectors = kwargs.pop('node_selectors', None)
+
+        extra_modules = kwargs.pop('extra_modules', None) or []
+        extra_modules = extra_modules.split(',') if isinstance(extra_modules, str) \
+            else extra_modules
+        extra_envs = kwargs.pop('extra_env', None) or dict()
+
+        def _override_modules(updates):
+            modules = set(extra_modules)
+            updates = updates.split(',') if isinstance(updates, str) \
+                else updates
+            modules.update(updates)
+            return sorted(modules)
+
+        def _override_envs(updates):
+            ret = extra_envs.copy()
+            ret.update(updates)
+            return ret
+
+        self._scheduler_num = scheduler_num
+        self._scheduler_cpu = scheduler_cpu
+        self._scheduler_mem = scheduler_mem
+        self._scheduler_extra_modules = _override_modules(kwargs.pop('scheduler_extra_modules', []))
+        self._scheduler_extra_env = _override_envs(kwargs.pop('scheduler_extra_env', None) or dict())
+
+        self._worker_num = worker_num
+        self._worker_cpu = worker_cpu
+        self._worker_mem = worker_mem
+        self._worker_spill_paths = worker_spill_paths
+        self._worker_cache_mem = worker_cache_mem
+        self._min_worker_num = min_worker_num or worker_num
+        self._worker_extra_modules = _override_modules(kwargs.pop('worker_extra_modules', []))
+        self._worker_extra_env = _override_envs(kwargs.pop('worker_extra_env', None) or dict())
+
+        self._web_num = web_num
+        self._web_cpu = web_cpu
+        self._web_mem = web_mem
+        self._web_extra_modules = _override_modules(kwargs.pop('web_extra_modules', []))
+        self._web_extra_env = _override_envs(kwargs.pop('web_extra_env', None) or dict())
+
+    @property
+    def verify_ssl(self):
+        return self._verify_ssl
+
+    def _create_service(self):
+        scheduler_cfg = MarsSchedulerSpecConfig(
+            self._image, self._scheduler_num, cpu=self._scheduler_cpu, memory=self._scheduler_mem,
+            node_selectors=self._node_selectors
+        )
+        worker_cfg = MarsWorkerSpecConfig(
+            self._image, self._worker_num, cpu=self._worker_cpu, memory=self._worker_mem,
+            cache_mem=self._worker_cache_mem, spill_dirs=self._worker_spill_paths,
+            node_selectors=self._node_selectors
+        )
+        web_cfg = MarsWebSpecConfig(
+            self._image, self._web_num, cpu=self._web_cpu, memory=self._web_mem,
+            node_selectors=self._node_selectors
+        )
+        job_cfg = MarsJobConfig(
+            job_name=self._job_name, scheduler_config=scheduler_cfg, worker_config=worker_cfg,
+            web_config=web_cfg, web_host=self._slb_endpoint
+        )
+
+        api, version = KUBEDL_API_VERSION.rsplit('/', 1)
+
+        cfg_json = job_cfg.build()
+        cfg_json['apiVersion'] = KUBEDL_API_VERSION
+
+        response = self._custom_api.create_namespaced_custom_object(
+            api, version, self._namespace, KUBEDL_MARS_PLURAL, cfg_json)
+        self._job_name = response['metadata']['name']
+
+    def _wait_service_ready(self):
+        self._mars_endpoint = f'{self._slb_endpoint}/mars/{self._namespace}/{self._job_name}-webservice-0'
+        check_start_time = time.time()
+        worker_count_url = self._mars_endpoint + '/api/worker?action=count'
+        while True:
+            try:
+                if self._timeout and time.time() - check_start_time > self._timeout:
+                    raise TimeoutError('Check Mars service start timeout')
+
+                if not self._verify_ssl:
+                    try:
+                        import urllib3
+                        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+                    except ImportError:  # pragma: no cover
+                        pass
+
+                api, version = KUBEDL_API_VERSION.rsplit('/', 1)
+                service_obj = self._custom_api.get_namespaced_custom_object_status(
+                    api, version, self._namespace, KUBEDL_MARS_PLURAL, self._job_name)
+                if len(service_obj.get('status', dict()).get('conditions', [])) > 0:
+                    if service_obj['status']['conditions'][-1]['type'] == 'Failed':
+                        raise SystemError(service_obj['status']['conditions'][-1]['message'])
+
+                with warnings.catch_warnings():
+                    warnings.filterwarnings('ignore', message='Unverified HTTPS request')
+                    resp = requests.get(worker_count_url, timeout=1, verify=self._verify_ssl)
+
+                if int(resp.text) >= self._min_worker_num:
+                    break
+            except (requests.Timeout, ValueError) as ex:
+                if not isinstance(ex, requests.Timeout):
+                    time.sleep(0.1)
+                pass
+
+    def start(self):
+        try:
+            self._create_service()
+            self._wait_service_ready()
+            return self._mars_endpoint
+        except:  # noqa: E722
+            self.stop()
+            raise
+
+    def stop(self, wait=False, timeout=0):
+        from kubernetes import client as kube_client
+        custom_api = kube_client.CustomObjectsApi(self._kube_api_client)
+        api, version = KUBEDL_API_VERSION.rsplit('/', 1)
+        custom_api.delete_namespaced_custom_object(
+            api, version, self._namespace, KUBEDL_MARS_PLURAL, self._job_name)
+
+        if wait:
+            start_time = time.time()
+            while True:
+                try:
+                    custom_api.get_namespaced_custom_object(
+                        api, version, self._namespace, KUBEDL_MARS_PLURAL, self._job_name)
+                except K8SApiException as ex:
+                    if ex.status != 404:  # pragma: no cover
+                        raise
+                    break
+                else:
+                    time.sleep(1)
+                    if timeout and time.time() - start_time > timeout:  # pragma: no cover
+                        raise TimeoutError('Check Mars service stop timeout')
+
+
+def new_cluster(kube_api_client=None, image=None, scheduler_num=1, scheduler_cpu=2,
+                scheduler_mem=4 * 1024 ** 3, worker_num=1, worker_cpu=8, worker_mem=32 * 1024 ** 3,
+                worker_spill_paths=None, worker_cache_mem='45%', min_worker_num=None,
+                web_num=1, web_cpu=1, web_mem=4 * 1024 ** 3, slb_endpoint=None, verify_ssl=True,
+                timeout=None, **kwargs):
+    worker_spill_paths = worker_spill_paths or ['/tmp/spill-dir']
+    cluster = KubeDLCluster(kube_api_client, image=image, scheduler_num=scheduler_num,
+                            scheduler_cpu=scheduler_cpu, scheduler_mem=scheduler_mem,
+                            worker_num=worker_num, worker_cpu=worker_cpu, worker_mem=worker_mem,
+                            worker_spill_paths=worker_spill_paths, worker_cache_mem=worker_cache_mem,
+                            min_worker_num=min_worker_num, web_num=web_num, web_cpu=web_cpu,
+                            web_mem=web_mem, slb_endpoint=slb_endpoint, verify_ssl=verify_ssl,
+                            timeout=timeout, **kwargs)
+    client = KubeDLClusterClient(cluster)
+    client.start()
+    return client
---
+++ mars/deploy/kubedl/config.py
@@ -0,0 +1,203 @@
+# Copyright 1999-2020 Alibaba Group Holding Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from urllib.parse import urlparse
+
+from ...utils import parse_readable_size
+from ..kubernetes.config import ContainerEnvConfig
+
+DEFAULT_SERVICE_ACCOUNT_NAME = 'kubedl-sa'
+
+
+def _remove_nones(cfg):
+    return dict((k, v) for k, v in cfg.items() if v is not None)
+
+
+class ResourceConfig:
+    """
+    Configuration builder for Kubernetes computation resources
+    """
+    def __init__(self, cpu, memory):
+        self._cpu = cpu
+        self._memory, ratio = parse_readable_size(memory) if memory is not None else (None, False)
+        assert not ratio
+
+    def build(self):
+        return {
+            'cpu': str(self._cpu),
+            'memory': str(int(self._memory)),
+        }
+
+
+class ReplicaSpecConfig:
+    """
+    Base configuration builder for Kubernetes replication controllers
+    """
+    container_name = 'mars'
+
+    def __init__(self, name, image, replicas, resource_request=None, resource_limit=None,
+                 node_selectors=None):
+        self._name = name
+        self._image = image
+        self._replicas = replicas
+        self._envs = dict()
+        self._node_selectors = node_selectors
+
+        self.add_default_envs()
+
+        self._resource_request = resource_request
+        self._resource_limit = resource_limit
+
+    def add_env(self, name, value=None, field_path=None):
+        self._envs[name] = ContainerEnvConfig(name, value=value, field_path=field_path)
+
+    def add_simple_envs(self, envs):
+        for k, v in envs.items() or ():
+            self.add_env(k, v)
+
+    def add_default_envs(self):
+        pass  # pragma: no cover
+
+    def build_container_command(self):
+        raise NotImplementedError
+
+    def build_container(self):
+        resources_dict = {
+            'requests': self._resource_request.build() if self._resource_request else None,
+            'limits': self._resource_limit.build() if self._resource_limit else None,
+        }
+        return _remove_nones({
+            'imagePullPolicy': 'Always',
+            'command': self.build_container_command(),
+            'env': [env.build() for env in self._envs.values()] or None,
+            'image': self._image,
+            'name': self.container_name,
+            'resources': dict((k, v) for k, v in resources_dict.items() if v) or None,
+        })
+
+    def build_template_spec(self):
+        return _remove_nones({
+            'serviceAccountName': DEFAULT_SERVICE_ACCOUNT_NAME,
+            'tolerations': [{'operator': 'Exists'}],
+            'nodeSelector': self._node_selectors,
+            'containers': [self.build_container()],
+        })
+
+    def build(self):
+        return {
+            'replicas': int(self._replicas),
+            'restartPolicy': 'Never',
+            'template': {
+                'metadata': {
+                    'labels': {'mars/service-type': self._name},
+                },
+                'spec': self.build_template_spec()
+            }
+        }
+
+
+class MarsReplicaSpecConfig(ReplicaSpecConfig):
+    service_name = None
+    service_label = None
+
+    def __init__(self, image, replicas, cpu=None, memory=None, limit_resources=False,
+                 modules=None, node_selectors=None):
+        self._cpu = cpu
+        self._memory, ratio = parse_readable_size(memory) if memory is not None else (None, False)
+        assert not ratio
+
+        if isinstance(modules, str):
+            self._modules = modules.split(',')
+        else:
+            self._modules = modules
+
+        res = ResourceConfig(cpu, memory) if cpu or memory else None
+        super().__init__(self.service_label, image, replicas, resource_request=res,
+                         resource_limit=res if limit_resources else None,
+                         node_selectors=node_selectors)
+
+    def build_container_command(self):
+        return ['/bin/sh', '-c', f'python -m mars.deploy.kubernetes.{self.service_name}']
+
+    def add_default_envs(self):
+        if self._modules:
+            self.add_env('MARS_LOAD_MODULES', ','.join(self._modules))
+
+
+class MarsSchedulerSpecConfig(MarsReplicaSpecConfig):
+    service_name = 'scheduler'
+    service_label = 'marsscheduler'
+
+
+class MarsWorkerSpecConfig(MarsReplicaSpecConfig):
+    service_name = 'worker'
+    service_label = 'marsworker'
+
+    def __init__(self, *args, **kwargs):
+        self._cache_mem = kwargs.pop('cache_mem', None)
+        self._spill_dirs = kwargs.pop('spill_dirs', None) or ()
+        kwargs['limit_resources'] = kwargs.get('limit_resources', True)
+        super().__init__(*args, **kwargs)
+
+    @property
+    def spill_dirs(self):
+        return self._spill_dirs
+
+    def add_default_envs(self):
+        super().add_default_envs()
+        self.add_env('MARS_CACHE_MEM_SIZE', self._cache_mem)
+        if self._spill_dirs:
+            self.add_env('MARS_SPILL_DIRS', ':'.join(self._spill_dirs))
+
+
+class MarsWebSpecConfig(MarsReplicaSpecConfig):
+    service_name = 'web'
+    service_label = 'marsweb'
+
+
+class MarsJobConfig:
+    def __init__(self, job_name, scheduler_config, worker_config, web_config,
+                 web_host=None):
+        self._job_name = job_name
+        self._scheduler_config = scheduler_config
+        self._worker_config = worker_config
+        self._web_config = web_config
+        self._web_host = web_host
+
+    def build(self):
+        if self._job_name is None:
+            metadata = {'generateName': 'mars-job-'}
+        else:
+            metadata = {'name': self._job_name}
+
+        web_host = self._web_host
+        if web_host is not None and '://' in web_host:
+            web_host = urlparse(web_host).netloc
+
+        return {
+            'kind': 'MarsJob',
+            'metadata': metadata,
+            'spec': _remove_nones({
+                'workerMemoryTuningPolicy': _remove_nones({
+                    'spillDirs': self._worker_config.spill_dirs or None
+                }),
+                'cleanPodPolicy': 'None',
+                'webHost': web_host,
+                'marsReplicaSpecs': {
+                    'Worker': self._worker_config.build(),
+                    'Scheduler': self._scheduler_config.build(),
+                    'WebService': self._web_config.build(),
+                },
+            }),
+        }
