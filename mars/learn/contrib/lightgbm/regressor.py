5ebefbd06997e43231a6fd6286536151ddc7c299
Wenjun Si <swj0066@gmail.com>
2020-09-29 10:33:35 +0800

    Allow LightGBM wrapper to use numpy arrays (#1607)

--- mars/learn/contrib/lightgbm/regressor.py
+++ mars/learn/contrib/lightgbm/regressor.py
@@ -38,6 +38,7 @@ if lightgbm:
         def predict(self, X, **kw):
             session = kw.pop('session', None)
             run_kwargs = kw.pop('run_kwargs', None)
+            X = self._convert_tileable(X)
             return predict(self, X, session=session, run_kwargs=run_kwargs, **kw)
 
         def to_local(self):
