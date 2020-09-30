e3714411492deb6727234febd98980a8db0c6680
永弘 <yonghong.cs@alibaba-inc.com>
2020-09-30 15:24:39 +0800

    proxima_search

--- mars/learn/neighbors/_proxima2.py
+++ mars/learn/neighbors/_proxima2.py
@@ -15,6 +15,7 @@
 import tempfile
 
 import numpy as np
+import pandas as pd
 try:
     import pyproxima2 as pp
 except ImportError:  # pragma: no cover
@@ -128,6 +129,135 @@ class Proxima2BuildIndex(LearnOperand, LearnOperandMixin):
         builder.dump(dumper)
 
         ctx[op.outputs[0].key] = path
+
+    @classmethod
+    def _execute_agg(cls, ctx, op: "Proxima2BuildIndex"):
+        paths = [ctx[inp.key] for inp in op.inputs]
+        ctx[op.outputs[0].key] = paths
+
+    @classmethod
+    def execute(cls, ctx, op: "Proxima2BuildIndex"):
+        if op.stage != OperandStage.agg:
+            return cls._execute_map(ctx, op)
+        else:
+            return cls._execute_agg(ctx, op)
+
+    @classmethod
+    def concat_tileable_chunks(cls, tileable):
+        assert not tileable.is_coarse()
+
+        op = cls(stage=OperandStage.agg)
+        chunk = cls(stage=OperandStage.agg).new_chunk(tileable.chunks)
+        return op.new_tileable([tileable], chunks=[chunk], nsplits=((1,),))
+
+
+class Proxima2SearchIndex(LearnOperand, LearnOperandMixin):
+    _op_type_ = opcodes.PROXIMA2_QUERY
+
+    _tensor = KeyField('tensor')
+    _pk = KeyField('pk')
+    _metric = StringField('metric')
+    _dimension = Int32Field('dimension')
+    _topk = Int32Field('topk')
+    _index = KeyField('index')
+
+    def __init__(self, tensor=None, pk=None, metric=None, dimension=None,
+                 index=None, topk=None, output_types=None, stage=None, **kw):
+        super().__init__(_tensor=tensor, _pk=pk, _metric=metric,
+                         _dimension=dimension, _output_types=output_types,
+                         _index=index, _topk=topk, _stage=stage, **kw)
+        if self._output_types is None:
+            self._output_types = [OutputType.dataframe]
+
+    @property
+    def tensor(self):
+        return self._tensor
+
+    @property
+    def pk(self):
+        return self._pk
+
+    @property
+    def metric(self):
+        return self._metric
+
+    @property
+    def dimension(self):
+        return self._dimension
+
+    @property
+    def index(self):
+        return self._index
+
+    @property
+    def topk(self):
+        return self._topk
+
+    def _set_inputs(self, inputs): # to Graph Node
+        super()._set_inputs(inputs)
+        self._tensor = self._inputs[0]
+        self._pk = self._inputs[1]
+        self._index = self._inputs[2]
+
+    def __call__(self, tensor, pk, index):
+        return self.new_tileable([tensor, pk, index])
+
+    @classmethod
+    def tile(cls, op):
+        # tensor = op.tensor
+        # pk = op.pk
+        # out = op.outputs[0]
+        #
+        # # make sure all inputs have known chunk sizes
+        # check_chunks_unknown_shape(op.inputs, TilesError)
+        #
+        # nsplit = decide_unify_split(tensor.nsplits[0], pk.nsplits[0])
+        # if tensor.chunk_shape[1] > 1:
+        #     tensor = tensor.rechunk({0: nsplit, 1: tensor.shape[1]})._inplace_tile()
+        # else:
+        #     tensor = tensor.rechunk({0: nsplit})._inplace_tile()
+        # pk = pk.rechunk({0: nsplit})._inplace_tile()
+        #
+        # out_chunks = []
+        # for chunk, pk_col_chunk in zip(tensor.chunks, pk.chunks):
+        #     chunk_op = op.copy().reset_key()
+        #     chunk_op._stage = OperandStage.map
+        #     out_chunk = chunk_op.new_chunk([chunk, pk_col_chunk],
+        #                                    index=pk_col_chunk.index)
+        #     out_chunks.append(out_chunk)
+        #
+        # params = out.params
+        # params['chunks'] = out_chunks
+        # params['nsplits'] = ((1,) * len(out_chunks),)
+        # new_op = op.copy()
+        # return new_op.new_tileables(op.inputs, kws=[params])
+        pass
+
+    @classmethod
+    def _execute_map(cls, ctx, op: "Proxima2SearchIndex"):
+        inp = ctx[op.tensor.key]
+        pks = ctx[op.pk.key]
+        index_path = ctx[op.index.key]
+
+        container = pp.IndexContainer(name="FileContainer", path=index_path)
+        searcher = pp.IndexSearcher("SsgSearcher")
+        pp_ctx = searcher.load(container).create_context(topk=op.topk)
+
+        # vec = np.random.uniform(low=0.0, high=1.0, size=self.dim).astype('f')
+        vecs = np.ascontiguousarray(inp)
+        results = ctx.search(query=vecs, count=len(vecs))
+
+        result = pd.DataFrame()
+        result[0] = np.empty()
+
+        for it in results:
+            for ele in it:
+                ele.key(), ele.score()
+
+
+        for ele in results[0]:
+            print("Result: the key is: {0}\tscore is: {1}.".format(ele.key(), ele.score()))
+
 
     @classmethod
     def _execute_agg(cls, ctx, op: "Proxima2BuildIndex"):
