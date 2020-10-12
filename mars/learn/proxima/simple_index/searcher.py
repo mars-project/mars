e14cab5b8f9e2148f1c7bd9f3732384580767d3e
永弘 <yonghong.cs@alibaba-inc.com>
2020-10-12 18:08:16 +0800

    refine

--- mars/learn/proxima/simple_index/searcher.py
+++ mars/learn/proxima/simple_index/searcher.py
@@ -13,7 +13,6 @@
 # limitations under the License.
 
 import itertools
-import tempfile
 
 import numpy as np
 
@@ -29,7 +28,7 @@ from ....tiles import TilesError
 from ....tensor.utils import decide_unify_split
 from ....utils import check_chunks_unknown_shape
 from ...operands import LearnOperand, LearnOperandMixin
-from ..core import proxima, get_proxima_type, validate_tensor
+from ..core import proxima, validate_tensor
 
 
 class ProximaSearcher(LearnOperand, LearnOperandMixin):
