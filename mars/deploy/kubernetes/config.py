1a029d5a090073c6a2f41776cab4df2a85e19368
He Kaisheng <heks93@163.com>
2020-11-11 17:04:51 +0800

    Remount /dev/shm directory in entrypoint.sh to avoid SIGBUS in Kubernetes (#1700)

--- mars/deploy/kubernetes/config.py
+++ mars/deploy/kubernetes/config.py
@@ -13,8 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys
-
 from ... import __version__ as mars_version
 from ...utils import parse_readable_size
 
@@ -495,17 +493,12 @@ class MarsWorkersConfig(MarsReplicationControllerConfig):
             self.add_env('MARS_SPILL_DIRS', ':'.join(self._spill_volumes))
 
         if mount_shm:
-            vol_host_path = '/tmp' if sys.platform == 'darwin' else '/dev/shm'
-            vol_cfg = HostPathVolumeConfig('shm-volume', '/dev/shm', vol_host_path, 'Directory')
-            self.add_volume(vol_cfg)
-            self.add_env('MARS_MOUNT_HOST_SHM', '1')
+            self.add_env('MARS_K8S_REMOUNT_SHM', '1')
 
         if worker_cache_mem:
             self.add_env('MARS_CACHE_MEM_SIZE', worker_cache_mem)
         if min_cache_mem:
             self.add_env('MARS_MIN_CACHE_MEM_SIZE', min_cache_mem)
-            if worker_cache_mem:
-                self.add_env('MARS_CACHE_MEM_SCALE', 0.8)
 
     def config_readiness_probe(self):
         readiness_cmd = [
