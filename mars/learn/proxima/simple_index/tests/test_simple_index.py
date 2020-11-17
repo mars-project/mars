10bd8dad7f384b61808bcad40907889460dac4f4
永弘 <yonghong.cs@alibaba-inc.com>
2020-11-17 17:23:22 +0800

    fix bugs & test Download op

--- mars/learn/proxima/simple_index/tests/test_simple_index.py
+++ mars/learn/proxima/simple_index/tests/test_simple_index.py
@@ -434,6 +434,7 @@ class Test(unittest.TestCase):
             pk_m, distance_m = search_index(tensor=q, topk=topk, index=index, row_number=5)
 
             # testing
+            np.testing.assert_array_equal(1, 2)
             np.testing.assert_array_equal(pk_p, pk_m)
             np.testing.assert_array_equal(distance_p, distance_m)
 
