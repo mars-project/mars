f9458634493813d72fcfe049d2031b06cc9cf1db
Xuye (Chris) Qin <xuye.qin@alibaba-inc.com>
2020-12-04 23:35:27 +0800

    Support setting scalar which is a tensor for DataFrame (#1755)

--- mars/dataframe/datasource/from_tensor.py
+++ mars/dataframe/datasource/from_tensor.py
@@ -470,7 +470,7 @@ class SeriesFromTensor(DataFrameOperand, DataFrameOperandMixin):
 
 def series_from_tensor(tensor, index=None, name=None, gpu=None, sparse=False):
     if tensor.ndim > 1 or tensor.ndim <= 0:
-        raise TypeError(f'Not support create DataFrame from {tensor.ndim} dims tensor')
+        raise TypeError(f'Not support create Series from {tensor.ndim} dims tensor')
     gpu = tensor.op.gpu if gpu is None else gpu
     op = SeriesFromTensor(dtype=tensor.dtype, gpu=gpu, sparse=sparse)
     return op(tensor, index, name)
