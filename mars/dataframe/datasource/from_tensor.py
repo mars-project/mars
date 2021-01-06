d30d09b0789761d1ac63e3e3afacb56854131736
继盛 <xuye.qin@alibaba-inc.com>
2021-01-06 20:30:35 +0800

    Support unknown shape for mt.reshape, mt.histogram and md.DataFrame

--- mars/dataframe/datasource/from_tensor.py
+++ mars/dataframe/datasource/from_tensor.py
@@ -153,7 +153,9 @@ class DataFrameFromTensor(DataFrameOperand, DataFrameOperandMixin):
         elif isinstance(input_tensor, SERIES_TYPE):
             index_value = input_tensor.index_value
         else:
-            index_value = parse_index(pd.RangeIndex(start=0, stop=input_tensor.shape[0]))
+            stop = input_tensor.shape[0]
+            stop = -1 if np.isnan(stop) else stop
+            index_value = parse_index(pd.RangeIndex(start=0, stop=stop))
 
         if columns is not None:
             if not (input_tensor.ndim == 1 and len(columns) == 1 or
@@ -247,9 +249,9 @@ class DataFrameFromTensor(DataFrameOperand, DataFrameOperandMixin):
         out_chunks = []
         nsplits = in_tensor.nsplits
 
+        check_chunks_unknown_shape(op.inputs, TilesError)
         if op.index is not None:
             # rechunk index if it's a tensor
-            check_chunks_unknown_shape(op.inputs, TilesError)
             index_tensor = op.index.rechunk([nsplits[0]])._inplace_tile()
         else:
             index_tensor = None
@@ -287,8 +289,11 @@ class DataFrameFromTensor(DataFrameOperand, DataFrameOperandMixin):
                 pd_index = out_df.index_value.to_pandas()
                 chunk_pd_index = pd_index[index_stop - in_chunk.shape[0]:index_stop]
                 index_value = parse_index(chunk_pd_index, store_data=True)
+            elif op.index is None:
+                # input tensor has unknown shape
+                index_value = parse_index(pd.RangeIndex(index_stop - in_chunk.shape[0], index_stop),
+                                          store_data=True)
             else:
-                assert op.index is not None
                 index_chunk = index_tensor.cix[in_chunk.index[0], ]
                 chunk_inputs.append(index_chunk)
                 index_value = parse_index(pd.Index([], dtype=index_tensor.dtype),
