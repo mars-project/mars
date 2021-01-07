33ad674deb6f2cec434912540f0ce6ed6be11096
继盛 <xuye.qin@alibaba-inc.com>
2021-01-07 15:03:49 +0800

    Fix wrongly raised error: Tileable object must be executed first before being fetched

--- mars/dataframe/core.py
+++ mars/dataframe/core.py
@@ -445,12 +445,20 @@ class _BatchedFetcher:
         return self._iter(batch_size=batch_size, session=session)
 
     def fetch(self, session=None, **kw):
+        from .indexing.iloc import DataFrameIlocGetItem, SeriesIlocGetItem
+
         batch_size = kw.pop('batch_size', 1000)
         if len(kw) > 0:  # pragma: no cover
             raise TypeError(
                 f"'{next(iter(kw))}' is an invalid keyword argument for this function")
-        batches = list(self._iter(batch_size=batch_size, session=session))
-        return pd.concat(batches) if len(batches) > 1 else batches[0]
+
+        if isinstance(self.op, (DataFrameIlocGetItem, SeriesIlocGetItem)):
+            # see GH#1871
+            # already iloc, do not trigger batch fetch
+            return self._fetch(session=session, **kw)
+        else:
+            batches = list(self._iter(batch_size=batch_size, session=session))
+            return pd.concat(batches) if len(batches) > 1 else batches[0]
 
 
 class IndexData(HasShapeTileableData, _ToPandasMixin):
