21093ceabaa6e619e0450bda3efae2c0c0266309
Xuye (Chris) Qin <xuye.qin@alibaba-inc.com>
2021-01-13 16:04:53 +0800

    Support column pruning for groupby().agg() on data sources (#1886)

--- mars/optimizes/tileable_graph/column_pruning.py
+++ mars/optimizes/tileable_graph/column_pruning.py
@@ -16,48 +16,31 @@
 
 from ...utils import copy_tileables
 from ...dataframe.utils import parse_index
+from ...dataframe.datasource.core import ColumnPruneSupportedDataSourceMixin
 from ...dataframe.groupby.aggregation import DataFrameGroupByAgg
-from ...dataframe.datasource.read_csv import DataFrameReadCSV
+from ...dataframe.indexing.getitem import DataFrameIndex
 from .core import TileableOptimizeRule, register
 
 
-class GroupbyPruneReadCSV(TileableOptimizeRule):
-    """
-    An experimental implementation for tileable optimization rule.
-    This rule works only when groupby aggregation operation follows the read CSV files,
-    we can prune the columns that not used by the following operations when read the files.
-    """
-    def match(self, node):
-        if isinstance(node.inputs[0].op, DataFrameReadCSV) and \
-                node.inputs[0] not in self._optimizer_context.result_tileables:
-            by_data = node.op.groupby_params.get('by')
-            by_columns = by_data if isinstance(by_data, (list, tuple)) else [by_data]
-            if isinstance(node.op.func, (str, list)):
-                # Passing func name(s) means perform on all columns.
-                return False
-            elif len(set(by_columns + list(node.op.func))) == len(node.inputs[0].op.usecols or node.inputs[0].dtypes):
-                # If performs on all columns, no need to prune.
-                return False
-            return True
-        return False
+class _PruneDataSource(TileableOptimizeRule):
+    def _get_selected_columns(self, op):  # pragma: no cover
+        raise NotImplementedError
 
     def apply(self, node):
-        by_data = node.op.groupby_params.get('by')
-        by_columns = by_data if isinstance(by_data, (list, tuple)) else [by_data]
-        agg_columns = list(node.op.func)
         input_node = node.inputs[0]
-        selected_columns = [c for c in list(input_node.dtypes.index) if c in by_columns + agg_columns]
+        selected_columns = self._get_selected_columns(node.op)
         if input_node in self._optimizer_context:
             new_input = self._optimizer_context[input_node]
             selected_columns = [
-                c for c in list(input_node.dtypes.index) if c in selected_columns + new_input.op._usecols]
+                c for c in list(input_node.dtypes.index)
+                if c in selected_columns + new_input.op.get_columns()]
         else:
             new_input = copy_tileables([input_node])[0].data
 
         new_input._shape = (input_node.shape[0], len(selected_columns))
         new_input._dtypes = input_node.dtypes[selected_columns]
         new_input._columns_value = parse_index(new_input._dtypes.index, store_data=True)
-        new_input.op._usecols = selected_columns
+        new_input.op.set_pruned_columns(selected_columns)
         new_node = copy_tileables([node], inputs=[new_input])[0].data
 
         self._optimizer_context[node] = new_node
@@ -65,4 +48,69 @@ class GroupbyPruneReadCSV(TileableOptimizeRule):
         return node
 
 
-register(DataFrameGroupByAgg, GroupbyPruneReadCSV)
+class GroupbyPruneDatasource(_PruneDataSource):
+    """
+    An experimental implementation for tileable optimization rule.
+    This rule works only when groupby aggregation operation follows the read CSV files,
+    we can prune the columns that not used by the following operations when read the files.
+    """
+    def match(self, node):
+        if isinstance(node.inputs[0].op, ColumnPruneSupportedDataSourceMixin) and \
+                node.inputs[0] not in self._optimizer_context.result_tileables:
+            selected_columns = self._get_selected_columns(node.op)
+            if not selected_columns:
+                return False
+            return True
+        return False
+
+    @classmethod
+    def _get_selected_columns(cls, op):
+        by = op.groupby_params.get('by')
+        by_cols = by if isinstance(by, (list, tuple)) else [by]
+
+        # check all by columns
+        for by_col in by_cols:
+            if not isinstance(by_col, str):
+                return
+
+        selected_columns = list(by_cols)
+
+        # DataFrameGroupby
+        selection = op.groupby_params.get('selection', list())
+        selection = list(selection) \
+            if isinstance(selection, (list, tuple)) else [selection]
+        if isinstance(op.func, (str, list)):
+            if not selection:
+                # if func is str or list and no selection
+                # cannot perform optimization
+                return
+            else:
+                selected_columns.extend(selection)
+        else:
+            # dict
+            func_cols = list(op.func)
+            selected_columns.extend(func_cols)
+
+        selected_columns = set(selected_columns)
+        if len(selected_columns) == \
+                len(op.inputs[0].op.get_columns() or op.inputs[0].dtypes):
+            # If performs on all columns, no need to prune
+            return
+
+        return [c for c in op.inputs[0].dtypes.index if c in selected_columns]
+
+
+class GetItemPruneDataSource(_PruneDataSource):
+    def match(self, node):
+        if isinstance(node.inputs[0].op, ColumnPruneSupportedDataSourceMixin) and \
+                node.inputs[0] not in self._optimizer_context.result_tileables and \
+                isinstance(node.op, DataFrameIndex) and node.op.col_names is not None:
+            return True
+        return False
+
+    def _get_selected_columns(self, op):
+        return op.col_names if isinstance(op.col_names, list) else [op.col_names]
+
+
+register(DataFrameGroupByAgg, GroupbyPruneDatasource)
+register(DataFrameIndex, GetItemPruneDataSource)
