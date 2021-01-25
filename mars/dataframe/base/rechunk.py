afd3ba8b0fc213fa94df0e9e7d705fe18153df90
继盛 <xuye.qin@alibaba-inc.com>
2021-01-25 14:06:26 +0800

    Fix rechunk when chunks have different dtypes that cannot compare

--- mars/dataframe/base/rechunk.py
+++ mars/dataframe/base/rechunk.py
@@ -109,7 +109,7 @@ def _get_chunk_size(a, chunk_size):
 
 
 def rechunk(a, chunk_size, threshold=None, chunk_size_limit=None, reassign_worker=False):
-    if not any(pd.isna(s) for s in a.shape):
+    if not any(pd.isna(s) for s in a.shape) and not a.is_coarse():
         # do client check only when no unknown shape,
         # real nsplits will be recalculated inside `tile`
         chunk_size = _get_chunk_size(a, chunk_size)
