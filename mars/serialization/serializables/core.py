441953b407b1ace500cf4713fc7da9a37010f213
继盛 <xuye.qin@alibaba-inc.com>
2021-03-18 21:24:19 +0800

    Implements Serializable based on new serialization mechanism.

---
+++ mars/serialization/serializables/core.py
@@ -0,0 +1,118 @@
+# Copyright 1999-2020 Alibaba Group Holding Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import cloudpickle
+from typing import Dict, Tuple, List, Type, Any
+
+from ..core import DictSerializer
+from .field import _STORE_VALUE_PROPERTY, Field, FunctionField, OneOfField
+
+
+class SerializableMeta(type):
+    def __new__(mcs,
+                name: str,
+                bases: Tuple[Type],
+                properties: Dict):
+        properties = properties.copy()
+        for base in bases:
+            if hasattr(base, '_FIELDS'):
+                properties.update(base._FIELDS)
+
+        property_to_fields = dict()
+        # filter out all fields
+        for k, v in properties.items():
+            if not isinstance(v, Field):
+                continue
+
+            property_to_fields[k] = v
+            properties[k] = v
+
+        properties['_FIELDS'] = property_to_fields
+        properties['__slots__'] = tuple(properties.pop('__slots__', ()))
+
+        clz = type.__new__(mcs, name, bases, properties)
+        return clz
+
+
+class Serializable(metaclass=SerializableMeta):
+    __slots__ = _STORE_VALUE_PROPERTY,
+
+    _FIELDS: Dict[str, Field]
+    _STORE_VALUE_PROPERTY: Dict[str, Any]
+
+    def __init__(self, *args, **kwargs):
+        setattr(self, _STORE_VALUE_PROPERTY, dict())
+
+        for slot, arg in zip(self.__slots__, args):  # pragma: no cover
+            object.__setattr__(self, slot, arg)
+
+        for key, val in kwargs.items():
+            object.__setattr__(self, key, val)
+
+
+class SerializableSerializer(DictSerializer):
+    """
+    Leverage DictSerializer to perform serde.
+    """
+    serializer_name = 'serializable'
+
+    def serialize(self, obj: Serializable):
+        fields = obj._FIELDS
+        tag_to_values = getattr(obj, _STORE_VALUE_PROPERTY).copy()
+
+        for field in fields.values():
+            tag = field.tag
+            try:
+                value = tag_to_values[tag]
+            except KeyError:
+                continue
+            if field.on_serialize:
+                value = tag_to_values[tag] = field.on_serialize(value)
+            if isinstance(field, FunctionField):
+                tag_to_values[tag] = cloudpickle.dumps(value)
+
+        header, buffers = super().serialize(tag_to_values)
+        header['class'] = type(obj)
+        return header, buffers
+
+    def deserialize(self, header: Dict, buffers: List) -> Serializable:
+        obj_class: Type[Serializable] = header.pop('class')
+        tag_to_values = super().deserialize(header, buffers)
+
+        property_to_values = dict()
+        for property_name, field in obj_class._FIELDS.items():
+            if isinstance(field, OneOfField):
+                value = None
+                for ref_field in field.reference_fields:
+                    try:
+                        value = tag_to_values.pop(ref_field.tag)
+                    except KeyError:
+                        continue
+                if value is None:
+                    continue
+            else:
+                try:
+                    value = tag_to_values[field.tag]
+                except KeyError:
+                    continue
+            if field.on_deserialize:
+                value = field.on_deserialize(value)
+            if isinstance(field, FunctionField):
+                value = cloudpickle.loads(value)
+            property_to_values[property_name] = value
+
+        return obj_class(**property_to_values)
+
+
+SerializableSerializer.register(Serializable)
---
+++ mars/serialization/serializables/tests/test_field_type.py
@@ -0,0 +1,104 @@
+# Copyright 1999-2020 Alibaba Group Holding Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from collections import namedtuple
+from datetime import datetime, timedelta, timezone
+
+import numpy as np
+import pandas as pd
+import pytest
+
+from mars.core import HasKey
+from mars.serialization.serializables import FieldTypes
+
+
+class MyClass(HasKey):
+    __slots__ = ()
+
+    @staticmethod
+    def my_func():
+        """
+        Test function
+        """
+
+
+my_named_tuple = namedtuple('my_named_tuple', 'a b')
+
+
+fields_values = [
+    # field_type, valid values, invalid values
+    [FieldTypes.bool, [True, np.bool(False)], [1]],
+    [FieldTypes.int8, [8, np.int8(8)], [8.]],
+    [FieldTypes.int16, [16, np.int16(16)], [16.]],
+    [FieldTypes.int32, [32, np.int32(32)], [64.]],
+    [FieldTypes.uint8, [8, np.uint8(8)], [8.]],
+    [FieldTypes.uint16, [16, np.uint16(16)], [16.]],
+    [FieldTypes.uint32, [32, np.uint32(32)], [32.]],
+    [FieldTypes.uint64, [64, np.uint64(64)], [64.]],
+    [FieldTypes.float16, [16., np.float16(16)], [16]],
+    [FieldTypes.float32, [32., np.float32(32)], [32]],
+    [FieldTypes.float64, [64., np.float64(64)], [64]],
+    [FieldTypes.complex64, [1+2j, np.complex64(1+2j)], [64]],
+    [FieldTypes.complex128, [1+2j, np.complex128(1+2j)], [128]],
+    [FieldTypes.bytes, [b'abc', np.bytes_('abc')], ['abc']],
+    [FieldTypes.string, ['abc', np.str_('abc')], [b'abc']],
+    [FieldTypes.ndarray, [np.array([1, 2, 3])], [object()]],
+    [FieldTypes.dtype, [np.dtype(np.int32), pd.StringDtype()], [object()]],
+    [FieldTypes.key, [MyClass()], [object()]],
+    [FieldTypes.slice, [slice(1, 10), slice('a', 'b')], [object()]],
+    [FieldTypes.datetime, [datetime.now(), pd.Timestamp(0)], [object()]],
+    [FieldTypes.timedelta, [timedelta(days=1), pd.Timedelta(days=1)], [object()]],
+    [FieldTypes.tzinfo, [timezone.utc], [object()]],
+    [FieldTypes.index, [pd.RangeIndex(10), pd.Index([1, 2])], [object()]],
+    [FieldTypes.series, [pd.Series([1, 2, 3])], [object()]],
+    [FieldTypes.dataframe, [pd.DataFrame({'a': [1, 2]})], [object()]],
+    [FieldTypes.interval_array, [pd.arrays.IntervalArray([])], [object()]],
+    [FieldTypes.function, [MyClass.my_func], [object()]],
+    [FieldTypes.namedtuple, [my_named_tuple(a=1, b=2)], [tuple()]],
+    [FieldTypes.reference(MyClass), [MyClass()], [object()]],
+    [FieldTypes.tuple(FieldTypes.int64, ...), [tuple(), tuple([1, 2])], [list(), tuple([1, 2.])]],
+    [FieldTypes.list(FieldTypes.int64, FieldTypes.float64), [[1, 1.]], [tuple(), [1, 1]]],
+    [FieldTypes.dict(FieldTypes.string, FieldTypes.int64), [{'a': 1}], [{1: 'a'}, {'a': 1.}]],
+    [FieldTypes.any, [object()], []],
+]
+
+
+@pytest.mark.parametrize(
+    'field_type, valid_values, invalid_values',
+    fields_values
+)
+def test_field_type(field_type, valid_values, invalid_values):
+    assert isinstance(field_type.type_name, str)
+    assert isinstance(field_type.name, str)
+
+    for valid_value in valid_values:
+        field_type.validate(valid_value)
+
+    for invalid_value in invalid_values:
+        with pytest.raises(TypeError):
+            field_type.validate(invalid_value)
+
+
+def test_collction_field_error():
+    with pytest.raises(ValueError):
+        FieldTypes.tuple(FieldTypes.int64, FieldTypes.float32).validate(tuple([1, 3., 3.]))
+
+
+def test_field_name():
+    assert FieldTypes.list().name == 'List'
+    assert FieldTypes.list(FieldTypes.int64, FieldTypes.float32).name == 'List[Int64, Float32]'
+    assert FieldTypes.tuple(FieldTypes.int8, ...).name == 'Tuple[Int8, ...]'
+    assert FieldTypes.tuple(FieldTypes.int8).name == 'Tuple[Int8, ...]'
+    assert FieldTypes.dict().name == 'Dict'
+    assert FieldTypes.dict(FieldTypes.int8, FieldTypes.float64).name == 'Dict[Int8, Float64]'
