917ad29b05766e0e297d29865b06e0e791054177
mubai <chaokun.yck@antfin.com>
2021-03-25 11:18:12 +0800

    ray cluster setup

--- mars/oscar/backends/mars/ray.py
+++ mars/oscar/backends/mars/ray.py
@@ -18,65 +18,89 @@ import os
 import posixpath
 from abc import ABC, abstractmethod
 from enum import Enum
-from typing import List, Optional
+from numbers import Number
+from typing import List, Optional, Dict
 from urllib.parse import urlparse, unquote
 
 from .communication.ray import ChannelID, RayServer
 from .config import ActorPoolConfig
 from .pool import AbstractActorPool, MainActorPool, SubActorPool, create_actor_pool
 from ....utils import lazy_import
+from ...backend import BaseActorBackend, register_backend
+from ...context import BaseActorContext
+from .driver import MarsActorDriver
+from .context import MarsActorContext
 
 ray = lazy_import('ray')
 logger = logging.getLogger(__name__)
 
 
-class RayActorPoolMixin(ABC, AbstractActorPool):
+@register_backend
+class RayActorBackend(BaseActorBackend):
+    @staticmethod
+    def name():
+        # return None because Mars is default scheme
+        return "ray"
+
+    @staticmethod
+    def get_context_cls():
+        return MarsActorContext
+
+    @staticmethod
+    def get_driver_cls():
+        return MarsActorDriver
+
+
+class RayActorPoolMixin(AbstractActorPool, ABC):
 
     async def __on_ray_recv__(self, channel_id: ChannelID, message):
         """Method for communication based on ray actors"""
-        assert len(self._servers) == 1, "Ray only support single server."
-        ray_server: RayServer = self._servers[0]
-        await ray_server.__on_ray_recv__(channel_id, message)
+        print(f"__on_ray_recv__ start channel_id {channel_id} message {message}")
+        if not hasattr(self, '_external_servers'):
+            ray_servers = [server for server in self._servers if isinstance(server, RayServer)]
+            assert len(ray_servers) == 1, f"Ray only support single server but got {ray_servers}."
+            self._external_servers = ray_servers
+        print(f"__on_ray_recv__ end channel_id {channel_id} message {message}")
+        return await self._external_servers[0].__on_ray_recv__(channel_id, message)
 
 
 class RayMainActorPool(RayActorPoolMixin, MainActorPool):
-
-    def get_server(self):
-        assert len(self._servers) == 1, "Ray only support single server."
-        ray_server: RayServer = self._servers[0]
-        return ray_server
 
     @classmethod
     def get_external_addresses(
             cls, address: str, n_process: int = None, ports: List[int] = None):
         assert not ports, f"ports should be none when actor pool running on ray, but got {ports}"
-        return [f'{address}/{i}' for i in range(n_process + 1)]
+        pg_name, bundle_index, _process_index = address_to_placement_info(address)
+        return [pg_bundle_to_address(pg_name, bundle_index, i) for i in range(n_process + 1)]
 
     @classmethod
     def get_sub_pool_manager_cls(cls):
-        return MainActorPool.ProcessSubActorPoolManager
+        return cls.RaySubActorPoolManager
 
     class RaySubActorPoolManager(MainActorPool.SubActorPoolManager):
 
         @classmethod
-        def start_sub_pool(
+        async def start_sub_pool(
                 cls,
-                actor_config: ActorPoolConfig,
+                actor_pool_config: ActorPoolConfig,
                 process_index: int,
                 start_method: str = None):
+            ray.init(ignore_reinit_error=True)
             external_addresses = \
-                actor_config.get_pool_config(process_index)['external_address']
+                actor_pool_config.get_pool_config(process_index)['external_address']
             assert len(external_addresses) == 1,\
                 f"Ray pool allows only one external address but got {external_addresses}"
             external_address = external_addresses[0]
             pg_name, bundle_index, _process_index = address_to_placement_info(external_address)
-            assert process_index == _process_index
+            assert process_index == _process_index,\
+                f"process_index {process_index} is not consistent with index {_process_index} " \
+                f"in external_address {external_address}"
             pg = ray.util.get_placement_group(pg_name) if pg_name else None
             # Hold actor_handle to avoid actor being freed.
             actor_handle = ray.remote(RaySubPool).options(
                 name=external_address, placement_group=pg,
                 placement_group_bundle_index=bundle_index).remote()
-            ray.get(actor_handle.start(actor_config, process_index).remote())
+            await actor_handle.start.remote(actor_pool_config, process_index)
             return actor_handle
 
         def kill_sub_pool(self, process: 'ray.actor.ActorHandle'):
@@ -89,6 +113,10 @@ class RayMainActorPool(RayActorPoolMixin, MainActorPool):
             except Exception:
                 logger.info("Detected RaySubPool actor {} died", process)
                 return False
+
+
+class RaySubActorPool(RayActorPoolMixin, SubActorPool):
+    pass
 
 
 class PoolStatus(Enum):
@@ -116,23 +144,25 @@ class RayPoolBase(ABC):
 class RayMainPool(RayPoolBase):
     actor_pool: RayMainActorPool
 
-    async def start(self, *args, **kwargs):
-        self.actor_pool = await create_actor_pool(*args, pool_cls=RayMainActorPool, **kwargs)
+    async def start(self, address, n_process, **kwargs):
+        self.actor_pool = await create_actor_pool(
+            address, n_process=n_process, pool_cls=RayMainActorPool,
+            subprocess_start_method="ray", **kwargs)
 
 
 class RaySubPool(RayPoolBase):
-    actor_pool: SubActorPool
+    actor_pool: RaySubActorPool
 
     async def start(self, *args, **kwargs):
         actor_config, process_index = args
         env = actor_config.get_pool_config(process_index)['env']
         if env:
             os.environ.update(env)
-        pool = asyncio.run(SubActorPool.create({
+        pool = await RaySubActorPool.create({
             'actor_pool_config': actor_config,
             'process_index': process_index
-        }))
-        asyncio.run(pool.start())
+        })
+        await pool.start()
         self.actor_pool = pool
         asyncio.create_task(pool.join())
 
@@ -141,7 +171,7 @@ def address_to_placement_info(address):
     """
     Args:
         address: The address of an actor pool which running in a ray actor. It's also
-        the name of the ray actor.
+        the name of the ray actor. address ex: ray://${pg_name}/${bundle_index}/${process_index}
 
     Returns:
         A tuple consisting of placement group name, bundle index, process index.
@@ -158,6 +188,7 @@ def address_to_placement_info(address):
         while tmp and tmp != "/":
             tmp, item = posixpath.split(tmp)
             parts.append(item)
+    parts = list(reversed(parts))
     if parts and len(parts) != 2:
         raise ValueError(f"Only bundle index and process index path are allowed in ray "
                          f"address {address}.")
@@ -169,3 +200,76 @@ def address_to_placement_info(address):
         return name, int(bundle_index), int(process_index)
     else:
         return name, -1, -1
+
+
+def pg_bundle_to_address(pg_name: str, bundle_index: int, process_index: int = 0):
+    return f"ray://{pg_name}/{bundle_index}/{process_index}"
+
+
+class NodeResourceSpec:
+    __slots__ = 'n_process', 'num_cpus', 'memory', 'num_gpus'
+
+    def __init__(self, n_process, num_cpus=None, num_gpus=None, memory=None):
+        self.n_process = n_process
+        self.num_cpus = num_cpus or n_process
+        assert n_process <= self.num_cpus
+        self.num_gpus = num_gpus
+        self.memory = memory
+
+    def to_bundle(self):
+        bundle = {'CPU': self.num_cpus}
+        if self.num_gpus:
+            bundle['GPU'] = self.num_gpus
+        if self.memory:
+            bundle['memory'] = self.memory
+        return bundle
+
+
+class ClusterManager:
+
+    def __init__(self, cluster_name, resource_specs: List[NodeResourceSpec]):
+        self.cluster_name = cluster_name
+        self.resource_specs = resource_specs
+        self.nodes_info = []
+        # Hold actor handle to avoid being freed.
+        self.main_pool_handles = [None] * len(resource_specs)
+
+    def add_node(self, index: int, main_handle, address):
+        pg_name, bundle_index, _process_index = address_to_placement_info(address)
+        assert len(self.nodes_info) == index
+        node_info = [pg_bundle_to_address(pg_name, bundle_index, process_index=i)
+                     for i in range(self.resource_specs[index].n_process + 1)]
+        self.nodes_info.append(node_info)
+        self.main_pool_handles[index] = main_handle
+
+    def get_node_info(self, index):
+        return self.nodes_info[index]
+
+    def get_all_nodes_info(self):
+        return list(self.nodes_info)
+
+    def addresses(self):
+        return [process for node in self.get_all_nodes_info() for process in node]
+
+    def address_to_resources(self) -> Dict[str, Dict[str, Number]]:
+        # TODO(chaokunyang) support address resources mapping
+        return {process: {"CPU": 1} for node in self.get_all_nodes_info() for process in node}
+
+
+def create_cluster(cluster_name, resource_specs: List[NodeResourceSpec]):
+    pg_name = cluster_name
+    bundles = [spec.to_bundle() for spec in resource_specs]
+    logger.info("Creating placement group %s with bundles %s.", pg_name, bundles)
+    pg = ray.util.placement_group(name=pg_name, bundles=bundles, strategy="SPREAD")
+    ray.get(pg.ready())
+    logger.info("Create placement group success.")
+    cluster_manager = ClusterManager(cluster_name, resource_specs)
+    for index, (spec, bundle) in enumerate(zip(resource_specs, bundles)):
+        address = pg_bundle_to_address(pg_name, index, process_index=0)
+        print(f"address {address}")
+        # Hold actor_handle to avoid actor being freed.
+        actor_handle = ray.remote(RayMainPool).options(
+            name=address, placement_group=pg, placement_group_bundle_index=index).remote()
+        ray.get(actor_handle.start.remote(address, spec.n_process))
+        cluster_manager.add_node(index, actor_handle, address)
+    return cluster_manager
