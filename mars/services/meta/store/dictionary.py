004d47ef68961164f296b7c526455c87744bf437
Wenjun Si <wenjun.swj@alibaba-inc.com>
2021-03-25 14:27:43 +0800

    Add bands to chunk meta (#2065)

--- mars/services/meta/store/dictionary.py
+++ mars/services/meta/store/dictionary.py
@@ -13,10 +13,10 @@
 # limitations under the License.
 
 from dataclasses import asdict
-from typing import Dict, List
+from typing import Dict, List, Tuple
 
 from ....utils import implements
-from ..core import _CommonMeta
+from ..core import _CommonMeta, _ChunkMeta
 from .base import AbstractMetaStore, register_meta_store
 
 
@@ -56,3 +56,11 @@ class DictMetaStore(AbstractMetaStore):
     async def del_meta(self,
                        object_id: str):
         del self._store[object_id]
+
+    @implements(AbstractMetaStore.add_chunk_bands)
+    async def add_chunk_bands(self,
+                              object_id: str,
+                              bands: List[Tuple[str, str]]):
+        meta = self._store[object_id]
+        assert isinstance(meta, _ChunkMeta)
+        meta.bands = list(set(meta.bands) | set(bands))
