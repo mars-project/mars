48dbda35ed3b01e2c1634bd2558e7fab8726848c
Tao He <sighingnow@gmail.com>
2021-03-30 12:34:13 +0800

    Fix batch get / delete object id and stop launching plasma when working on vineyard (#2072)
    * Fixes the batch get/delete object id, and don't launch plasma when working with vineyard.
    * Fixes and restore the ray CI part.
    
    Signed-off-by: Tao He <sighingnow@gmail.com>

--- mars/worker/storage/vineyardhandler.py
+++ mars/worker/storage/vineyardhandler.py
@@ -210,15 +210,19 @@ class VineyardHandler(StorageHandler, ObjectStorageMixin):
         return obj_id
 
     def _batch_get_object_id(self, session_id, data_keys):
-        addr = self._cluster_info.get_scheduler((session_id, data_keys[0]))
-        obj_ids = self._actor_ctx.actor_ref(VineyardKeyMapActor.default_uid(), address=addr) \
-            .batch_get(session_id, data_keys)
+        obj_ids = []
+        for data_key in data_keys:
+            addr = self._cluster_info.get_scheduler((session_id, data_key))
+            obj_id = self._actor_ctx.actor_ref(VineyardKeyMapActor.default_uid(), address=addr) \
+                .get(session_id, data_key)
+            obj_ids.append(obj_id)
         return obj_ids
 
     def _batch_delete_from_key_mapper(self, session_id, data_keys):
-        addr = self._cluster_info.get_scheduler((session_id, data_keys[0]))
-        self._actor_ctx.actor_ref(VineyardKeyMapActor.default_uid(), address=addr) \
-            .batch_delete(session_id, data_keys)
+        for data_key in data_keys:
+            addr = self._cluster_info.get_scheduler((session_id, data_key))
+            self._actor_ctx.actor_ref(VineyardKeyMapActor.default_uid(), address=addr) \
+                .delete(session_id, data_key)
 
     @wrap_promised
     def create_bytes_reader(self, session_id, data_key, packed=False, packed_compression=None,
@@ -281,7 +285,10 @@ class VineyardHandler(StorageHandler, ObjectStorageMixin):
 
     def delete(self, session_id, data_keys, _tell=False):
         data_ids = self._batch_get_object_id(session_id, data_keys)
-        logger.debug('delete chunks from vineyard: %s', data_ids)
+        logger.debug('delete chunks from vineyard: keys = %s, in vineyard is %s', data_keys, data_ids)
+        # when multiple workers connected to the same vineyard, they will think they both
+        # hold the chunk, but when it being deleted, it won't be there anymore.
+        data_ids = [data_id for data_id in data_ids if data_id]
         try:
             self._client.delete(data_ids, deep=True)
         except vineyard._C.ObjectNotExistsException:
