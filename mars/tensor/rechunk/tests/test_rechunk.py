774710903d9477871231dcdb606a66fd4f8a2555
继盛 <xuye.qin@alibaba-inc.com>
2021-05-18 16:20:53 +0800

    Modify tensor rechunk

--- mars/tensor/rechunk/tests/test_rechunk.py
+++ mars/tensor/rechunk/tests/test_rechunk.py
@@ -14,64 +14,56 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import unittest
-
-from mars.core import get_tiled
+from mars.core import tile
 from mars.tensor.datasource import ones
 from mars.tensor.indexing.slice import TensorSlice
 from mars.tensor.rechunk.rechunk import compute_rechunk
 
+    
+def test_compute_rechunk():
+    tensor = ones((12, 8), chunk_size=((4, 4, 3, 1), (3, 3, 2)))
+    tensor = tile(tensor)
+    new_tensor = compute_rechunk(tensor, ((9, 2, 1), (2, 1, 4, 1)))
 
-class Test(unittest.TestCase):
-    def testComputeRechunk(self):
-        tensor = ones((12, 8), chunk_size=((4, 4, 3, 1), (3, 3, 2)))
-        tensor = tensor.tiles()
-        new_tensor = compute_rechunk(tensor, ((9, 2, 1), (2, 1, 4, 1)))
+    assert len(new_tensor.chunks) == 12
+    assert len(new_tensor.chunks[0].inputs) == 3
+    assert isinstance(new_tensor.chunks[0].inputs[0].op, TensorSlice)
+    assert new_tensor.chunks[0].inputs[0].inputs[0] is tensor.chunks[0].data
+    assert new_tensor.chunks[0].inputs[0].op.slices == [slice(None, None, None), slice(None, 2, None)]
+    assert new_tensor.chunks[0].inputs[1].inputs[0] is tensor.chunks[3].data
+    assert new_tensor.chunks[0].inputs[1].op.slices == [slice(None, None, None), slice(None, 2, None)]
+    assert new_tensor.chunks[0].inputs[2].inputs[0] is tensor.chunks[6].data
+    assert new_tensor.chunks[0].inputs[2].op.slices == [slice(None, 1, None), slice(None, 2, None)]
+    assert new_tensor.chunks[-1].inputs[0] is tensor.chunks[-1].data
+    assert new_tensor.chunks[-1].op.slices == [slice(None, None, None), slice(1, None, None)]
 
-        self.assertEqual(len(new_tensor.chunks), 12)
-        self.assertEqual(len(new_tensor.chunks[0].inputs), 3)
-        self.assertIsInstance(new_tensor.chunks[0].inputs[0].op, TensorSlice)
-        self.assertIs(new_tensor.chunks[0].inputs[0].inputs[0], tensor.chunks[0].data)
-        self.assertEqual(new_tensor.chunks[0].inputs[0].op.slices,
-                         [slice(None, None, None), slice(None, 2, None)])
-        self.assertIs(new_tensor.chunks[0].inputs[1].inputs[0], tensor.chunks[3].data)
-        self.assertEqual(new_tensor.chunks[0].inputs[1].op.slices,
-                         [slice(None, None, None), slice(None, 2, None)])
-        self.assertIs(new_tensor.chunks[0].inputs[2].inputs[0], tensor.chunks[6].data)
-        self.assertEqual(new_tensor.chunks[0].inputs[2].op.slices,
-                         [slice(None, 1, None), slice(None, 2, None)])
-        self.assertIs(new_tensor.chunks[-1].inputs[0], tensor.chunks[-1].data)
-        self.assertEqual(new_tensor.chunks[-1].op.slices,
-                         [slice(None, None, None), slice(1, None, None)])
 
-    def testRechunk(self):
-        tensor = ones((12, 9), chunk_size=4)
-        new_tensor = tensor.rechunk(3)
-        new_tensor = new_tensor.tiles()
+def test_rechunk():
+    tensor = ones((12, 9), chunk_size=4)
+    new_tensor = tensor.rechunk(3)
+    t, new_tensor = tile(tensor, new_tensor)
 
-        self.assertEqual(len(new_tensor.chunks), 12)
-        self.assertEqual(new_tensor.chunks[0].inputs[0], get_tiled(tensor).chunks[0].data)
-        self.assertEqual(len(new_tensor.chunks[1].inputs), 2)
-        self.assertEqual(new_tensor.chunks[1].inputs[0].op.slices,
-                         [slice(None, 3, None), slice(3, None, None)])
-        self.assertEqual(new_tensor.chunks[1].inputs[1].op.slices,
-                         [slice(None, 3, None), slice(None, 2, None)])
-        self.assertEqual(len(new_tensor.chunks[-1].inputs), 2)
-        self.assertEqual(new_tensor.chunks[-1].inputs[0].op.slices,
-                         [slice(1, None, None), slice(2, None, None)])
-        self.assertEqual(new_tensor.chunks[-1].inputs[1].op.slices,
-                         [slice(1, None, None), slice(None, None, None)])
+    assert len(new_tensor.chunks) == 12
+    assert new_tensor.chunks[0].inputs[0] == t.chunks[0].data
+    assert len(new_tensor.chunks[1].inputs) == 2
+    assert new_tensor.chunks[1].inputs[0].op.slices == [slice(None, 3, None), slice(3, None, None)]
+    assert new_tensor.chunks[1].inputs[1].op.slices == [slice(None, 3, None), slice(None, 2, None)]
+    assert len(new_tensor.chunks[-1].inputs) == 2
+    assert new_tensor.chunks[-1].inputs[0].op.slices == [slice(1, None, None), slice(2, None, None)]
+    assert new_tensor.chunks[-1].inputs[1].op.slices == [slice(1, None, None), slice(None, None, None)]
 
-    def testSparse(self):
-        tensor = ones((7, 12), chunk_size=4).tosparse()
-        new_tensor = tensor.rechunk(5)
-        new_tensor = new_tensor.tiles()
 
-        self.assertTrue(new_tensor.issparse())
-        self.assertTrue(all(c.issparse() for c in new_tensor.chunks))
+def test_sparse():
+    tensor = ones((7, 12), chunk_size=4).tosparse()
+    new_tensor = tensor.rechunk(5)
+    new_tensor = tile(new_tensor)
 
-    def testOrder(self):
-        tensor = ones((7, 12), chunk_size=4, order='F')
-        new_tensor = tensor.rechunk(5)
+    assert new_tensor.issparse() is True
+    assert all(c.issparse() for c in new_tensor.chunks) is True
 
-        self.assertTrue(new_tensor.order.value, 'F')
+
+def test_order():
+    tensor = ones((7, 12), chunk_size=4, order='F')
+    new_tensor = tensor.rechunk(5)
+
+    assert new_tensor.order.value
