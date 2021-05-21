be03431a7b0f88b3af9ba532b926402c9aea8bce
hekaisheng <kaisheng.hks@alibaba-inc.com>
2021-05-21 17:06:56 +0800

    core & util, except test_fetch_tensor_corner_data and test_view_data_on_reshape

--- mars/tensor/tests/test_utils.py
+++ mars/tensor/tests/test_utils.py
@@ -13,83 +13,92 @@
 # limitations under the License.
 
 import numpy as np
+import pytest
 
-import unittest
-
-from mars.lib.mmh3 import hash_from_buffer as mmh3_hash_from_buffer
-from mars.session import new_session
 from mars import tensor as mt
+from mars.config import option_context
+from mars.lib.mmh3 import hash_from_buffer as mmh3_hash_from_buffer
 from mars.tensor.utils import hash_on_axis, normalize_axis_tuple, fetch_corner_data
+from mars.tests import new_test_session
 
 
-class Test(unittest.TestCase):
-    def testHashOnAxis(self):
-        hash_from_buffer = lambda x: mmh3_hash_from_buffer(memoryview(x))
+@pytest.fixture(scope='module')
+def setup():
+    sess = new_test_session(default=True)
+    with option_context({'show_progress': False}):
+        try:
+            yield sess
+        finally:
+            sess.stop_server()
 
-        a = np.random.rand(10)
 
-        result = hash_on_axis(a, 0, 3)
-        expected = np.array([mmh3_hash_from_buffer(element) % 3 for element in a])
+def test_hash_on_axis():
+    hash_from_buffer = lambda x: mmh3_hash_from_buffer(memoryview(x))
 
-        np.testing.assert_array_equal(result, expected)
+    a = np.random.rand(10)
 
-        result = hash_on_axis(a, 0, 1)
-        expected = np.array([0 for _ in a])
+    result = hash_on_axis(a, 0, 3)
+    expected = np.array([mmh3_hash_from_buffer(element) % 3 for element in a])
 
-        np.testing.assert_array_equal(result, expected)
+    np.testing.assert_array_equal(result, expected)
 
-        a = np.random.rand(10, 5)
+    result = hash_on_axis(a, 0, 1)
+    expected = np.array([0 for _ in a])
 
-        result = hash_on_axis(a, 0, 3)
-        expected = np.array([hash_from_buffer(a[i, :]) % 3 for i in range(a.shape[0])])
+    np.testing.assert_array_equal(result, expected)
 
-        np.testing.assert_array_equal(result, expected)
+    a = np.random.rand(10, 5)
 
-        result = hash_on_axis(a, 1, 3)
-        expected = np.array([hash_from_buffer(a[:, i]) % 3 for i in range(a.shape[1])])
+    result = hash_on_axis(a, 0, 3)
+    expected = np.array([hash_from_buffer(a[i, :]) % 3 for i in range(a.shape[0])])
 
-        np.testing.assert_array_equal(result, expected)
+    np.testing.assert_array_equal(result, expected)
 
-        a = np.random.rand(10, 5, 4)
+    result = hash_on_axis(a, 1, 3)
+    expected = np.array([hash_from_buffer(a[:, i]) % 3 for i in range(a.shape[1])])
 
-        result = hash_on_axis(a, 2, 3)
-        expected = np.array([hash_from_buffer(a[:, :, i]) % 3 for i in range(a.shape[2])])
+    np.testing.assert_array_equal(result, expected)
 
-        np.testing.assert_array_equal(result, expected)
+    a = np.random.rand(10, 5, 4)
 
-    def testNormalizeAxisTuple(self):
-        self.assertEqual(normalize_axis_tuple(-1, 3), (2,))
-        self.assertEqual(normalize_axis_tuple([0, -2], 3), (0, 1))
-        self.assertEqual(sorted(normalize_axis_tuple({0, -2}, 3)), [0, 1])
+    result = hash_on_axis(a, 2, 3)
+    expected = np.array([hash_from_buffer(a[:, :, i]) % 3 for i in range(a.shape[2])])
 
-        with self.assertRaises(ValueError) as cm:
-            normalize_axis_tuple((1, -2), 3, argname='axes')
-        self.assertIn('axes', str(cm.exception))
+    np.testing.assert_array_equal(result, expected)
 
-        with self.assertRaises(ValueError):
-            normalize_axis_tuple((1, -2), 3)
 
-    def testFetchTensorCornerData(self):
-        sess = new_session()
-        print_options = np.get_printoptions()
+def test_normalize_axis_tuple():
+    assert normalize_axis_tuple(-1, 3) == (2,)
+    assert normalize_axis_tuple([0, -2], 3) == (0, 1)
+    assert sorted(normalize_axis_tuple({0, -2}, 3)) == [0, 1]
 
-        # make sure numpy default option
-        self.assertEqual(print_options['edgeitems'], 3)
-        self.assertEqual(print_options['threshold'], 1000)
+    with pytest.raises(ValueError) as cm:
+        normalize_axis_tuple((1, -2), 3, argname='axes')
+    assert 'axes' in str(cm.value)
 
-        size = 12
-        for i in (2, 4, size - 3, size, size + 3):
-            arr = np.random.rand(i, i, i)
-            t = mt.tensor(arr, chunk_size=size // 2)
-            sess.run(t, fetch=False)
+    with pytest.raises(ValueError):
+        normalize_axis_tuple((1, -2), 3)
 
-            corner_data = fetch_corner_data(t, session=sess)
-            corner_threshold = 1000 if t.size < 1000 else corner_data.size - 1
-            with np.printoptions(threshold=corner_threshold, suppress=True):
-                # when we repr corner data, we need to limit threshold that
-                # it's exactly less than the size
-                repr_corner_data = repr(corner_data)
-            with np.printoptions(suppress=True):
-                repr_result = repr(arr)
-            self.assertEqual(repr_corner_data, repr_result,
-                             f'failed when size == {i}')
+
+def test_fetch_tensor_corner_data(setup):
+    print_options = np.get_printoptions()
+
+    # make sure numpy default option
+    assert print_options['edgeitems'] == 3
+    assert print_options['threshold'] == 1000
+
+    size = 12
+    for i in (2, 4, size - 3, size, size + 3):
+        arr = np.random.rand(i, i, i)
+        t = mt.tensor(arr, chunk_size=size // 2)
+        t.execute()
+
+        corner_data = fetch_corner_data(t)
+        corner_threshold = 1000 if t.size < 1000 else corner_data.size - 1
+        with np.printoptions(threshold=corner_threshold, suppress=True):
+            # when we repr corner data, we need to limit threshold that
+            # it's exactly less than the size
+            repr_corner_data = repr(corner_data)
+        with np.printoptions(suppress=True):
+            repr_result = repr(arr)
+        assert repr_corner_data == repr_result
