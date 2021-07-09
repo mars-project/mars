fb125ab2e2cc7be18cc6504a53f58df83b4b9d12
Xuye (Chris) Qin <xuye.qin@alibaba-inc.com>
2021-07-09 16:53:47 +0800

    Fix merge_index_value when index_values come from multi range indexes (#2207)

--- mars/dataframe/utils.py
+++ mars/dataframe/utils.py
@@ -714,24 +714,44 @@ def indexing_index_value(index_value, indexes, store_data=False):
             return parse_index(pd_index[indexes], store_data=store_data)
 
 
-def merge_index_value(to_merge_index_values, store_data=False):
+def merge_index_value(to_merge_index_values: dict, store_data: bool = False):
     """
     Merge index value according to their chunk index.
-    :param to_merge_index_values: Dict object. {index: index_value}
-    :return: Merged index_value
+
+    Parameters
+    ----------
+    to_merge_index_values : dict
+        index to index_value
+    store_data : bool
+        store data in index_value
+
+    Returns
+    -------
+    merged_index_value
     """
-    index_value = None
+
+    pd_index = None
     min_val, min_val_close, max_val, max_val_close = None, None, None, None
     for _, chunk_index_value in sorted(to_merge_index_values.items()):
-        if index_value is None:
-            index_value = chunk_index_value.to_pandas()
+        if pd_index is None:
+            pd_index = chunk_index_value.to_pandas()
             min_val, min_val_close, max_val, max_val_close = \
                 chunk_index_value.min_val, \
                 chunk_index_value.min_val_close, \
                 chunk_index_value.max_val, \
                 chunk_index_value.max_val_close
         else:
-            index_value = index_value.append(chunk_index_value.to_pandas())
+            cur_pd_index = chunk_index_value.to_pandas()
+            if store_data or (
+                    isinstance(pd_index, pd.RangeIndex) and
+                    isinstance(cur_pd_index, pd.RangeIndex) and
+                    cur_pd_index.step == pd_index.step and
+                    cur_pd_index.start == pd_index.stop
+            ):
+                # range index that is continuous
+                pd_index = pd_index.append(cur_pd_index)
+            else:
+                pd_index = pd.Index([], dtype=pd_index.dtype)
             if chunk_index_value.min_val is not None:
                 try:
                     if min_val is None or min_val > chunk_index_value.min_val:
@@ -746,13 +766,13 @@ def merge_index_value(to_merge_index_values, store_data=False):
                     max_val = chunk_index_value.max_val
                     max_val_close = chunk_index_value.max_val_close
 
-    new_index_value = parse_index(index_value, store_data=store_data)
-    if not new_index_value.has_value():
-        new_index_value._index_value._min_val = min_val
-        new_index_value._index_value._min_val_close = min_val_close
-        new_index_value._index_value._max_val = max_val
-        new_index_value._index_value._max_val_close = max_val_close
-    return new_index_value
+    index_value = parse_index(pd_index, store_data=store_data)
+    if not index_value.has_value():
+        index_value._index_value._min_val = min_val
+        index_value._index_value._min_val_close = min_val_close
+        index_value._index_value._max_val = max_val
+        index_value._index_value._max_val_close = max_val_close
+    return index_value
 
 
 def infer_dtypes(left_dtypes, right_dtypes, operator):
