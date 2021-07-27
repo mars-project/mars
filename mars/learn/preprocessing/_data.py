b9ea61f7cd32f1aba28f5182c80be72e1e4e4009
mac_zhu <zfm19980618@icloud.com>
2021-07-27 16:37:38 +0800

    delete unnecessary parts

--- mars/learn/preprocessing/_data.py
+++ mars/learn/preprocessing/_data.py
@@ -377,19 +377,3 @@ def minmax_scale(X, feature_range=(0, 1), *, axis=0, copy=True,
         X = X.ravel()
 
     return X.execute(session=session, **(run_kwargs or dict()))
-
-
-def _is_constant_feature(var, mean, n_samples):
-    """Detect if a feature is indistinguishable from a constant feature.
-
-    The detection is based on its computed variance and on the theoretical
-    error bounds of the '2 pass algorithm' for variance computation.
-
-    See "Algorithms for computing the sample variance: analysis and
-    recommendations", by Chan, Golub, and LeVeque.
-    """
-    # In scikit-learn, variance is always computed using float64 accumulators.
-    eps = np.finfo(np.float64).eps
-
-    upper_bound = n_samples * eps * var + (n_samples * mean * eps) ** 2
-    return var <= upper_bound
