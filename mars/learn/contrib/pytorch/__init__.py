422c4af983603a6ac308f4979b981ca642a2369e
yuanchong <yuanchongtt@163.com>
2021-08-05 14:46:23 +0800

    some changes in pytorch

--- mars/learn/contrib/pytorch/__init__.py
+++ mars/learn/contrib/pytorch/__init__.py
@@ -14,7 +14,7 @@
 
 from .run_script import run_pytorch_script
 from .dataset import MarsDataset
-from .sampler import MarsSequentialSampler, MarsRandomSampler
+from .sampler import SequentialSampler, RandomSampler
 
 
 def register_op():
