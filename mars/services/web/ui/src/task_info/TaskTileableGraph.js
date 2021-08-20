d2170822b780b575b004630e8051ddef944def97
Jiahe Yan <yjh020711@ucla.edu>
2021-08-20 14:51:39 +0800

    tileable progress

--- mars/services/web/ui/src/task_info/TaskTileableGraph.js
+++ mars/services/web/ui/src/task_info/TaskTileableGraph.js
@@ -34,19 +34,32 @@ export default class TaskTileableGraph extends React.Component {
             selectedTileable: null,
             tileables: [],
             dependencies: [],
+            tileableDetails: {},
         };
     }
 
     fetchGraphDetail() {
         const { sessionId, taskId } = this.props;
 
+        const tileableStatus = ['succeeded', 'running', 'cancelled', 'failed', 'pending'];
+
         fetch(`api/session/${sessionId}/task/${taskId
         }/tileable_graph?action=get_tileable_graph_as_json`)
             .then(res => res.json())
             .then((res) => {
+                console.log(res);
+                let newTileableDetails = {};
+                for (let i = 0; i < res.tileables.length; i++) {
+                    newTileableDetails[res.tileables[i].tileable_id] = {
+                        progress: Math.random(),
+                        status: tileableStatus[Math.floor(Math.random()*tileableStatus.length)],
+                    };
+                }
+
                 this.setState({
                     tileables: res.tileables,
                     dependencies: res.dependencies,
+                    tileableDetails: newTileableDetails,
                 });
             });
     }
@@ -62,6 +75,79 @@ export default class TaskTileableGraph extends React.Component {
                 && prevStates.dependencies !== this.state.dependencies) {
             d3Select('#svg-canvas').selectAll('*').remove();
 
+            // Set up an SVG group so that we can translate the final graph.
+            const svg = d3Select('#svg-canvas'),
+                inner = svg.append('g');
+
+            svg
+                .append("circle")
+                .attr("cx", 400)
+                .attr("cy", 20)
+                .attr("r", 6)
+                .style("fill", "#f4b400");
+            svg
+                .append("circle")
+                .attr("cx", 400)
+                .attr("cy", 40)
+                .attr("r", 6)
+                .style("fill", "#00ff00");
+            svg
+                .append("circle")
+                .attr("cx", 400)
+                .attr("cy", 60)
+                .attr("r", 6)
+                .style("fill", "#808080");
+            svg
+                .append("circle")
+                .attr("cx", 400)
+                .attr("cy", 80)
+                .attr("r", 6)
+                .style("fill", "#ff0000");
+            svg
+                .append("circle")
+                .attr("cx", 400)
+                .attr("cy", 100)
+                .attr("r", 6)
+                .attr("stroke", "#333")
+                .style("fill", "#ffffff");
+
+            svg
+                .append("text")
+                .attr("x", 420)
+                .attr("y", 100)
+                .text("Pending")
+                .style("font-size", "15px")
+                .attr("alignment-baseline", "middle");
+            svg
+                .append("text")
+                .attr("x", 420)
+                .attr("y", 20)
+                .text("Running")
+                .style("font-size", "15px")
+                .attr("alignment-baseline", "middle");
+            svg
+                .append("text")
+                .attr("x", 420)
+                .attr("y", 40)
+                .text("Succeeded")
+                .style("font-size", "15px")
+                .attr("alignment-baseline", "middle");
+            svg
+                .append("text")
+                .attr("x", 420)
+                .attr("y", 60)
+                .text("Cancelled")
+                .style("font-size", "15px")
+                .attr("alignment-baseline", "middle");
+            svg
+                .append("text")
+                .attr("x", 420)
+                .attr("y", 80)
+                .text("Failed")
+                .style("font-size", "15px")
+                .attr("alignment-baseline", "middle");
+
+
             this.g = new dagGraphLib.Graph().setGraph({});
             this.state.tileables.forEach((tileable) => {
                 const value = { tileable };
@@ -71,9 +157,43 @@ export default class TaskTileableGraph extends React.Component {
                 value.rx = value.ry = 5;
                 this.g.setNode(tileable.tileable_id, value);
 
+                const tileableDetail = this.state.tileableDetails[tileable.tileable_id];
+
+                var nodeProgressGradient = inner.append('linearGradient')
+                    .attr('id', 'progress-' + tileable.tileable_id);
+
+                if (tileableDetail.status === 'pending') {
+                    nodeProgressGradient.append('stop')
+                    .attr('stop-color', '#ffffff')
+                    .attr('offset', '0%');
+                } else if (tileableDetail.status === 'running') {
+                    nodeProgressGradient.append('stop')
+                    .attr('stop-color', '#f4b400')
+                    .attr('offset', tileableDetail.progress * 100 + '%');
+                } else if (tileableDetail.status === 'succeeded') {
+                    nodeProgressGradient.append('stop')
+                    .attr('stop-color', '#00ff00')
+                    .attr('offset', '100%');
+                } else if (tileableDetail.status === 'failed') {
+                    nodeProgressGradient.append('stop')
+                    .attr('stop-color', '#ff0000')
+                    .attr('offset', tileableDetail.progress * 100 + '%');
+                } else {
+                    nodeProgressGradient.append('stop')
+                    .attr('stop-color', '#808080')
+                    .attr('offset', tileableDetail.progress * 100 + '%');
+                }
+
+                nodeProgressGradient.append('stop')
+                    .attr('stop-color', '#ffffff')
+                    .attr('offset', '0%');
+
                 // In future fill color based on progress
                 const node = this.g.node(tileable.tileable_id);
-                node.style = 'fill: #f4b400; cursor: pointer;';
+                node.style = `
+                    cursor: pointer;
+                    stroke: #333;
+                    fill: url(#progress-` + tileable.tileable_id + `)`;
                 node.labelStyle = 'cursor: pointer';
             });
 
@@ -101,10 +221,6 @@ export default class TaskTileableGraph extends React.Component {
 
             // Create the renderer
             const render = new DagRender();
-
-            // Set up an SVG group so that we can translate the final graph.
-            const svg = d3Select('#svg-canvas'),
-                inner = svg.append('g');
 
             // Set up zoom support
             const zoom = d3Zoom().on('zoom', function (e) {
