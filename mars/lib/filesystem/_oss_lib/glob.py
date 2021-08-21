d1df0692657bc0c0292794edc52114d86ba52d6c
zz <zengzebin.zzb@antgroup.com>
2021-08-21 15:27:26 +0800

    Add test cases for glob and reformat code.

--- mars/lib/filesystem/_oss_lib/glob.py
+++ mars/lib/filesystem/_oss_lib/glob.py
@@ -20,6 +20,9 @@ obviously，this implementation is not optimal, it will cause too many oss reque
 Lately, We can then convert the glob expression into a regular expression, and then match the oss key list.
 But before that, we need to figure out how to deal with magic char in oss key,
 such like oss glob: oss://bucket/[key]/*, the key oss://bucket/[key]/a exactly exists.
+
+Notes:
+	OSS need a bucket to specify the file or dir, the "**" patten is not supported. So _isrecursive(pattern) is removed.
 """
 
 import fnmatch
@@ -32,68 +35,52 @@ __all__ = ["glob", "iglob", "escape"]
 
 
 def glob(pathname, *, recursive=False):
-	"""Return a list of paths matching a pathname pattern.
+    """Return a list of paths matching a pathname pattern.
     The pattern may contain simple shell-style wildcards a la
     fnmatch. However, unlike fnmatch, filenames starting with a
     dot are special cases that are not matched by '*' and '?'
     patterns.
-    If recursive is true, the pattern '**' will match any files and
-    zero or more directories and subdirectories.
     """
-	return list(iglob(pathname, recursive=recursive))
+    return list(iglob(pathname, recursive=recursive))
 
 
 def iglob(pathname, *, recursive=False):
-	"""Return an iterator which yields the paths matching a pathname pattern.
+    """Return an iterator which yields the paths matching a pathname pattern.
     The pattern may contain simple shell-style wildcards a la
     fnmatch. However, unlike fnmatch, filenames starting with a
     dot are special cases that are not matched by '*' and '?'
     patterns.
-    If recursive is true, the pattern '**' will match any files and
-    zero or more directories and subdirectories.
     """
-	it = _iglob(pathname, recursive, False)
-	if recursive and _isrecursive(pathname):
-		s = next(it)  # skip empty string
-		assert not s
-	return it
+    it = _iglob(pathname, recursive, False)
+    return it
 
 
 def _iglob(pathname, recursive, dironly):
-	dirname, basename = os.path.split(pathname)
-	if not has_magic(pathname):
-		assert not dironly
-		if basename:
-			if oss_exists(pathname):
-				yield pathname
-		else:
-			# Patterns ending with a slash should match only directories
-			if oss_isdir(dirname):
-				yield pathname
-		return
-	if not dirname:
-		if recursive and _isrecursive(basename):
-			yield from _glob2(dirname, basename, dironly)
-		else:
-			yield from _glob1(dirname, basename, dironly)
-		return
-	# `os.path.split()` returns the argument itself as a dirname if it is a
-	# drive or UNC path.  Prevent an infinite recursion if a drive or UNC path
-	# contains magic characters (i.e. r'\\?\C:').
-	if dirname != pathname and has_magic(dirname):
-		dirs = _iglob(dirname, recursive, True)
-	else:
-		dirs = [dirname]
-	if has_magic(basename):
-		if recursive and _isrecursive(basename):
-			glob_in_dir = _glob2
-		else:
-			glob_in_dir = _glob1
-	else:
-		glob_in_dir = _glob0
-	for dirname in dirs:
-		for name in glob_in_dir(dirname, basename, dironly):
-			yield os.path.join(dirname, name)
+    dirname, basename = os.path.split(pathname)
+    if not has_magic(pathname):
+        assert not dironly
+        if basename:
+            if oss_exists(pathname):
+                yield pathname
+        else:
+            # Patterns ending with a slash should match only directories
+            if oss_isdir(dirname):
+                yield pathname
+        return
+    # dirname will not be None in oss path.
+    #  Prevent an infinite recursion if a drive or UNC path
+    # contains magic characters (i.e. r'\\?\C:').
+    if dirname != pathname and has_magic(dirname):
+        dirs = _iglob(dirname, recursive, True)
+    else:
+        dirs = [dirname]
+    if has_magic(basename):
+        glob_in_dir = _glob1
+    else:
+        glob_in_dir = _glob0
+    for dirname in dirs:
+        for name in glob_in_dir(dirname, basename, dironly):
+            yield os.path.join(dirname, name)
 
 
 # These 2 helper functions non-recursively glob inside a literal directory.
@@ -101,71 +88,32 @@ def _iglob(pathname, recursive, dironly):
 # takes a literal basename (so it only has to check for its existence).
 
 def _glob1(dirname, pattern, dironly):
-	names = list(_iterdir(dirname, dironly))
-	if not _ishidden(pattern):
-		names = (x for x in names if not _ishidden(x))
-	return fnmatch.filter(names, pattern)
+    names = list(_iterdir(dirname, dironly))
+    if not _ishidden(pattern):
+        names = (x for x in names if not _ishidden(x))
+    return fnmatch.filter(names, pattern)
 
 
 def _glob0(dirname, basename, dironly):
-	if not basename:
-		# `os.path.split()` returns an empty basename for paths ending with a
-		# directory separator.  'q*x/' should match only directories.
-		if oss_isdir(dirname):
-			return [basename]
-	else:
-		if oss_exists(os.path.join(dirname, basename)):
-			return [basename]
-	return []
-
-
-# Following functions are not public but can be used by third-party code.
-
-def glob0(dirname, pattern):
-	return _glob0(dirname, pattern, False)
-
-
-def glob1(dirname, pattern):
-	return _glob1(dirname, pattern, False)
-
-
-# This helper function recursively yields relative pathnames inside a literal
-# directory.
-
-def _glob2(dirname, pattern, dironly):
-	assert _isrecursive(pattern)
-	yield pattern[:0]
-	yield from _rlistdir(dirname, dironly)
+    if not basename:
+        # `os.path.split()` returns an empty basename for paths ending with a
+        # directory separator.  'q*x/' should match only directories.
+        if oss_isdir(dirname):
+            return [basename]
+    else:
+        if oss_exists(os.path.join(dirname, basename)):
+            return [basename]
+    return []
 
 
 # If dironly is false, yields all file names inside a directory.
 # If dironly is true, yields only directory names.
+# An oss path must contain a dirname.
 def _iterdir(dirname, dironly):
-	if not dirname:
-		if isinstance(dirname, bytes):
-			dirname = bytes(os.curdir, 'ASCII')
-		else:
-			dirname = os.curdir
-	try:
-		for entry in oss_scandir(dirname):
-			try:
-				if not dironly or entry.is_dir():
-					yield entry.name
-			except OSError:
-				pass
-	except OSError:
-		return
-
-
-# Recursively yields relative pathnames inside a literal directory.
-def _rlistdir(dirname, dironly):
-	names = list(_iterdir(dirname, dironly))
-	for x in names:
-		if not _ishidden(x):
-			yield x
-			path = os.path.join(dirname, x) if dirname else x
-			for y in _rlistdir(path, dironly):
-				yield os.path.join(x, y)
+    for entry in oss_scandir(dirname):
+        if not dironly or entry.is_dir():
+            yield entry.name
+    return
 
 
 magic_check = re.compile('([*?[])')
@@ -173,32 +121,25 @@ magic_check_bytes = re.compile(b'([*?[])')
 
 
 def has_magic(s):
-	if isinstance(s, bytes):
-		match = magic_check_bytes.search(s)
-	else:
-		match = magic_check.search(s)
-	return match is not None
+    if isinstance(s, bytes):
+        match = magic_check_bytes.search(s)
+    else:
+        match = magic_check.search(s)
+    return match is not None
 
 
 def _ishidden(path):
-	return False
-
-
-def _isrecursive(pattern):
-	if isinstance(pattern, bytes):
-		return pattern == b'**'
-	else:
-		return pattern == '**'
+    return False
 
 
 def escape(pathname):
-	"""Escape all special characters.
+    """Escape all special characters.
     """
-	# Escaping is done by wrapping any of "*?[" between square brackets.
-	# Metacharacters do not work in the drive part and shouldn't be escaped.
-	drive, pathname = os.path.splitdrive(pathname)
-	if isinstance(pathname, bytes):
-		pathname = magic_check_bytes.sub(br'[\1]', pathname)
-	else:
-		pathname = magic_check.sub(r'[\1]', pathname)
-	return drive + pathname
+    # Escaping is done by wrapping any of "*?[" between square brackets.
+    # Metacharacters do not work in the drive part and shouldn't be escaped.
+    drive, pathname = os.path.splitdrive(pathname)
+    if isinstance(pathname, bytes):
+        pathname = magic_check_bytes.sub(br'[\1]', pathname)
+    else:
+        pathname = magic_check.sub(r'[\1]', pathname)
+    return drive + pathname
--- mars/lib/filesystem/oss.py
+++ mars/lib/filesystem/oss.py
@@ -22,111 +22,113 @@ from .base import FileSystem, path_type
 from ...utils import implements, ImportErrorHandler
 
 try:
-	import oss2
+    import oss2
 except ImportError:
-	oss2 = ImportErrorHandler('oss2')
+    oss2 = ImportErrorHandler('oss2')
 
 _oss_time_out = 10
 
 
 class OSSFileSystem(FileSystem):
-	_instance = None
-	
-	@classmethod
-	def get_instance(cls):
-		if cls._instance is None:
-			cls._instance = OSSFileSystem()
-		return cls._instance
-	
-	@implements(FileSystem.cat)
-	def cat(self, path: path_type):
-		raise NotImplementedError
-	
-	@implements(FileSystem.ls)
-	def ls(self, path: path_type) -> List[path_type]:
-		file_list = []
-		file_entry = oc.OSSFileEntry(path)
-		if not file_entry.is_dir():
-			raise OSError("ls for file is not supported")
-		else:
-			bucket, key, access_key_id, access_key_secret, end_point = oc.parse_osspath(path)
-			oss_bucket = oss2.Bucket(
-				auth=oss2.Auth(access_key_id=access_key_id,
-				               access_key_secret=access_key_secret),
-				endpoint=end_point,
-				bucket_name=bucket,
-				connect_timeout=_oss_time_out)
-			for obj in oss2.ObjectIteratorV2(oss_bucket, prefix=key):
-				obj_path = rf"oss://{bucket}/{obj.key}"
-				file_list.append(obj_path)
-		return file_list
-	
-	@implements(FileSystem.delete)
-	def delete(self,
-	           path: path_type,
-	           recursive: bool = False):
-		raise NotImplementedError
-	
-	@implements(FileSystem.rename)
-	def rename(self,
-	           path: path_type,
-	           new_path: path_type):
-		raise NotImplementedError
-	
-	@implements(FileSystem.stat)
-	def stat(self, path: path_type) -> Dict:
-		ofe = oc.OSSFileEntry(path)
-		return ofe.stat()
-	
-	@implements(FileSystem.mkdir)
-	def mkdir(self,
-	          path: path_type,
-	          create_parents: bool = True):
-		raise NotImplementedError
-	
-	@implements(FileSystem.isdir)
-	def isdir(self, path: path_type) -> bool:
-		file_entry = oc.OSSFileEntry(path)
-		return file_entry.is_dir()
-	
-	@implements(FileSystem.isfile)
-	def isfile(self, path: path_type) -> bool:
-		file_entry = oc.OSSFileEntry(path)
-		return file_entry.is_file()
-	
-	@implements(FileSystem._isfilestore)
-	def _isfilestore(self) -> bool:
-		raise NotImplementedError
-	
-	@implements(FileSystem.exists)
-	def exists(self, path: path_type):
-		return oc.oss_exists(path)
-	
-	@implements(FileSystem.open)
-	def open(self,
-	         path: path_type,
-	         mode: str = 'rb') -> OSSIOBase:
-		file_handle = OSSIOBase(path, mode)
-		return file_handle
-	
-	@implements(FileSystem.walk)
-	def walk(self, path: path_type) -> Iterator[Tuple[str, List[str], List[str]]]:
-		raise NotImplementedError
-	
-	@implements(FileSystem.glob)
-	def glob(self,
-	         path: path_type,
-	         recursive: bool = False) -> List[path_type]:
-		return glob(path, recursive=recursive)
+    _instance = None
+    
+    @classmethod
+    def get_instance(cls):
+        if cls._instance is None:
+            cls._instance = OSSFileSystem()
+        return cls._instance
+    
+    @implements(FileSystem.cat)
+    def cat(self, path: path_type):
+        raise NotImplementedError
+    
+    @implements(FileSystem.ls)
+    def ls(self, path: path_type) -> List[path_type]:
+        file_list = []
+        file_entry = oc.OSSFileEntry(path)
+        if not file_entry.is_dir():
+            raise OSError("ls for file is not supported")
+        else:
+            bucket, key, access_key_id, access_key_secret, end_point = oc.parse_osspath(path)
+            oss_bucket = oss2.Bucket(
+                auth=oss2.Auth(access_key_id=access_key_id,
+                               access_key_secret=access_key_secret),
+                endpoint=end_point,
+                bucket_name=bucket,
+                connect_timeout=_oss_time_out)
+            for obj in oss2.ObjectIteratorV2(oss_bucket, prefix=key):
+                if obj.key.endswith('/'):
+                    continue
+                obj_path = rf"oss://{bucket}/{obj.key}"
+                file_list.append(obj_path)
+        return file_list
+    
+    @implements(FileSystem.delete)
+    def delete(self,
+               path: path_type,
+               recursive: bool = False):
+        raise NotImplementedError
+    
+    @implements(FileSystem.rename)
+    def rename(self,
+               path: path_type,
+               new_path: path_type):
+        raise NotImplementedError
+    
+    @implements(FileSystem.stat)
+    def stat(self, path: path_type) -> Dict:
+        ofe = oc.OSSFileEntry(path)
+        return ofe.stat()
+    
+    @implements(FileSystem.mkdir)
+    def mkdir(self,
+              path: path_type,
+              create_parents: bool = True):
+        raise NotImplementedError
+    
+    @implements(FileSystem.isdir)
+    def isdir(self, path: path_type) -> bool:
+        file_entry = oc.OSSFileEntry(path)
+        return file_entry.is_dir()
+    
+    @implements(FileSystem.isfile)
+    def isfile(self, path: path_type) -> bool:
+        file_entry = oc.OSSFileEntry(path)
+        return file_entry.is_file()
+    
+    @implements(FileSystem._isfilestore)
+    def _isfilestore(self) -> bool:
+        raise NotImplementedError
+    
+    @implements(FileSystem.exists)
+    def exists(self, path: path_type):
+        return oc.oss_exists(path)
+    
+    @implements(FileSystem.open)
+    def open(self,
+             path: path_type,
+             mode: str = 'rb') -> OSSIOBase:
+        file_handle = OSSIOBase(path, mode)
+        return file_handle
+    
+    @implements(FileSystem.walk)
+    def walk(self, path: path_type) -> Iterator[Tuple[str, List[str], List[str]]]:
+        raise NotImplementedError
+    
+    @implements(FileSystem.glob)
+    def glob(self,
+             path: path_type,
+             recursive: bool = False) -> List[path_type]:
+        return glob(path, recursive=recursive)
 
 
 def build_oss_path(path: path_type, access_key_id, access_key_secret, end_point):
-	"""
-	Returns a path with oss info.
-	Used to register the access_key_id, access_key_secret and endpoint of OSS.
-	The access_key_id and endpoint are put into the url with url-safe-base64 encoding.
-	
-	Parameters
+    """
+    Returns a path with oss info.
+    Used to register the access_key_id, access_key_secret and endpoint of OSS.
+    The access_key_id and endpoint are put into the url with url-safe-base64 encoding.
+    
+    Parameters
     ----------
     path : path_type
         The original oss url.
@@ -144,12 +146,12 @@ def build_oss_path(path: path_type, access_key_id, access_key_secret, end_point)
     -------
     path_type
         Path include the encoded access key id, end point and access key secret of oss.
-	"""
-	if isinstance(path, (list, tuple)):
-		path = path[0]
-	param_dict = {'access_key_id': access_key_id, 'end_point': end_point}
-	id_endpoint = dict_to_url(param_dict)
-	password = access_key_secret
-	parse_result = parse.urlparse(path)
-	new_path = f'{parse_result.scheme}://{id_endpoint}:{password}@{parse_result.netloc}{parse_result.path}'
-	return new_path
+    """
+    if isinstance(path, (list, tuple)):
+        path = path[0]
+    param_dict = {'access_key_id': access_key_id, 'end_point': end_point}
+    id_endpoint = dict_to_url(param_dict)
+    password = access_key_secret
+    parse_result = parse.urlparse(path)
+    new_path = f'{parse_result.scheme}://{id_endpoint}:{password}@{parse_result.netloc}{parse_result.path}'
+    return new_path
