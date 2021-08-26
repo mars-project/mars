6fc16afa28168b725d4ae08db4a858083c924625
Jiahe Yan <yjh020711@ucla.edu>
2021-08-26 16:23:32 +0800

    more unit test

--- mars/services/task/tests/test_service.py
+++ mars/services/task/tests/test_service.py
@@ -407,8 +407,36 @@ async def test_get_subtasks(start_test_service):
     task_id = await task_api.submit_tileable_graph(graph, fuse_enabled=False)
 
     with pytest.raises(TaskNotExist):
-        await task_api.get_tileable_graph_as_json('non_exist')
+        await task_api.get_tileable_subtasks('non_exist')
 
+    tileable_graph = await task_api.get_tileable_graph_as_json(task_id)
+
+    for tileable in tileable_graph.get('tileables'):
+        subtask_details = await task_api.get_tileable_subtasks(task_id, tileable.get('tileableId'))
+
+        num_subtasks = len(subtask_details.get('subtasks'))
+        num_dependencies = len(subtask_details.get('dependencies'))
+        assert num_subtasks >= 0
+        assert num_dependencies <= (num_subtasks / 2) * (num_subtasks / 2)
+        assert ((num_subtasks == 0 or num_subtasks == 1) and num_dependencies == 0) or (num_subtasks > 1 and num_dependencies > 0)
+
+        subtask_ids = set()
+        for subtask in subtask_details.get('subtasks'):
+            assert subtask.get('status') >= 0 and subtask.get('status') <= 4
+            assert subtask.get('subtaskId') not in subtask_ids
+            subtask_ids.add(subtask.get('subtaskId'))
+
+        for dependency in subtask_details.get('dependencies'):
+            assert dependency.get('fromSubtaskId') in subtask_ids
+            assert dependency.get('toSubtaskId') in subtask_ids
+
+    r5 = mr.spawn(f, args=(0,))
+    r6 = mr.spawn(f, args=(r5,))
+
+    graph = TileableGraph([r6.data])
+    next(TileableGraphBuilder(graph).build())
+
+    task_id = await task_api.submit_tileable_graph(graph, fuse_enabled=True)
     tileable_graph = await task_api.get_tileable_graph_as_json(task_id)
 
     for tileable in tileable_graph.get('tileables'):
