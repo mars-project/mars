095d64bc05d94bfbd848d1370601c0e3547ea10b
wenjun.swj <wenjun.swj@alibaba-inc.com>
2021-08-31 11:03:53 +0800

    Fix code style

--- mars/oscar/tests/test_actorcaller.py
+++ mars/oscar/tests/test_actorcaller.py
@@ -1,10 +1,26 @@
-import asyncio
-import pytest
-import mock
+# Copyright 1999-2021 Alibaba Group Holding Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-from ..backends.router import Router
-from ..backends.core import ActorCaller
-from ..errors import ServerClosed
+import asyncio
+
+import pytest
+
+from mars.oscar.backends.router import Router
+from mars.oscar.backends.core import ActorCaller
+from mars.oscar.errors import ServerClosed
+from mars.tests.core import mock
+
 
 @pytest.mark.asyncio
 @mock.patch.object(Router, "get_client")
@@ -41,25 +57,23 @@ async def test_send_when_close(fake_get_client):
     caller = ActorCaller()
 
     router = Router(
-        external_addresses = ["test1"],
-        local_address = "test2",
+        external_addresses=["test1"],
+        local_address="test2",
     )
     futures = []
     for index in range(2):
         futures.append(await caller.call(
-            router = router,
-            dest_address = "test1",
-            message = FakeMessage(index),
-            wait = False))
+            router=router, dest_address="test1", message=FakeMessage(index),
+            wait=False
+        ))
 
     with pytest.raises(ServerClosed):
         # Just wait _list run.
         await asyncio.sleep(1)
         await caller.call(
-            router = router,
-            dest_address = "test1",
-            message = FakeMessage(2),
-            wait = False)
+            router=router, dest_address="test1", message=FakeMessage(2),
+            wait=False
+        )
 
     res0 = await futures[0]
     assert res0.message_id == 0
