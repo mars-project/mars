caa340667f9fd6dc8500fb4874367b2973a4d65a
易可 <a923602148@163.com>
2021-09-29 16:05:39 +0800

    style

--- mars/services/mutable/worker/core.py
+++ mars/services/mutable/worker/core.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from sys import last_value
 from typing import Tuple
 from collections import OrderedDict
+
 import numpy as np
 
 
