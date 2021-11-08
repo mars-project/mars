adc55e000a796f8f1f882dd12b397c11b9afb955
mubai <chaokun.yck@antfin.com>
2021-11-08 17:43:30 +0800

    fix add/finish subtasks

--- mars/services/scheduling/supervisor/manager.py
+++ mars/services/scheduling/supervisor/manager.py
@@ -79,9 +79,6 @@ class SubtaskManagerActor(mo.Actor):
         self._subtask_max_reschedules = subtask_max_reschedules
         self._subtask_cancel_timeout = subtask_cancel_timeout
         self._speculation_config = speculation_config or {}
-        self._subtask_speculation_enabled = self._speculation_config.get('enabled', False)
-        assert self._subtask_speculation_enabled in (True, False)
-
         self._queueing_ref = None
         self._global_slot_ref = None
         logger.info('Created SubtaskManager with subtask_max_reschedules %s, '
@@ -94,10 +91,9 @@ class SubtaskManagerActor(mo.Actor):
         from ..supervisor import GlobalSlotManagerActor
         self._global_slot_ref = await mo.actor_ref(
             GlobalSlotManagerActor.default_uid(), address=self.address)
-        if self._subtask_speculation_enabled:
-            self._speculation_execution_scheduler = SpeculativeScheduler(
-                self._queueing_ref, self._global_slot_ref, self._speculation_config)
-            await self._speculation_execution_scheduler.start()
+        self._speculation_execution_scheduler = SpeculativeScheduler(
+            self._queueing_ref, self._global_slot_ref, self._speculation_config)
+        await self._speculation_execution_scheduler.start()
 
     async def __pre_destroy__(self):
         if self._subtask_speculation_enabled:
@@ -389,6 +385,9 @@ class SpeculativeScheduler:
         self._grouped_finished_subtasks = defaultdict(dict)
         self._queueing_ref = queueing_ref
         self._global_slot_ref = global_slot_ref
+        self._speculation_config = speculation_config
+        self._subtask_speculation_enabled = speculation_config.get('enabled', False)
+        assert self._subtask_speculation_enabled in (True, False)
         self._subtask_speculation_dry = speculation_config.get('dry', False)
         self._subtask_speculation_threshold = parse_readable_size(speculation_config.get(
             'threshold', DEFAULT_SUBTASK_SPECULATION_THRESHOLD))[0]
@@ -400,16 +399,17 @@ class SpeculativeScheduler:
             'multiplier', DEFAULT_SUBTASK_SPECULATION_MULTIPLIER)
         self._subtask_speculation_max_concurrent_run = speculation_config.get(
             'max_concurrent_run', DEFAULT_SUBTASK_MAX_CONCURRENT_RUN)
-        assert 1 >= self._subtask_speculation_threshold > 0
-        assert self._subtask_speculation_interval > 0
-        assert self._subtask_speculation_min_task_runtime > 0
-        assert self._subtask_speculation_multiplier > 0
-        assert self._subtask_speculation_max_concurrent_run > 0
+        if self._subtask_speculation_enabled:
+            assert 1 >= self._subtask_speculation_threshold > 0
+            assert self._subtask_speculation_interval > 0
+            assert self._subtask_speculation_min_task_runtime > 0
+            assert self._subtask_speculation_multiplier > 0
+            assert self._subtask_speculation_max_concurrent_run > 0
         self._speculation_execution_task = None
 
     async def start(self):
         self._speculation_execution_task = create_task_with_ex_logged(self._speculative_execution())
-        logger.info('Speculative execution started.')
+        logger.info('Speculative execution started with config %s.', self._speculation_config)
 
     async def stop(self):
         self._speculation_execution_task.cancel()
@@ -418,17 +418,18 @@ class SpeculativeScheduler:
 
     def add_subtask(self, subtask_info: SubtaskScheduleInfo):
         # duplicate subtask add will be handled in `_speculative_execution`.
-        self._grouped_unfinished_subtasks[subtask_info.subtask.logic_id][subtask_info.subtask_id] = subtask_info
+        subtask = subtask_info.subtask
+        self._grouped_unfinished_subtasks[subtask.logic_id][subtask.subtask_id] = subtask_info
 
     def finish_subtask(self, subtask_info: SubtaskScheduleInfo):
-        grouped_finished_subtasks = self._grouped_finished_subtasks[subtask_info.subtask.logic_id]
-        grouped_finished_subtasks[subtask_info.subtask_id] = subtask_info
-        if len(grouped_finished_subtasks) == subtask_info.subtask.parallelism:
-            self._grouped_finished_subtasks.pop(subtask_info.subtask.logic_id)
-            self._grouped_unfinished_subtasks.pop(subtask_info.subtask.logic_id, None)
+        subtask = subtask_info.subtask
+        grouped_finished_subtasks = self._grouped_finished_subtasks[subtask.logic_id]
+        grouped_finished_subtasks[subtask.subtask_id] = subtask_info
+        if len(grouped_finished_subtasks) == subtask.parallelism:
+            self._grouped_finished_subtasks.pop(subtask.logic_id)
+            self._grouped_unfinished_subtasks.pop(subtask.logic_id, None)
             logger.info('Subtask group with logic id %s parallelism %s finished.',
-                        subtask_info.subtask.logic_id,
-                        subtask_info.subtask.parallelism)
+                        subtask.logic_id, subtask.parallelism)
 
     async def _speculative_execution(self):
         while True:
