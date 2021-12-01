03b1313590666294877360d3f30aee841d0d456b
刘宝 <po.lb@antfin.com>
2021-12-01 15:09:59 +0800

    Fix lint

--- mars/utils.py
+++ mars/utils.py
@@ -43,7 +43,7 @@ import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator
 
-from ._utils import (
+from ._utils import (  # noqa: F401; pylint: disable=unused-variable
     to_binary,
     to_str,
     to_text,
