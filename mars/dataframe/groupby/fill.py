8364514bffab5f68bbc462a6ac7245df04fb8918
继盛 <xuye.qin@alibaba-inc.com>
2022-01-25 11:36:02 +0800

    Flake8

--- mars/dataframe/groupby/fill.py
+++ mars/dataframe/groupby/fill.py
@@ -162,12 +162,12 @@ class GroupByFillNa(GroupByFillOperand):
 
 def ffill(groupby, limit=None):
     """
-        Forward fill the values.
-        
-        limit:  int, default None
-                Limit number of values to fill
-                
-        return: Series or DataFrame
+    Forward fill the values.
+
+    limit:  int, default None
+            Limit number of values to fill
+
+    return: Series or DataFrame
     """
     op = GroupByFFill(limit=limit)
     return op(groupby)
@@ -175,12 +175,12 @@ def ffill(groupby, limit=None):
 
 def bfill(groupby, limit=None):
     """
-        Backward fill the values.
-        
-        limit:  int, default None
-                Limit number of values to fill
-                
-        return: Series or DataFrame
+    Backward fill the values.
+
+    limit:  int, default None
+            Limit number of values to fill
+
+    return: Series or DataFrame
     """
     op = GroupByBFill(limit=limit)
     return op(groupby)
@@ -188,18 +188,23 @@ def bfill(groupby, limit=None):
 
 def fillna(groupby, value=None, method=None, axis=None, limit=None, downcast=None):
     """
-        Fill NA/NaN values using the specified method
-        
-        value:  scalar, dict, Series, or DataFrame
-                Value to use to fill holes (e.g. 0), alternately a dict/Series/DataFrame of values specifying which value to use for each index (for a Series) or column (for a DataFrame). Values not in the dict/Series/DataFrame will not be filled. This value cannot be a list.
-        method: {'backfill','bfill','ffill',None}, default None
-        axis:   {0 or 'index', 1 or 'column'}
-        limit:  int, default None
-                If method is specified, this is the maximum number of consecutive NaN values to forward/backward fill
-        downcast:   dict, default None
-                    A dict of item->dtype of what to downcast if possible, or the string ‘infer’ which will try to downcast to an appropriate equal type
-                    
-        return: DataFrame or None
+    Fill NA/NaN values using the specified method
+
+    value:  scalar, dict, Series, or DataFrame
+            Value to use to fill holes (e.g. 0), alternately a dict/Series/DataFrame
+            of values specifying which value to use for each index (for a Series) or
+            column (for a DataFrame). Values not in the dict/Series/DataFrame
+            will not be filled. This value cannot be a list.
+    method: {'backfill','bfill','ffill',None}, default None
+    axis:   {0 or 'index', 1 or 'column'}
+    limit:  int, default None
+            If method is specified, this is the maximum number of consecutive
+            NaN values to forward/backward fill
+    downcast:   dict, default None
+                A dict of item->dtype of what to downcast if possible,
+                or the string ‘infer’ which will try to downcast to an appropriate equal type
+
+    return: DataFrame or None
     """
     op = GroupByFillNa(
         value=value, method=method, axis=axis, limit=limit, downcast=downcast
