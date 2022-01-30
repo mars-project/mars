5d792e2a8cb1e3fd6ac7e236e41251aa8e461929
Xuye (Chris) Qin <xuye.qin@alibaba-inc.com>
2022-01-30 10:12:26 +0800

    Add merging small files support for `md.{read_parquet, read_csv}` (#2661)
    * Simplify read_parquet
    
    * Add merge_small_files support
    
    * Add merge_small_files support for read_csv
    
    * Refine
    
    * Fix ut

--- mars/_version.py
+++ mars/_version.py
@@ -16,7 +16,7 @@ import subprocess
 import os
 from typing import NamedTuple, Optional
 
-version_info = (0, 9, 0, "a1")
+version_info = (0, 9, 0, "a2")
 _num_index = max(idx if isinstance(v, int) else 0 for idx, v in enumerate(version_info))
 __version__ = ".".join(map(str, version_info[: _num_index + 1])) + "".join(
     version_info[_num_index + 1 :]
