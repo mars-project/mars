34b5d90910012fbf2ed2a55413e25465bb3208a4
Wenjun Si <wenjun.swj@alibaba-inc.com>
2022-01-30 21:41:46 +0800

    [BACKPORT] Fix the wrong index_value generated by df.loc[:] (#2658) (#2666)

--- mars/dataframe/indexing/tests/test_indexing.py
+++ mars/dataframe/indexing/tests/test_indexing.py
@@ -430,6 +430,18 @@ def test_iloc_setitem():
     ]
     assert series.chunks[1].op.value == 2
 
+    raw = pd.DataFrame(
+        np.random.rand(9, 2),
+        index=["a1", "a2", "a3"] * 3,
+        columns=["x", "y"],
+    )
+    df = md.DataFrame(raw, chunk_size=4)
+    iloc_df = df.iloc[:, 1:]
+    tiled_df, tiled_iloc_df = tile(df, iloc_df)
+    # for full slice, index_value should be same as input chunk
+    for loc_chunk, chunk in zip(tiled_iloc_df.chunks, tiled_df.chunks):
+        assert loc_chunk.index_value.key == chunk.index_value.key
+
     # fancy index
     series = md.Series(pd.Series(np.arange(10)), chunk_size=3)
     series.iloc[[2, 4, 9]] = 3
@@ -619,6 +631,19 @@ def test_dataframe_loc():
 
     with pytest.raises(KeyError):
         _ = df.loc[:, ["non_exist"]]
+
+    # test loc chunk's index_value
+    raw = pd.DataFrame(
+        np.random.rand(9, 2),
+        index=["a1", "a2", "a3"] * 3,
+        columns=["x", "y"],
+    )
+    df = md.DataFrame(raw, chunk_size=4)
+    loc_df = df.loc[:, ["x"]]
+    tiled_df, tiled_loc_df = tile(df, loc_df)
+    # for full slice, index_value should be same as input chunk
+    for loc_chunk, chunk in zip(tiled_loc_df.chunks, tiled_df.chunks):
+        assert loc_chunk.index_value.key == chunk.index_value.key
 
 
 def test_loc_use_iloc():
