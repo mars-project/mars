8ba8645211965fe9eb35c4ca205204141951aaaa
继盛 <xuye.qin@alibaba-inc.com>
2022-02-15 18:14:23 +0800

    Implements coloring algorithm

--- mars/services/task/analyzer/fusion.py
+++ mars/services/task/analyzer/fusion.py
@@ -12,10 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import List, Tuple
+import itertools
+from collections import defaultdict
+from typing import Dict, List, Tuple
 
-from ....core import ChunkGraph, ChunkType
+from ....config import options
+from ....core import ChunkGraph
 from ....core.operand import VirtualOperand, Fetch
+from ....typing import BandType, ChunkType, OperandType
 from ....utils import build_fuse_chunk
 
 
@@ -110,3 +114,131 @@ class Fusion:
                 explored.update(selected)
                 fuses.append(list(selected))
         return self._fuse_nodes(fuses)
+
+
+class Coloring:
+    """
+    Coloring a chunk graph according to an algorithm
+    described in https://github.com/mars-project/mars/issues/2435
+    """
+
+    _band_to_colors: Dict[BandType, int]
+
+    def __init__(
+        self,
+        chunk_graph: ChunkGraph,
+        all_bands: List[BandType],
+        chunk_to_bands: Dict[ChunkType, BandType],
+        initial_same_color_num: int = None,
+        successor_same_color_num: int = None,
+    ):
+        self.chunk_graph = chunk_graph
+        self.all_bands = all_bands
+        self.chunk_to_bands = chunk_to_bands
+        self.initial_same_color_num = initial_same_color_num
+        if successor_same_color_num is None:
+            successor_same_color_num = options.combine_size * 2
+        self.successor_same_color_num = successor_same_color_num
+
+        self._coloring_iter = itertools.count()
+        # band to color
+        self._band_to_colors = {band: self._next_color() for band in self.all_bands}
+
+    def _next_color(self) -> int:
+        return next(self._coloring_iter)
+
+    @classmethod
+    def _can_color_same(cls, chunk: ChunkType, predecessors: List[ChunkType]) -> bool:
+        if (
+            any(isinstance(n.op, VirtualOperand) for n in [chunk] + predecessors)
+            or len({n.op.gpu for n in [chunk] + predecessors}) > 1
+            or chunk.op.scheduling_hint is not None
+            and not chunk.op.scheduling_hint.can_be_fused()
+        ):
+            return False
+        return True
+
+    def _color_init_nodes(self) -> Dict[OperandType, int]:
+        if self.initial_same_color_num is None:
+            return {
+                chunk.op: self._band_to_colors[band]
+                for chunk, band in self.chunk_to_bands.items()
+            }
+        else:
+            band_to_colors = defaultdict(list)
+            for band, color in self._band_to_colors.items():
+                band_to_colors[band].append(color)
+            color_to_size = defaultdict(lambda: 0)
+            op_to_colors = dict()
+            for chunk, band in self.chunk_to_bands.items():
+                color = band_to_colors[band][-1]
+                size = color_to_size[color]
+                if size >= self.initial_same_color_num:
+                    color = self._next_color()
+                    band_to_colors[band].append(color)
+                color_to_size[color] += 1
+                op_to_colors[chunk.op] = color
+            return op_to_colors
+
+    def color(self) -> Dict[ChunkType, int]:
+        chunk_to_colors = dict()
+
+        # step 1: Coloring the initial nodes according to the bands that assigned by assigner
+        op_to_colors = self._color_init_nodes()
+
+        # step2: Propagate color in the topological order,
+        # if the input nodes have same color, color it with the same color;
+        # otherwise, color with a new color.
+        chunk_to_is_broadcaster = dict()
+        for chunk in self.chunk_graph.topological_iter():
+            if self.chunk_graph.count_successors(chunk) > self.successor_same_color_num:
+                # is broadcaster
+                chunk_to_is_broadcaster[chunk] = True
+
+            if chunk.op in op_to_colors:
+                # colored
+                chunk_to_colors[chunk] = op_to_colors[chunk.op]
+                continue
+
+            predecessors = self.chunk_graph.predecessors(chunk)
+            pred_colors = {op_to_colors[pred.op] for pred in predecessors}
+            if len(predecessors) == 1 and chunk_to_is_broadcaster.get(predecessors[0]):
+                # predecessor is broadcaster, just allocate a new color
+                color = self._next_color()
+            elif len(pred_colors) == 1:
+                if self._can_color_same(chunk, predecessors):
+                    # predecessors have only 1 color, will color with same one
+                    color = next(iter(pred_colors))
+                else:
+                    color = self._next_color()
+            else:
+                # has more than 1 color, color a new one
+                assert len(pred_colors) > 1
+                color = self._next_color()
+
+            op_to_colors[chunk.op] = chunk_to_colors[chunk] = color
+
+        # step 3: Propagate with reversed topological order,
+        # check a node with its inputs, if all inputs have different color with itself, skip;
+        # otherwise, if some of inputs have the same color, but some others have different color,
+        # color the input nodes with same one with a new color, and propagate to its inputs and so on.
+        for chunk in self.chunk_graph.topological_iter(reverse=True):
+            pred_colors = {
+                op_to_colors[pred.op]
+                for pred in self.chunk_graph.iter_successors(chunk)
+            }
+            prev_color = curr_color = chunk_to_colors[chunk]
+            if curr_color in pred_colors and len(pred_colors) > 1:
+                # conflict
+                curr_color = self._next_color()
+                # color the descendants with same color to the new one
+                stack = list(self.chunk_graph.iter_successors(chunk))
+                while len(stack) > 0:
+                    node = stack.pop()
+                    node_color = chunk_to_colors[node]
+                    if node_color == prev_color:
+                        # same color, recolor to the new one
+                        chunk_to_colors[node] = op_to_colors[node.op] = curr_color
+                        stack.extend(self.chunk_graph.successors(node))
+
+        return chunk_to_colors
