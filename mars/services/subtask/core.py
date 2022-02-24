6a6b024349575306dca2f93db0d5d6ebfeb83b39
mubai <chaokun.yck@antfin.com>
2022-02-24 10:47:56 +0800

    fix ci

--- mars/services/subtask/core.py
+++ mars/services/subtask/core.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from collections import defaultdict
 from enum import Enum
 from typing import Iterable, List, Optional, Tuple
 
