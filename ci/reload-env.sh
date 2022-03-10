88e265d4ace1a17a273300d2c76d6ba7327dbe16
Wenjun Si <swj0066@gmail.com>
2022-03-10 11:05:41 +0800

    Update reload-env.sh

--- ci/reload-env.sh
+++ ci/reload-env.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -x
 export UNAME="$(uname | awk '{print tolower($0)}')"
 export PYTEST_CONFIG_WITHOUT_COV="--log-level=DEBUG --timeout=1500 -W ignore::PendingDeprecationWarning"
 export PYTEST_CONFIG="$PYTEST_CONFIG_WITHOUT_COV --cov-config=setup.cfg --cov-report= --cov=mars"
