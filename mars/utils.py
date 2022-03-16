e87a9a660d19c4c24804106c61e5c9571082cd22
hejialing <hejialing.hjl@antgroup.com>
2022-03-16 15:19:56 +0800

    fix import

--- mars/utils.py
+++ mars/utils.py
@@ -37,6 +37,7 @@ import warnings
 import zlib
 from abc import ABC
 from contextlib import contextmanager
+from enum import Enum
 from queue import PriorityQueue
 from typing import (
     Any,
@@ -55,7 +56,6 @@ from types import TracebackType
 import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator
-from enum import Enum
 
 from ._utils import (  # noqa: F401 # pylint: disable=unused-import
     to_binary,
