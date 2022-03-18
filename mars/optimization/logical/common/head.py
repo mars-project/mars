51aae536de9f24554f5f1beb0334bdbc268c3667
hekaisheng <heks93@163.com>
2022-03-18 10:18:11 +0800

    Fix ut

--- mars/optimization/logical/common/head.py
+++ mars/optimization/logical/common/head.py
@@ -121,7 +121,7 @@ class HeadPushDown(OptimizationRule):
             new_entity = (
                 new_op.new_tileable
                 if not isinstance(node, CHUNK_TYPE)
-                else new_op.new_entity
+                else new_op.new_chunk
             )
             new_node = new_entity([new_input_node], kws=[params]).data
             self._replace_node(node, new_node)
