fabe7fa117d38d49b5c2d52b2e30f1d7f1d4dafc
Shawn <chaokun.yck@antfin.com>
2022-03-24 22:33:06 +0800

    Fix duplicate node iteration in GraphAssigner (#2857)

---
+++ asv_bench/benchmarks/graph_assigner.py
@@ -0,0 +1,46 @@
+# Copyright 1999-2022 Alibaba Group Holding Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import mars.tensor as mt
+import mars.dataframe as md
+from mars.core.graph import TileableGraph, TileableGraphBuilder, ChunkGraphBuilder
+from mars.services.task.analyzer import GraphAnalyzer
+from mars.services.task.analyzer.assigner import GraphAssigner
+
+
+class ChunkGraphAssignerSuite:
+    """
+    Benchmark that times performance of chunk graph assigner
+    """
+
+    def setup(self):
+        num_rows = 10000
+        df1 = md.DataFrame(
+            mt.random.rand(num_rows, 4, chunk_size=10), columns=list("abcd")
+        )
+        df2 = md.DataFrame(
+            mt.random.rand(num_rows, 4, chunk_size=10), columns=list("abcd")
+        )
+        merged_df = df1.merge(df2, left_on="a", right_on="a")
+        graph = TileableGraph([merged_df.data])
+        next(TileableGraphBuilder(graph).build())
+        self.chunk_graph = next(ChunkGraphBuilder(graph, fuse_enabled=False).build())
+
+    def time_assigner(self):
+        start_ops = list(GraphAnalyzer._iter_start_ops(self.chunk_graph))
+        band_slots = {(f"worker-{i}", "numa-0"): 16 for i in range(50)}
+        current_assign = {}
+        assigner = GraphAssigner(self.chunk_graph, start_ops, band_slots)
+        assigned_result = assigner.assign(current_assign)
+        assert len(assigned_result) == len(start_ops)
