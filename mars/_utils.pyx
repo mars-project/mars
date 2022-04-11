5c1c659b8e47aa0606b46b8c72c194d5c363c3e1
Wenjun Si <wenjun.swj@alibaba-inc.com>
2022-04-11 16:45:02 +0800

    Change code of test_to_dot

--- mars/_utils.pyx
+++ mars/_utils.pyx
@@ -402,4 +402,4 @@ cdef class Timer:
 
 
 __all__ = ['to_str', 'to_binary', 'to_text', 'TypeDispatcher', 'tokenize', 'tokenize_int',
-           'register_tokenizer', 'insert_reversed_tuple', 'ceildiv', 'Timer']
+           'register_tokenizer', 'ceildiv', 'Timer']
