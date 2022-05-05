115ee0b1ca822df6d8f0711038daac5639cbacc4
Wenjun Si <wenjun.swj@alibaba-inc.com>
2022-05-05 15:51:04 +0800

    Fix version mismatch between kubernetes and minikube (#2986)

--- .github/workflows/install-minikube.sh
+++ .github/workflows/install-minikube.sh
@@ -5,24 +5,10 @@ export CHANGE_MINIKUBE_NONE_USER=true
 sudo apt-get -q update || true
 sudo apt-get install -yq conntrack jq
 
-get_latest_release() {
-  curl --silent "https://api.github.com/repos/$1/releases" |
-    jq -c '[.[] | select(.prerelease == false)][0].tag_name' |
-    sed -E 's/.*"([^"]+)".*/\1/'
-}
-
-K8S_VERSION=$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)
-if [[ "$K8S_VERSION" == *"alpha"* ]] || [[ "$K8S_VERSION" == *"beta"* ]] || [[ "$K8S_VERSION" == *"rc"* ]]; then
-  K8S_VERSION=$(get_latest_release "kubernetes/kubernetes")
-fi
-
-curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$K8S_VERSION/bin/linux/amd64/kubectl && \
-  chmod +x kubectl && sudo mv kubectl /usr/local/bin/
-
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && \
   chmod +x minikube && sudo mv minikube /usr/local/bin/
 
-sudo minikube start --vm-driver=none --kubernetes-version=$K8S_VERSION
+sudo minikube start --vm-driver=none
 export KUBECONFIG=$HOME/.kube/config
 sudo cp -R /root/.kube /root/.minikube $HOME/
 sudo chown -R $(id -u):$(id -g) $HOME/.kube $HOME/.minikube
@@ -31,6 +17,10 @@ sed "s/root/home\/$USER/g" $KUBECONFIG > tmp
 mv tmp $KUBECONFIG
 
 minikube update-context
+
+K8S_VERSION=$(minikube kubectl -- version --client --output='json' | jq -r '.clientVersion.gitVersion')
+curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$K8S_VERSION/bin/linux/amd64/kubectl && \
+  chmod +x kubectl && sudo mv kubectl /usr/local/bin/
 
 JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'
 until kubectl get nodes -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True"; do
