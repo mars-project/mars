49ee66f9227a88b2c02131938ded13dd40b46fa8
Saksham Kumar <saksham.kumar@bytedance.com>
2022-05-08 04:04:09 -0400

    code style check

--- mars/dataframe/groupby/sort.py
+++ mars/dataframe/groupby/sort.py
@@ -107,12 +107,7 @@ class DataFrameGroupbySortShuffle(MapReduceOperand, DataFrameOperandMixin):
     _n_partition = Int32Field("n_partition")
 
     def __init__(
-            self,
-            by=None,
-            n_partition=None,
-            inplace=None,
-            output_types=None,
-            **kw
+            self, by=None, n_partition=None, inplace=None, output_types=None, **kw
     ):
         super().__init__(
             _by=by,
@@ -147,11 +142,11 @@ class DataFrameGroupbySortShuffle(MapReduceOperand, DataFrameOperandMixin):
             if p_index == 0:
                 out_df = in_df.loc[: pivots[p_index]]
             elif p_index == op.n_partition - 1:
-                out_df = in_df.loc[pivots[p_index - 1]:].drop(
+                out_df = in_df.loc[pivots[p_index - 1] :].drop(
                     index=pivots[p_index - 1], errors="ignore"
                 )
             else:
-                out_df = in_df.loc[pivots[p_index - 1]: pivots[p_index]].drop(
+                out_df = in_df.loc[pivots[p_index - 1] : pivots[p_index]].drop(
                     index=pivots[p_index - 1], errors="ignore"
                 )
             return out_df
