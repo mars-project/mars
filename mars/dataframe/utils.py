4dabb97b7393363bd0bbff04e0ba84c2eb884a5a
Liu Bao <fyrestone@outlook.com>
2022-05-16 19:45:39 +0800

    Refine ThreadedServiceContext.get_chunks_meta usage (#3037)

--- mars/dataframe/utils.py
+++ mars/dataframe/utils.py
@@ -1086,7 +1086,7 @@ def standardize_range_index(chunks: List[ChunkType], axis: int = 0):
     for c in chunks:
         prev_chunks = row_chunks[: c.index[axis]]
         op = ChunkStandardizeRangeIndex(
-            prev_keys=[p.key for p in prev_chunks], axis=axis
+            prev_shapes=[p.shape for p in prev_chunks], axis=axis
         )
         op.output_types = c.op.output_types
         params = c.params.copy()
