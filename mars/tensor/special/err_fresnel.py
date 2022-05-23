700d4c535da9228b439042fe88056117b31aed64
RandomY-2 <69881587+RandomY-2@users.noreply.github.com>
2022-05-22 22:08:22 -0700

    removed comments & added doc entries

--- mars/tensor/special/err_fresnel.py
+++ mars/tensor/special/err_fresnel.py
@@ -58,51 +58,6 @@ class TensorErfcinv(TensorSpecialUnaryOp):
 @implement_scipy(spspecial.erf)
 @infer_dtype(spspecial.erf)
 def erf(x, out=None, where=None, **kwargs):
-    """
-    Returns the error function of complex argument.
-
-    It is defined as ``2/sqrt(pi)*integral(exp(-t**2), t=0..z)``.
-
-    Parameters
-    ----------
-    x : Tensor
-        Input tensor.
-
-    Returns
-    -------
-    res : Tensor
-        The values of the error function at the given points `x`.
-
-    See Also
-    --------
-    erfc, erfinv, erfcinv, wofz, erfcx, erfi
-
-    Notes
-    -----
-    The cumulative of the unit normal distribution is given by
-    ``Phi(z) = 1/2[1 + erf(z/sqrt(2))]``.
-
-    References
-    ----------
-    .. [1] https://en.wikipedia.org/wiki/Error_function
-    .. [2] Milton Abramowitz and Irene A. Stegun, eds.
-        Handbook of Mathematical Functions with Formulas,
-        Graphs, and Mathematical Tables. New York: Dover,
-        1972. http://www.math.sfu.ca/~cbm/aands/page_297.htm
-    .. [3] Steven G. Johnson, Faddeeva W function implementation.
-       http://ab-initio.mit.edu/Faddeeva
-
-    Examples
-    --------
-    >>> import mars.tensor as mt
-    >>> from mars.tensor import special
-    >>> import matplotlib.pyplot as plt
-    >>> x = mt.linspace(-3, 3)
-    >>> plt.plot(x, special.erf(x))
-    >>> plt.xlabel('$x$')
-    >>> plt.ylabel('$erf(x)$')
-    >>> plt.show()
-    """
     op = TensorErf(**kwargs)
     return op(x, out=out, where=where)
 
@@ -110,40 +65,6 @@ def erf(x, out=None, where=None, **kwargs):
 @implement_scipy(spspecial.erfc)
 @infer_dtype(spspecial.erfc)
 def erfc(x, out=None, where=None, **kwargs):
-    """
-    Returns the complementary error function for x
-
-    It is defined as ``1 - erf(x)``.
-
-    Parameters
-    ----------
-    x : Tensor
-        Input tensor.
-
-    out: Tensor or None
-        Optional output tensor for the complementary error function results.
-
-    Returns
-    -------
-    res : Scalar or Tensor
-        Results of the complementary error function for x.
-
-    See Also
-    --------
-    erfc, erfinv, erfcinv, wofz, erfcx, erfi
-
-    Examples
-    --------
-    >>> import mars.tensor as mt
-    >>> from mars.tensor import special
-    >>> # without optional tensor
-    >>> x = mt.linspace(-3, 3)
-    >>> special.erfc(x).execute()
-    >>> # with optional tensor
-    >>> out = mt.linspace(-3, 3)
-    >>> special.erfcx(x, out).execute()
-    >>> out
-    """
     op = TensorErfc(**kwargs)
     return op(x, out=out, where=where)
 
@@ -151,40 +72,6 @@ def erfc(x, out=None, where=None, **kwargs):
 @implement_scipy(spspecial.erfcx)
 @infer_dtype(spspecial.erfcx)
 def erfcx(x, out=None, where=None, **kwargs):
-    """
-    Returns the scaled complementary error function for x
-
-    It is defined as ``exp(x**2) * erfc(x)``.
-
-    Parameters
-    ----------
-    x : Tensor
-        Input tensor.
-
-    out: Tensor or None
-        Optional output tensor for the scaled complementary error function results.
-
-    Returns
-    -------
-    res : Scalar or Tensor
-        Results of the scaled complementary error function for x.
-
-    See Also
-    --------
-    erfc, erfinv, erfcinv, wofz, erfcx, erfi
-
-    Examples
-    --------
-    >>> import mars.tensor as mt
-    >>> from mars.tensor import special
-    >>> # without optional tensor
-    >>> x = mt.linspace(-3, 3)
-    >>> special.erfcx(x).execute()
-    >>> # with optional tensor
-    >>> out = mt.linspace(-3, 3)
-    >>> special.erfcx(x, out).execute()
-    >>> out
-    """
     op = TensorErfcx(**kwargs)
     return op(x, out=out, where=where)
 
@@ -192,40 +79,6 @@ def erfcx(x, out=None, where=None, **kwargs):
 @implement_scipy(spspecial.erfi)
 @infer_dtype(spspecial.erfi)
 def erfi(x, out=None, where=None, **kwargs):
-    """
-    Returns the imaginary error function for x
-
-    It is defined as ``-i erf(i, x)``.
-
-    Parameters
-    ----------
-    x : Tensor
-        Input tensor.
-
-    out: Tensor or None
-        Optional output tensor for the imaginary error function results.
-
-    Returns
-    -------
-    res : Scalar or Tensor
-        Results of the imaginary error function for x.
-
-    See Also
-    --------
-    erfc, erfinv, erfcinv, wofz, erfcx, erfi
-
-    Examples
-    --------
-    >>> import mars.tensor as mt
-    >>> from mars.tensor import special
-    >>> # without optional tensor
-    >>> x = mt.linspace(-3, 3)
-    >>> special.erfi(x).execute()
-    >>> # with optional tensor
-    >>> out = mt.linspace(-3, 3)
-    >>> special.erfi(x, out).execute()
-    >>> out
-    """
     op = TensorErfi(**kwargs)
     return op(x, out=out, where=where)
 
@@ -233,30 +86,6 @@ def erfi(x, out=None, where=None, **kwargs):
 @implement_scipy(spspecial.erfinv)
 @infer_dtype(spspecial.erfinv)
 def erfinv(x, out=None, where=None, **kwargs):
-    """
-    Returns the inverse of the error function for x
-
-    Parameters
-    ----------
-    x : Tensor
-        Input tensor.
-
-    Returns
-    -------
-    res : Tensor
-        Results of the inverse of the error function for x.
-
-    See Also
-    --------
-    erfc, erfinv, erfcinv, wofz, erfcx, erfi
-
-    Examples
-    --------
-    >>> import mars.tensor as mt
-    >>> from mars.tensor import special
-    >>> x = mt.linspace(-3, 3)
-    >>> special.erfinv(x).execute()
-    """
     op = TensorErfinv(**kwargs)
     return op(x, out=out, where=where)
 
@@ -264,28 +93,5 @@ def erfinv(x, out=None, where=None, **kwargs):
 @implement_scipy(spspecial.erfcinv)
 @infer_dtype(spspecial.erfcinv)
 def erfcinv(x, out=None, where=None, **kwargs):
-    """
-    Returns the inverse of the complementary error function for x
-
-    Parameters
-    ----------
-    x : Tensor
-        Input tensor.
-
-    Returns
-    -------
-    res : Tensor
-        Results of the inverse of the complementary error function for x.
-
-    See Also
-    --------
-    erfc, erfinv, erfcinv, wofz, erfcx, erfi
-    Examples
-    --------
-    >>> import mars.tensor as mt
-    >>> from mars.tensor import special
-    >>> x = mt.linspace(-3, 3)
-    >>> special.erfcinv(x).execute()
-    """
     op = TensorErfcinv(**kwargs)
     return op(x, out=out, where=where)
