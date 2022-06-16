8ca0d847b2a618580f6760742cd310364dd3f1c3
Shawn <shawn.ck.yang@gmail.com>
2022-06-16 16:38:19 +0800

    Fix deserializing RandomStateField when its value is None (#3149)

--- mars/tensor/random/core.py
+++ mars/tensor/random/core.py
@@ -313,6 +313,9 @@ def _on_serialize_random_state(rs):
 
 
 def _on_deserialize_random_state(tup):
+    if tup is None:
+        return None
+
     rs = np.random.RandomState()
     rs.set_state(tup)
     return rs
