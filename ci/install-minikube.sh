2a709e6705402f5ef48dbbe897172fb55d0ba4ef
wjsi <swj0066@gmail.com>
2022-06-24 08:22:29 +0800

    Manually install cri-dockerd before installing kubernetes

--- ci/install-minikube.sh
+++ ci/install-minikube.sh
@@ -5,6 +5,19 @@ export CHANGE_MINIKUBE_NONE_USER=true
 sudo apt-get -q update || true
 sudo apt-get install -yq conntrack jq
 
+CRI_DOCKERD_VERSION=$(curl -s https://api.github.com/repos/Mirantis/cri-dockerd/releases/latest | jq -r .tag_name | sed 's/v//g')
+curl -Lo /tmp/cri-dockerd.tgz "https://github.com/Mirantis/cri-dockerd/releases/download/v$CRI_DOCKERD_VERSION/cri-dockerd-$CRI_DOCKERD_VERSION.amd64.tgz"
+tar xzf /tmp/cri-dockerd.tgz -C /tmp
+sudo mv /tmp/cri-dockerd/cri-dockerd /usr/local/bin/
+cri-dockerd --version
+
+sudo curl -Lo /etc/systemd/system/cri-docker.service https://raw.githubusercontent.com/Mirantis/cri-dockerd/master/packaging/systemd/cri-docker.service
+sudo curl -Lo /etc/systemd/system/cri-docker.socket https://raw.githubusercontent.com/Mirantis/cri-dockerd/master/packaging/systemd/cri-docker.socket
+sudo sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/system/cri-docker.service
+sudo systemctl daemon-reload
+sudo systemctl enable cri-docker.service
+sudo systemctl enable --now cri-docker.socket
+
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && \
   chmod +x minikube && sudo mv minikube /usr/local/bin/
 
