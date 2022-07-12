2872137025a416cb3ae14298b7b994cfaf1a6ce5
刘宝 <po.lb@antfin.com>
2022-07-12 16:55:58 +0800

    Ensure the first char of key is a letter

--- mars/lib/mmh3_src/mmh3module.cpp
+++ mars/lib/mmh3_src/mmh3module.cpp
@@ -291,9 +291,12 @@ mmh3_hash_bytes(PyObject *self, PyObject *args, PyObject *keywds)
         Py_DECREF(target_mview);
     }
 
-    char bytes[16];
+    unsigned char bytes[16];
     memcpy(bytes, result, 16);
-    return PyBytes_FromStringAndSize(bytes, 16);
+    // Make the first char of hex be a letter, then the Mars chunk key can be a valid Python identifier:
+    // https://docs.python.org/3/reference/lexical_analysis.html#identifiers
+    bytes[0] |= 0xC0;
+    return PyBytes_FromStringAndSize((char*)bytes, 16);
 }
 
 struct module_state {
