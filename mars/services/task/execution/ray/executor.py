c0d930c59d601b800aa5cdaea2bfe97ab94f994e
Liu Bao <fyrestone@outlook.com>
2022-08-10 14:17:08 +0800

    Add subtask_num_cpus config, default is 1 (#3207)
    Co-authored-by: 刘宝 <po.lb@antgroup.com>

--- mars/services/task/execution/ray/executor.py
+++ mars/services/task/execution/ray/executor.py
@@ -489,6 +489,7 @@ class RayTaskExecutor(TaskExecutor):
         }
         shuffle_manager = ShuffleManager(subtask_graph)
         subtask_max_retries = self._config.get_subtask_max_retries()
+        subtask_num_cpus = self._config.get_subtask_num_cpus()
         for subtask in subtask_graph.topological_iter():
             if subtask.virtual:
                 continue
@@ -513,7 +514,9 @@ class RayTaskExecutor(TaskExecutor):
                 output_count = out_count + bool(subtask_output_meta_keys)
             subtask_max_retries = subtask_max_retries if subtask.retryable else 0
             output_object_refs = self._ray_executor.options(
-                num_returns=output_count, max_retries=subtask_max_retries
+                num_cpus=subtask_num_cpus,
+                num_returns=output_count,
+                max_retries=subtask_max_retries,
             ).remote(
                 subtask.task_id,
                 subtask.subtask_id,
